809c80baa815057b648b42004556c9ba63e77536
Richard Feldman <richard.t.feldman@gmail.com>
2019-01-27 17:39:45 -0500

    Initial code

---
+++ src/unify.rs
@@ -0,0 +1,52 @@
+use std::collections::HashMap;
+
+pub type Ident<'a> = &'a str;
+
+pub type Field<'a> = &'a str;
+
+#[derive(Debug, PartialEq)]
+pub enum Type<'a> {
+    String,
+    Record(HashMap<Field<'a>, Type<'a>>),
+    Assignment(Ident<'a>, Box<Type<'a>>)
+}
+
+
+// CANONICAL IR - we have already done stuff like giving errors for
+// duplicate field names
+
+#[derive(Debug, PartialEq)]
+pub enum Expr<'a> {
+    Literal(Literal<'a>),
+    Assignment(Ident<'a>, Box<Expr<'a>>),
+    // TODO add record update
+    // TODO add conditional
+    // TODO add function
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Literal<'a> {
+    String(&'a str),
+    Record(HashMap<Field<'a>, Expr<'a>>)
+}
+
+
+pub fn infer<'a>(expr: Expr<'a>) -> Type<'a> {
+    match expr {
+        Expr::Literal(Literal::String(_)) => Type::String,
+        Expr::Literal(Literal::Record(fields)) => {
+            let mut rec_type: HashMap<&'a str, Type<'a>> = HashMap::new();
+
+            for (field, subexpr) in fields {
+                let field_type = infer(subexpr);
+
+                rec_type.insert(&field, field_type);
+            }
+
+            Type::Record(rec_type)
+        },
+        Expr::Assignment(ident, subexpr) => {
+            Type::Assignment(ident, Box::new(infer(*subexpr)))
+        }
+    }
+}
---
+++ tests/test_unify.rs
@@ -0,0 +1,33 @@
+#[macro_use] extern crate maplit;
+#[macro_use] extern crate pretty_assertions;
+
+extern crate roc;
+
+#[cfg(test)]
+mod tests {
+    use roc::unify::Type;
+    use roc::unify::Expr::Literal;
+    use roc::unify::Literal::{String, Record};
+    use roc::unify::infer;
+
+    #[test]
+    fn test_infer_record_literals() {
+        let expr = Literal(Record(hashmap!{
+            "string" => Literal(String("doesn't matter")),
+            "record" => Literal(Record(hashmap!{
+                "x" => Literal(String("ignored")),
+                "y" => Literal(String("also ignored")),
+            }))
+        }));
+
+        let expected_type = Type::Record(hashmap!{
+            "string" => Type::String,
+            "record" => Type::Record(hashmap!{
+                "x" => Type::String,
+                "y" => Type::String,
+            })
+        });
+
+        assert_eq!(expected_type, infer(expr));
+    }
+}
