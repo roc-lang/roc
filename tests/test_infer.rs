28d9ce9e86189decfd75240e2c78eaba37495050
Folkert <folkert@folkertdev.nl>
2019-11-14 22:29:44 +0100

    add commented test for block string
    block strings are not parsed, so the test triggers a panic for now.

--- tests/test_infer.rs
+++ tests/test_infer.rs
@@ -64,6 +64,19 @@ mod test_infer {
         );
     }
 
+    // #[test]
+    // fn block_string_literal() {
+    //     infer_eq(
+    //         indoc!(
+    //             r#"
+    //             """type
+    //             inference!"""
+    //         "#
+    //         ),
+    //         "Str",
+    //     );
+    // }
+
     // LIST
 
     #[test]
@@ -624,5 +637,4 @@ mod test_infer {
             "Int",
         );
     }
-
 }
