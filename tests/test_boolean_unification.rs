14e9207a23709ba0e0175394712acf79adc90613
Folkert <folkert@folkertdev.nl>
2019-11-21 15:51:55 +0100

    do what clippy says

--- tests/test_boolean_unification.rs
+++ tests/test_boolean_unification.rs
@@ -8,20 +8,20 @@ extern crate roc;
 
 #[cfg(test)]
 mod test_format {
-    use bumpalo::Bump;
+    
     use roc::can::boolean_unification;
     use roc::can::boolean_unification::Term::*;
     use roc::can::boolean_unification::Var::*;
 
     #[test]
     fn ground_true() {
-        let (sub, term) = boolean_unification::unify(&Ground(true), vec![].iter());
+        let (_sub, term) = boolean_unification::unify(&Ground(true), vec![].iter());
         assert_eq!(boolean_unification::solve(&term), Some(true));
     }
 
     #[test]
     fn ground_false() {
-        let (sub, term) = boolean_unification::unify(&Ground(false), vec![].iter());
+        let (_sub, term) = boolean_unification::unify(&Ground(false), vec![].iter());
         assert_eq!(boolean_unification::solve(&term), Some(false));
     }
 
@@ -31,7 +31,7 @@ mod test_format {
         let input = Disjunction(Box::new(Variable(var.clone())), Box::new(Ground(true)));
         let vars = vec![var];
 
-        let (sub, term) = boolean_unification::unify(&input, vars.iter());
+        let (_sub, term) = boolean_unification::unify(&input, vars.iter());
         assert_eq!(boolean_unification::solve(&term), Some(true));
     }
 }
