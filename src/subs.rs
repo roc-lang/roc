e30206bf06dfc3637c6fc189b451236db8e9e02b
Richard Feldman <oss@rtfeldman.com>
2019-12-06 20:17:25 -0500

    Improve Mark Debug

--- src/subs.rs
+++ src/subs.rs
@@ -31,11 +31,11 @@ impl Mark {
 impl fmt::Debug for Mark {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         if self == &Mark::none() {
-            write!(f, "Mark::none")
+            write!(f, "none")
         } else if self == &Mark::occurs() {
-            write!(f, "Mark::occurs")
+            write!(f, "occurs")
         } else if self == &Mark::get_var_names() {
-            write!(f, "Mark::get_var_names")
+            write!(f, "get_var_names")
         } else {
             write!(f, "Mark({})", self.0)
         }
