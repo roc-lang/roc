7fa77e10601904bae71ae0d22c7e5fa0b61818f4
Richard Feldman <oss@rtfeldman.com>
2020-01-04 23:07:08 -0500

    Test parsing record updates

--- tests/test_parse.rs
+++ tests/test_parse.rs
@@ -21,6 +21,7 @@ mod test_parse {
     use roc::operator::BinOp::*;
     use roc::operator::CalledVia;
     use roc::operator::UnaryOp;
+    use roc::parse::ast::AssignedField::*;
     use roc::parse::ast::CommentOrNewline::*;
     use roc::parse::ast::Expr::{self, *};
     use roc::parse::ast::Pattern::{self, *};
@@ -249,6 +250,34 @@ mod test_parse {
         };
         let actual = parse_with(&arena, "{}");
 
+        assert_eq!(Ok(expected), actual);
+    }
+
+    #[test]
+    fn record_update() {
+        let arena = Bump::new();
+        let label1 = LabeledValue(
+            Located::new(0, 0, 16, 17, "x"),
+            &[],
+            arena.alloc(Located::new(0, 0, 19, 20, Int("5"))),
+        );
+        let label2 = LabeledValue(
+            Located::new(0, 0, 22, 23, "y"),
+            &[],
+            arena.alloc(Located::new(0, 0, 25, 26, Int("0"))),
+        );
+        let fields = bumpalo::vec![in &arena;
+            Located::new(0, 0, 16, 20, label1),
+            Located::new(0, 0, 22, 26, label2)
+        ];
+        let module_parts = bumpalo::vec![in &arena; "Foo", "Bar"].into_bump_slice();
+        let update_target = Located::new(0, 0, 2, 13, Var(module_parts, "baz"));
+        let expected = Record {
+            update: Some(&*arena.alloc(update_target)),
+            fields,
+        };
+
+        let actual = parse_with(&arena, "{ Foo.Bar.baz & x: 5, y: 0 }");
         assert_eq!(Ok(expected), actual);
     }
 
