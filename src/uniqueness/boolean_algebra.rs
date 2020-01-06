ac42117e10c7b20d01d2ef917f6fa84de0324923
Folkert <folkert@folkertdev.nl>
2020-01-06 20:30:06 +0100

    rewrite boolean_algebra
    This is based on code I got from Edsko de Vries

--- src/uniqueness/boolean_algebra.rs
+++ src/uniqueness/boolean_algebra.rs
@@ -1,6 +1,202 @@
-use crate::collections::ImMap;
+use crate::collections::{ImMap, ImSet};
 use crate::subs::Variable;
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Bool {
+    Zero,
+    One,
+    And(Box<Bool>, Box<Bool>),
+    Or(Box<Bool>, Box<Bool>),
+    Not(Box<Bool>),
+    Variable(Variable),
+}
+
+fn not(nested: Bool) -> Bool {
+    use self::Bool::*;
+    Not(Box::new(nested))
+}
+
+fn and(left: Bool, right: Bool) -> Bool {
+    use self::Bool::*;
+    And(Box::new(left), Box::new(right))
+}
+
+fn or(left: Bool, right: Bool) -> Bool {
+    use self::Bool::*;
+    Or(Box::new(left), Box::new(right))
+}
+
+type Substitution = ImMap<Variable, Bool>;
+
+impl Bool {
+    pub fn variables(&self) -> ImSet<Variable> {
+        let mut result = ImSet::default();
+
+        self.variables_help(&mut result);
+        result
+    }
+
+    fn variables_help(&self, vars: &mut ImSet<Variable>) {
+        use self::Bool::*;
+
+        match self {
+            Zero => (),
+            One => (),
+            And(left, right) => {
+                left.variables_help(vars);
+                right.variables_help(vars)
+            }
+            Or(left, right) => {
+                left.variables_help(vars);
+                right.variables_help(vars)
+            }
+            Not(nested) => nested.variables_help(vars),
+            Variable(var) => {
+                vars.insert(*var);
+            }
+        };
+    }
+
+    pub fn substitute(&self, substitutions: &Substitution) -> Self {
+        use self::Bool::*;
+
+        match self {
+            Zero => Zero,
+            One => One,
+            And(left, right) => And(
+                Box::new(left.substitute(substitutions)),
+                Box::new(right.substitute(substitutions)),
+            ),
+            Or(left, right) => Or(
+                Box::new(left.substitute(substitutions)),
+                Box::new(right.substitute(substitutions)),
+            ),
+            Not(nested) => Not(Box::new(nested.substitute(substitutions))),
+            Variable(current) => match substitutions.get(current) {
+                Some(new) => new.clone(),
+                None => Variable(*current),
+            },
+        }
+    }
+
+    pub fn is_var(&self) -> bool {
+        use self::Bool::*;
+
+        match self {
+            Variable(_) => true,
+            _ => false,
+        }
+    }
+}
+
+pub fn simplify(term: Bool) -> Bool {
+    panic!();
+}
+
+pub fn try_unify(p: Bool, q: Bool) -> Option<Substitution> {
+    use self::Bool::*;
+
+    let (sub, consistency) = unify(p, q);
+
+    let mut substitution = ImMap::default();
+
+    for (x, p) in sub.into_iter() {
+        if p != Variable(x) {
+            substitution.insert(x, p);
+        }
+    }
+
+    if consistency == Zero {
+        Some(substitution)
+    } else {
+        None
+    }
+}
+
+fn unify(p: Bool, q: Bool) -> (Substitution, Bool) {
+    let t = if q.is_var() && !p.is_var() {
+        or(and(q.clone(), not(p.clone())), and(not(q), p))
+    } else {
+        or(and(p.clone(), not(q.clone())), and(not(p), q))
+    };
+
+    unify0(t.variables(), t)
+}
+
+fn unify0(names: ImSet<Variable>, input: Bool) -> (Substitution, Bool) {
+    use self::Bool::*;
+
+    let mut substitution: Substitution = ImMap::default();
+    let mut term: Bool = simplify(input);
+    for x in names {
+        let mut sub_zero = ImMap::default();
+        sub_zero.insert(x, Zero);
+
+        let mut sub_one = ImMap::default();
+        sub_one.insert(x, One);
+
+        let subbed_zero = term.substitute(&sub_zero);
+        let subbed_one = term.substitute(&sub_one);
+
+        term = and(subbed_zero.clone(), subbed_one.clone());
+
+        let replacement: Bool = simplify(or(
+            subbed_zero.substitute(&substitution),
+            or(Variable(x), subbed_one.substitute(&substitution)),
+        ));
+
+        substitution.insert(x, replacement);
+    }
+
+    (substitution, term)
+}
+
+// --- Simplification ---
+
+fn normalize_term(term: &Bool) -> Bool {
+    use self::Bool::*;
+
+    match term {
+        Zero => Zero,
+        One => One,
+        And(left, right) => {
+            let p = normalize_term(&left);
+            let q = normalize_term(&right);
+
+            match (p == One, p == Zero, q == One, q == Zero) {
+                (true, _, _, _) => q,
+                (_, true, _, _) => Zero,
+                (_, _, true, _) => p,
+                (_, _, _, true) => Zero,
+                _ => and(p, q),
+            }
+        }
+        Or(left, right) => {
+            let p = normalize_term(&left);
+            let q = normalize_term(&right);
+
+            match (p == One, p == Zero, q == One, q == Zero) {
+                (true, _, _, _) => One,
+                (_, true, _, _) => q,
+                (_, _, true, _) => One,
+                (_, _, _, true) => p,
+                _ => or(p, q),
+            }
+        }
+        Not(nested) => {
+            let p = normalize_term(&nested);
+
+            match (p == One, p == Zero) {
+                (true, _) => Zero,
+                (_, true) => One,
+                _ => not(p),
+            }
+        }
+        Variable(v) => Variable(*v),
+    }
+}
+
+/*
 pub fn unify(typ: &BooleanAlgebra, _expected: &BooleanAlgebra) -> Option<Substitution> {
     // find the most general unifier.
     let mut val = typ.clone();
@@ -166,3 +362,4 @@ fn boolean_unification(
 
     (substitution, term.clone())
 }
+*/
