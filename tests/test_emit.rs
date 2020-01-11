aa8686f96ce7254df5883a4601f5c0177d5c5b2a
Richard Feldman <oss@rtfeldman.com>
2020-01-10 21:04:24 -0500

    Revert "Attempt to share some code between llvm and crane tests"
    This reverts commit c9f2bf77d94b9a5de2619a4a7e2b9049a7f47576.

--- tests/test_emit.rs
+++ tests/test_emit.rs
@@ -36,231 +36,252 @@ mod test_crane {
     use std::mem;
     use target_lexicon::HOST;
 
-    macro_rules! assert_evals_to {
+    macro_rules! assert_crane_evals_to {
         ($src:expr, $expected:expr, $ty:ty) => {
+            let arena = Bump::new();
+            let mut module: Module<SimpleJITBackend> =
+                Module::new(SimpleJITBuilder::new(default_libcall_names()));
+            let mut ctx = module.make_context();
+            let mut func_ctx = FunctionBuilderContext::new();
+
+            let (expr, _output, _problems, var_store, variable, constraint) = can_expr($src);
+            let mut subs = Subs::new(var_store.into());
+            let mut unify_problems = Vec::new();
+            let content = infer_expr(&mut subs, &mut unify_problems, &constraint, variable);
+            let shared_builder = settings::builder();
+            let shared_flags = settings::Flags::new(shared_builder);
+            let cfg = match isa::lookup(HOST) {
+                Err(err) => {
+                    panic!(
+                        "Unsupported target ISA for test runner {:?} - error: {:?}",
+                        HOST, err
+                    );
+                }
+                Ok(isa_builder) => {
+                    let isa = isa_builder.finish(shared_flags.clone());
+
+                    isa.frontend_config()
+                }
+            };
+
             let main_fn_name = "$Test.main";
+
+            // Compute main_fn_ret_type before moving subs to Env
+            let main_ret_type = type_from_content(&content, &mut subs, cfg);
+
+            // Compile and add all the Procs before adding main
+            let mut procs = MutMap::default();
+            let env = roc::crane::build::Env {
+                arena: &arena,
+                subs,
+                cfg,
+            };
+
+            // Populate Procs and Subs, and get the low-level Expr from the canonical Expr
+            let mono_expr = Expr::new(&arena, &env.subs, expr, &mut procs);
+            let mut scope = ImMap::default();
+            let mut declared = Vec::with_capacity(procs.len());
+
+            // Declare all the Procs, then insert them into scope so their bodies
+            // can look up their Funcs in scope later when calling each other by value.
+            for (name, opt_proc) in procs.iter() {
+                if let Some(proc) = opt_proc {
+                    let (func_id, sig) = declare_proc(&env, &mut module, name.clone(), proc);
+
+                    declared.push((proc.clone(), sig.clone(), func_id));
+
+                    scope.insert(name.clone(), ScopeEntry::Func { func_id, sig });
+                }
+            }
+
+            // Now that scope includes all the Procs, we can build their bodies.
+            for (proc, sig, fn_id) in declared {
+                define_proc_body(
+                    &env,
+                    &mut ctx,
+                    &mut module,
+                    fn_id,
+                    &scope,
+                    sig,
+                    proc,
+                    &procs,
+                );
+
+                // Verify the function we just defined
+                if let Err(errors) = verify_function(&ctx.func, &shared_flags) {
+                    // NOTE: We don't include proc here because it's already
+                    // been moved. If you need to know which proc failed, go back
+                    // and add some logging.
+                    panic!("Errors defining proc: {}", errors);
+                }
+            }
+
+            // Add main itself
+            let mut sig = module.make_signature();
+            sig.returns.push(AbiParam::new(main_ret_type));
+
+            let main_fn = module
+                .declare_function(main_fn_name, Linkage::Local, &sig)
+                .unwrap();
+
+            ctx.func.signature = sig;
+            ctx.func.name = ExternalName::user(0, main_fn.as_u32());
+
+            {
+                let mut builder: FunctionBuilder =
+                    FunctionBuilder::new(&mut ctx.func, &mut func_ctx);
+                let block = builder.create_ebb();
+
+                builder.switch_to_block(block);
+                // TODO try deleting this line and seeing if everything still works.
+                builder.append_ebb_params_for_function_params(block);
+
+                let main_body =
+                    roc::crane::build::build_expr(&env, &scope, &mut module, &mut builder, &mono_expr, &procs);
+
+                builder.ins().return_(&[main_body]);
+                // TODO re-enable this once Switch stops making unsealed
+                // EBBs, e.g. https://docs.rs/cranelift-frontend/0.52.0/src/cranelift_frontend/switch.rs.html#143
+                // builder.seal_block(block);
+                builder.seal_all_blocks();
+                builder.finalize();
+            }
+
+            module.define_function(main_fn, &mut ctx).unwrap();
+            module.clear_context(&mut ctx);
+
+            // Perform linking
+            module.finalize_definitions();
+
+            // Verify the main function
+            if let Err(errors) = verify_function(&ctx.func, &shared_flags) {
+                panic!("Errors defining {} - {}", main_fn_name, errors);
+            }
+
+            let main_ptr = module.get_finalized_function(main_fn);
+            let run_main = unsafe { mem::transmute::<_, fn() -> $ty>(main_ptr) };
+
+            assert_eq!(run_main(), $expected);
+        };
+    }
+
+    macro_rules! assert_llvm_evals_to {
+        ($src:expr, $expected:expr, $ty:ty) => {
             let arena = Bump::new();
             let (expr, _output, _problems, var_store, variable, constraint) = can_expr($src);
             let mut subs = Subs::new(var_store.into());
             let mut unify_problems = Vec::new();
             let content = infer_expr(&mut subs, &mut unify_problems, &constraint, variable);
 
-            // Populate Procs and Subs, and get the monomorphized Expr from the canonical Expr
+            let context = Context::create();
+            let module = context.create_module("app");
+            let builder = context.create_builder();
+            let fpm = PassManager::create(&module);
+
+            // Enable optimizations when running cargo test --release
+            if !cfg!(debug_assetions) {
+                fpm.add_instruction_combining_pass();
+                fpm.add_reassociate_pass();
+                fpm.add_basic_alias_analysis_pass();
+                fpm.add_promote_memory_to_register_pass();
+                fpm.add_cfg_simplification_pass();
+                fpm.add_gvn_pass();
+                // TODO figure out why enabling any of these (even alone) causes LLVM to segfault
+                // fpm.add_strip_dead_prototypes_pass();
+                // fpm.add_dead_arg_elimination_pass();
+                // fpm.add_function_inlining_pass();
+            }
+
+            fpm.initialize();
+
+            // Compute main_fn_type before moving subs to Env
+            let main_fn_type = content_to_basic_type(&content, &mut subs, &context)
+                .expect("Unable to infer type for test expr")
+                .fn_type(&[], false);
+            let main_fn_name = "$Test.main";
+
+            // Compile and add all the Procs before adding main
             let mut procs = MutMap::default();
-            let mono_expr = Expr::new(&arena, &subs, expr, &mut procs);
+            let env = roc::llvm::build::Env {
+                subs,
+                builder: &builder,
+                context: &context,
+                module: arena.alloc(module),
+            };
 
-            // Run Cranelift tests in their own scope
-            {
-                let mut module: Module<SimpleJITBackend> =
-                    Module::new(SimpleJITBuilder::new(default_libcall_names()));
-                let mut ctx = module.make_context();
-                let mut func_ctx = FunctionBuilderContext::new();
-                let shared_builder = settings::builder();
-                let shared_flags = settings::Flags::new(shared_builder);
-                let cfg = match isa::lookup(HOST) {
-                    Err(err) => {
-                        panic!(
-                            "Unsupported target ISA for test runner {:?} - error: {:?}",
-                            HOST, err
-                        );
-                    }
-                    Ok(isa_builder) => {
-                        let isa = isa_builder.finish(shared_flags.clone());
+            // Populate Procs and get the low-level Expr from the canonical Expr
+            let main_body = Expr::new(&arena, &env.subs, expr, &mut procs);
 
-                        isa.frontend_config()
-                    }
-                };
+            // Add all the Procs to the module
+            for (name, opt_proc) in procs.clone() {
+                if let Some(proc) = opt_proc {
+                    // NOTE: This is here to be uncommented in case verification fails.
+                    // (This approach means we don't have to defensively clone name here.)
+                    //
+                    // println!("\n\nBuilding and then verifying function {}\n\n", name);
+                    let fn_val = build_proc(&env, name, proc, &procs);
 
-                // Compute main_fn_ret_type before moving subs to Env
-                let main_ret_type = type_from_content(&content, &mut subs, cfg);
-
-                let env = roc::crane::build::Env {
-                    arena: &arena,
-                    subs,
-                    cfg,
-                };
-
-                let mut scope = ImMap::default();
-                let mut declared = Vec::with_capacity(procs.len());
-
-                // Declare all the Procs, then insert them into scope so their bodies
-                // can look up their Funcs in scope later when calling each other by value.
-                for (name, opt_proc) in procs.iter() {
-                    if let Some(proc) = opt_proc {
-                        let (func_id, sig) = declare_proc(&env, &mut module, name.clone(), proc);
-
-                        declared.push((proc.clone(), sig.clone(), func_id));
-
-                        scope.insert(name.clone(), ScopeEntry::Func { func_id, sig });
+                    if fn_val.verify(true) {
+                        fpm.run_on(&fn_val);
+                    } else {
+                        // NOTE: If this fails, uncomment the above println to debug
+                        panic!("Non-main function failed LLVM verification.");
                     }
                 }
-
-                // Now that scope includes all the Procs, we can build their bodies.
-                for (proc, sig, fn_id) in declared {
-                    define_proc_body(
-                        &env,
-                        &mut ctx,
-                        &mut module,
-                        fn_id,
-                        &scope,
-                        sig,
-                        proc,
-                        &procs,
-                    );
-
-                    // Verify the function we just defined
-                    if let Err(errors) = verify_function(&ctx.func, &shared_flags) {
-                        // NOTE: We don't include proc here because it's already
-                        // been moved. If you need to know which proc failed, go back
-                        // and add some logging.
-                        panic!("Errors defining proc: {}", errors);
-                    }
-                }
-
-                // Add main itself
-                let mut sig = module.make_signature();
-                sig.returns.push(AbiParam::new(main_ret_type));
-
-                let main_fn = module
-                    .declare_function(main_fn_name, Linkage::Local, &sig)
-                    .unwrap();
-
-                ctx.func.signature = sig;
-                ctx.func.name = ExternalName::user(0, main_fn.as_u32());
-
-                {
-                    let mut builder: FunctionBuilder =
-                        FunctionBuilder::new(&mut ctx.func, &mut func_ctx);
-                    let block = builder.create_ebb();
-
-                    builder.switch_to_block(block);
-                    // TODO try deleting this line and seeing if everything still works.
-                    builder.append_ebb_params_for_function_params(block);
-
-                    let main_body =
-                        roc::crane::build::build_expr(&env, &scope, &mut module, &mut builder, &mono_expr, &procs);
-
-                    builder.ins().return_(&[main_body]);
-                    // TODO re-enable this once Switch stops making unsealed
-                    // EBBs, e.g. https://docs.rs/cranelift-frontend/0.52.0/src/cranelift_frontend/switch.rs.html#143
-                    // builder.seal_block(block);
-                    builder.seal_all_blocks();
-                    builder.finalize();
-                }
-
-                module.define_function(main_fn, &mut ctx).unwrap();
-                module.clear_context(&mut ctx);
-
-                // Perform linking
-                module.finalize_definitions();
-
-                // Verify the main function
-                if let Err(errors) = verify_function(&ctx.func, &shared_flags) {
-                    panic!("Errors defining {} - {}", main_fn_name, errors);
-                }
-
-                let main_ptr = module.get_finalized_function(main_fn);
-                let run_main = unsafe { mem::transmute::<_, fn() -> $ty>(main_ptr) };
-
-                assert_eq!(run_main(), $expected);
-
             }
 
-            // Now run LLVM tests in their own scope
-            {
-                let context = Context::create();
-                let module = context.create_module("app");
-                let builder = context.create_builder();
-                let fpm = PassManager::create(&module);
+            // Add main to the module.
+            let main_fn = env.module.add_function(main_fn_name, main_fn_type, None);
 
-                // Enable optimizations when running cargo test --release
-                if !cfg!(debug_assetions) {
-                    fpm.add_instruction_combining_pass();
-                    fpm.add_reassociate_pass();
-                    fpm.add_basic_alias_analysis_pass();
-                    fpm.add_promote_memory_to_register_pass();
-                    fpm.add_cfg_simplification_pass();
-                    fpm.add_gvn_pass();
-                    // TODO figure out why enabling any of these (even alone) causes LLVM to segfault
-                    // fpm.add_strip_dead_prototypes_pass();
-                    // fpm.add_dead_arg_elimination_pass();
-                    // fpm.add_function_inlining_pass();
-                }
+            // Add main's body
+            let basic_block = context.append_basic_block(main_fn, "entry");
 
-                fpm.initialize();
+            builder.position_at_end(&basic_block);
 
-                // Compute main_fn_type before moving subs to Env
-                let main_fn_type = content_to_basic_type(&content, &mut subs, &context)
-                    .expect("Unable to infer type for test expr")
-                    .fn_type(&[], false);
+            let ret = roc::llvm::build::build_expr(
+                &env,
+                &ImMap::default(),
+                main_fn,
+                &main_body,
+                &mut MutMap::default(),
+            );
 
-                // Compile and add all the Procs before adding main
-                let env = roc::llvm::build::Env {
-                    subs,
-                    builder: &builder,
-                    context: &context,
-                    module: arena.alloc(module),
-                };
+            builder.build_return(Some(&ret));
 
-                // Add all the Procs to the module
-                for (name, opt_proc) in procs.clone() {
-                    if let Some(proc) = opt_proc {
-                        // NOTE: This is here to be uncommented in case verification fails.
-                        // (This approach means we don't have to defensively clone name here.)
-                        //
-                        // println!("\n\nBuilding and then verifying function {}\n\n", name);
-                        let fn_val = build_proc(&env, name, proc, &procs);
-
-                        if fn_val.verify(true) {
-                            fpm.run_on(&fn_val);
-                        } else {
-                            // NOTE: If this fails, uncomment the above println to debug
-                            panic!("Non-main function failed LLVM verification.");
-                        }
-                    }
-                }
-
-                // Add main to the module.
-                let main_fn = env.module.add_function(main_fn_name, main_fn_type, None);
-
-                // Add main's body
-                let basic_block = context.append_basic_block(main_fn, "entry");
-
-                builder.position_at_end(&basic_block);
-
-                let ret = roc::llvm::build::build_expr(
-                    &env,
-                    &ImMap::default(),
-                    main_fn,
-                    &mono_expr,
-                    &mut MutMap::default(),
-                );
-
-                builder.build_return(Some(&ret));
-
-                if !main_fn.verify(true) {
-                    panic!("Function {} failed LLVM verification.", main_fn_name);
-                }
-
-                // Uncomment this to see the module's LLVM instruction output:
-                // env.module.print_to_stderr();
-
-                let execution_engine = env
-                    .module
-                    .create_jit_execution_engine(OptimizationLevel::None)
-                    .expect("Error creating JIT execution engine for test");
-
-                unsafe {
-                    let main: JitFunction<unsafe extern "C" fn() -> $ty> = execution_engine
-                        .get_function(main_fn_name)
-                        .ok()
-                        .ok_or(format!("Unable to JIT compile `{}`", main_fn_name))
-                        .expect("errored");
-
-                    assert_eq!(main.call(), $expected);
-                }
+            if !main_fn.verify(true) {
+                panic!("Function {} failed LLVM verification.", main_fn_name);
             }
-        }
+
+            // Uncomment this to see the module's LLVM instruction output:
+            // env.module.print_to_stderr();
+
+            let execution_engine = env
+                .module
+                .create_jit_execution_engine(OptimizationLevel::None)
+                .expect("Error creating JIT execution engine for test");
+
+            unsafe {
+                let main: JitFunction<unsafe extern "C" fn() -> $ty> = execution_engine
+                    .get_function(main_fn_name)
+                    .ok()
+                    .ok_or(format!("Unable to JIT compile `{}`", main_fn_name))
+                    .expect("errored");
+
+                assert_eq!(main.call(), $expected);
+            }
+        };
+    }
+
+    macro_rules! assert_evals_to {
+        ($src:expr, $expected:expr, $ty:ty) => {
+            // Run Cranelift tests, then LLVM tests
+            {
+                assert_crane_evals_to!($src, $expected, $ty);
+            }
+            {
+                assert_llvm_evals_to!($src, $expected, $ty);
+            }
+        };
     }
 
     #[test]
