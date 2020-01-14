27cf9eb979570457757b0684445713c0a05a9e31
Chad Stearns <chad@humio.com>
2020-01-13 19:59:00 -0500

    Multiline list comments, and some more multiline list tests

--- src/fmt/expr.rs
+++ src/fmt/expr.rs
@@ -202,15 +202,56 @@ pub fn fmt_list<'a>(
 
     while let Some(item) = iter.next() {
         if is_multiline {
-            newline(buf, item_indent);
+            match &item.value {
+                Expr::SpaceBefore(expr_below, spaces_above_expr) => {
+                    newline(buf, item_indent);
+                    fmt_comments_only(buf, spaces_above_expr.iter(), item_indent);
+
+                    match &expr_below {
+                        Expr::SpaceAfter(expr_above, spaces_below_expr) => {
+                            fmt_expr(buf, expr_above, item_indent, false, false);
+
+                            if iter.peek().is_some() {
+                                buf.push(',');
+                            }
+
+                            fmt_if_spaces(buf, spaces_below_expr.iter(), item_indent);
+                        }
+                        _ => {
+                            fmt_expr(buf, expr_below, item_indent, false, false);
+                            if iter.peek().is_some() {
+                                buf.push(',');
+                            }
+                        }
+                    }
+                }
+
+                Expr::SpaceAfter(sub_expr, spaces) => {
+                    newline(buf, item_indent);
+
+                    fmt_expr(buf, sub_expr, item_indent, false, false);
+
+                    if iter.peek().is_some() {
+                        buf.push(',');
+                    }
+
+                    fmt_if_spaces(buf, spaces.iter(), item_indent);
+                }
+
+                _ => {
+                    newline(buf, item_indent);
+                    fmt_expr(buf, &item.value, item_indent, false, true);
+                    if iter.peek().is_some() {
+                        buf.push(',');
+                    }
+                }
+            }
         } else {
             buf.push(' ');
-        }
-
-        fmt_expr(buf, &item.value, item_indent, false, false);
-
-        if iter.peek().is_some() {
-            buf.push(',');
+            fmt_expr(buf, &item.value, item_indent, false, true);
+            if iter.peek().is_some() {
+                buf.push(',');
+            }
         }
     }
 
