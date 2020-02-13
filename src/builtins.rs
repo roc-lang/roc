87999d662eff420f4e4807acb56075a0a2ac0c62
Richard Feldman <oss@rtfeldman.com>
2020-02-12 22:23:44 -0500

    Add more builtin types

--- src/builtins.rs
+++ src/builtins.rs
@@ -11,143 +11,137 @@ const NUM_BUILTIN_IMPORTS: usize = 7;
 pub fn types() -> MutMap<Symbol, (SolvedType, Region)> {
     let mut types = HashMap::with_capacity_and_hasher(NUM_BUILTIN_IMPORTS, default_hasher());
 
-    // TODO instead of using Region::zero for all of these,
-    // instead use the Region where they were defined in their
-    // source .roc files! This can give nicer error messages.
+    let mut add_type = |symbol, typ| {
+        // TODO instead of using Region::zero for all of these,
+        // instead use the Region where they were defined in their
+        // source .roc files! This can give nicer error messages.
+        debug_assert!(!types.contains_key(&symbol));
+
+        types.insert(symbol, (typ, Region::zero()));
+    };
 
     // Str module
 
     // Str : [ @Str ]
-    types.insert(
+    add_type(
         Symbol::STR_STR,
-        (
-            SolvedType::Alias(
-                Symbol::STR_STR,
-                Vec::new(),
-                Box::new(SolvedType::Apply(Symbol::STR_AT_STR, Vec::new())),
-            ),
-            Region::zero(),
+        SolvedType::Alias(
+            Symbol::STR_STR,
+            Vec::new(),
+            Box::new(SolvedType::Apply(Symbol::STR_AT_STR, Vec::new())),
         ),
     );
 
     // Num module
 
     // Num range : [ @Num range ]
-    types.insert(
+    add_type(
         Symbol::NUM_NUM,
-        (
-            SolvedType::Alias(
-                Symbol::NUM_NUM,
-                vec!["range".into()],
-                Box::new(SolvedType::Apply(
-                    Symbol::NUM_AT_NUM,
-                    vec![SolvedType::Rigid("range".into())],
-                )),
-            ),
-            Region::zero(),
+        SolvedType::Alias(
+            Symbol::NUM_NUM,
+            vec!["range".into()],
+            Box::new(SolvedType::Apply(
+                Symbol::NUM_AT_NUM,
+                vec![SolvedType::Rigid("range".into())],
+            )),
         ),
     );
 
     // Int module
 
     // Integer : [ @Integer ]
-    types.insert(
+    add_type(
         Symbol::INT_INTEGER,
-        (
-            SolvedType::Alias(
-                Symbol::INT_INTEGER,
-                Vec::new(),
-                Box::new(SolvedType::Apply(Symbol::INT_AT_INTEGER, Vec::new())),
-            ),
-            Region::zero(),
+        SolvedType::Alias(
+            Symbol::INT_INTEGER,
+            Vec::new(),
+            Box::new(SolvedType::Apply(Symbol::INT_AT_INTEGER, Vec::new())),
         ),
     );
 
     // Int : Num Integer
-    types.insert(
+    add_type(
         Symbol::INT_INT,
-        (
-            SolvedType::Alias(
-                Symbol::INT_INT,
-                Vec::new(),
-                Box::new(SolvedType::Apply(
-                    Symbol::NUM_NUM,
-                    vec![SolvedType::Apply(Symbol::INT_INTEGER, Vec::new())],
-                )),
-            ),
-            Region::zero(),
-        ),
-    );
-
-    // Float module
-
-    // FloatingPoint : [ @FloatingPoint ]
-    types.insert(
-        Symbol::FLOAT_FLOATINGPOINT,
-        (
-            SolvedType::Alias(
-                Symbol::FLOAT_FLOATINGPOINT,
-                Vec::new(),
-                Box::new(SolvedType::Apply(
-                    Symbol::FLOAT_AT_FLOATINTPOINT,
-                    Vec::new(),
-                )),
-            ),
-            Region::zero(),
-        ),
-    );
-
-    // Float : Num FloatingPoint
-    types.insert(
-        Symbol::FLOAT_FLOAT,
-        (
-            SolvedType::Alias(
-                Symbol::FLOAT_FLOAT,
-                Vec::new(),
-                Box::new(SolvedType::Apply(
-                    Symbol::NUM_NUM,
-                    vec![SolvedType::Apply(Symbol::FLOAT_FLOATINGPOINT, Vec::new())],
-                )),
-            ),
-            Region::zero(),
+        SolvedType::Alias(
+            Symbol::INT_INT,
+            Vec::new(),
+            Box::new(SolvedType::Apply(
+                Symbol::NUM_NUM,
+                vec![SolvedType::Apply(Symbol::INT_INTEGER, Vec::new())],
+            )),
         ),
     );
 
     // highest : Float
-    types.insert(
-        Symbol::FLOAT_HIGHEST,
-        (
-            SolvedType::Apply(Symbol::FLOAT_FLOAT, Vec::new()),
-            Region::zero(),
+    add_type(Symbol::INT_HIGHEST, int_type());
+
+    // lowest : Float
+    add_type(Symbol::INT_LOWEST, int_type());
+
+    // Float module
+
+    // FloatingPoint : [ @FloatingPoint ]
+    add_type(
+        Symbol::FLOAT_FLOATINGPOINT,
+        SolvedType::Alias(
+            Symbol::FLOAT_FLOATINGPOINT,
+            Vec::new(),
+            Box::new(SolvedType::Apply(
+                Symbol::FLOAT_AT_FLOATINTPOINT,
+                Vec::new(),
+            )),
         ),
     );
 
-    // lowest : Float
-    types.insert(
-        Symbol::FLOAT_LOWEST,
-        (
-            SolvedType::Apply(Symbol::FLOAT_FLOAT, Vec::new()),
-            Region::zero(),
+    // Float : Num FloatingPoint
+    add_type(
+        Symbol::FLOAT_FLOAT,
+        SolvedType::Alias(
+            Symbol::FLOAT_FLOAT,
+            Vec::new(),
+            Box::new(SolvedType::Apply(
+                Symbol::NUM_NUM,
+                vec![SolvedType::Apply(Symbol::FLOAT_FLOATINGPOINT, Vec::new())],
+            )),
         ),
     );
+
+    // div : Float -> Float
+    add_type(
+        Symbol::FLOAT_DIV,
+        SolvedType::Func(vec![float_type(), float_type()], Box::new(float_type())),
+    );
+
+    // highest : Float
+    add_type(Symbol::FLOAT_HIGHEST, float_type());
+
+    // lowest : Float
+    add_type(Symbol::FLOAT_LOWEST, float_type());
 
     // List module
 
     // List elem : [ @List elem ]
-    types.insert(
+    add_type(
         Symbol::LIST_LIST,
-        (
-            SolvedType::Alias(
-                Symbol::LIST_LIST,
-                vec!["elem".into()],
-                Box::new(SolvedType::Apply(
-                    Symbol::LIST_AT_LIST,
-                    vec![SolvedType::Rigid("elem".into())],
-                )),
-            ),
-            Region::zero(),
+        SolvedType::Alias(
+            Symbol::LIST_LIST,
+            vec!["elem".into()],
+            Box::new(SolvedType::Apply(
+                Symbol::LIST_AT_LIST,
+                vec![SolvedType::Rigid("elem".into())],
+            )),
         ),
     );
 
     types
+}
+
+#[inline(always)]
+fn float_type() -> SolvedType {
+    SolvedType::Apply(Symbol::FLOAT_FLOAT, Vec::new())
+}
+
+#[inline(always)]
+fn int_type() -> SolvedType {
+    SolvedType::Apply(Symbol::INT_INT, Vec::new())
 }
