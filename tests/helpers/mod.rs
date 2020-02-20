5131cc5c3964233eee22b96c95104e7754203787
Richard Feldman <oss@rtfeldman.com>
2020-02-19 18:17:48 -0800

    rename an inaccurately named var

--- tests/helpers/mod.rs
+++ tests/helpers/mod.rs
@@ -182,8 +182,7 @@ pub fn can_expr_with(arena: &Bump, home: ModuleId, expr_str: &str) -> CanExprOut
 
     let mut scope = Scope::new(home);
     let dep_idents = IdentIds::exposed_builtins(0);
-    let home_ident_ids = IdentIds::default();
-    let mut env = Env::new(home, dep_idents, &module_ids, home_ident_ids);
+    let mut env = Env::new(home, dep_idents, &module_ids, IdentIds::default());
     let (loc_expr, output) = canonicalize_expr(
         &mut env,
         &var_store,
