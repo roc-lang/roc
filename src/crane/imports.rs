b3ead65c4415e8811cc39c3d828b18ee5ab565b3
Richard Feldman <oss@rtfeldman.com>
2020-02-28 21:03:57 -0500

    Remove obsolete test

--- src/crane/imports.rs
+++ src/crane/imports.rs
@@ -62,62 +62,6 @@ pub fn define_malloc_body<B: Backend>(
     module.define_function(func_id, ctx).unwrap();
 }
 
-#[test]
-fn libcall_function() {
-    use cranelift::prelude::*;
-    use cranelift_codegen::ir::types::I16;
-    use cranelift_codegen::ir::*;
-    use cranelift_codegen::isa::CallConv;
-    use cranelift_module::*;
-    use cranelift_simplejit::*;
-
-    let mut module: Module<SimpleJITBackend> =
-        Module::new(SimpleJITBuilder::new(default_libcall_names()));
-
-    // let sig = Signature {
-    //     params: vec![],
-    //     returns: vec![],
-    //     call_conv: CallConv::SystemV,
-    // };
-
-    // let func_id = module
-    //     .declare_function("function", Linkage::Local, &sig)
-    //     .unwrap();
-
-    let mut ctx = module.make_context();
-    define_malloc(&mut module, &mut ctx);
-    // ctx.func = Function::with_name_signature(ExternalName::user(0, func_id.as_u32()), sig);
-    // let mut func_ctx = FunctionBuilderContext::new();
-    // {
-    //     let mut bcx: FunctionBuilder = FunctionBuilder::new(&mut ctx.func, &mut func_ctx);
-    //     let block = bcx.create_block();
-    //     bcx.switch_to_block(block);
-
-    //     let int = module.target_config().pointer_type();
-    //     let zero = bcx.ins().iconst(I16, 0);
-    //     let size = bcx.ins().iconst(int, 10);
-
-    //     let mut signature = module.make_signature();
-    //     signature.params.push(AbiParam::new(int));
-    //     signature.returns.push(AbiParam::new(int));
-    //     let callee = module
-    //         .declare_function("malloc", Linkage::Import, &signature)
-    //         .expect("declare malloc function");
-    //     let local_callee = module.declare_func_in_func(callee, &mut bcx.func);
-    //     let argument_exprs = vec![size];
-    //     let call = bcx.ins().call(local_callee, &argument_exprs);
-    //     let buffer = bcx.inst_results(call)[0];
-
-    //     bcx.call_memset(module.target_config(), buffer, zero, size);
-
-    //     bcx.ins().return_(&[]);
-    // }
-
-    // module.define_function(func_id, &mut ctx).unwrap();
-
-    module.finalize_definitions();
-}
-
 pub fn define_malloc<B: Backend>(module: &mut Module<B>, ctx: &mut Context) -> FuncId {
     // TODO investigate whether we can remove this wrapper function somehow.
     // It may get inlined away, but it seems like it shouldn't be
