8fc75f2b2e1633fd536ffe8c95209e06499e50c9
Richard Feldman <oss@rtfeldman.com>
2020-03-06 18:08:00 -0500

    fix some tests

--- compiler/solve/tests/helpers/mod.rs
+++ compiler/solve/tests/helpers/mod.rs
@@ -151,7 +151,10 @@ pub fn uniq_expr_with(
         .collect();
 
     // load builtin values
-    let constraint = constrain_imported_values(imports, constraint, &var_store);
+
+    // TODO what to do with those rigids?
+    let (_introduced_rigids, constraint) =
+        constrain_imported_values(imports, constraint, &var_store);
 
     // load builtin types
     let mut constraint = load_builtin_aliases(&stdlib.aliases, constraint, &var_store);
@@ -231,7 +234,8 @@ pub fn can_expr_with(arena: &Bump, home: ModuleId, expr_str: &str) -> CanExprOut
         .collect();
 
     //load builtin values
-    let constraint = constrain_imported_values(imports, constraint, &var_store);
+    let (_introduced_rigids, constraint) =
+        constrain_imported_values(imports, constraint, &var_store);
 
     //load builtin types
     let mut constraint =
