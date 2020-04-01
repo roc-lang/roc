9aba6ff22eb0b2125b0076c8b9133461aab7242b
Richard Feldman <oss@rtfeldman.com>
2020-03-31 23:56:09 -0400

    Add a note about using build_cast

--- compiler/builtins/bitcode/src/lib.rs
+++ compiler/builtins/bitcode/src/lib.rs
@@ -1,3 +1,6 @@
+// TODO replace this with a normal Inkwell build_cast call - this was just
+// used as a proof of concept for getting bitcode importing working!
+
 #[no_mangle]
 pub fn i64_to_f64_(num: i64) -> f64 {
     num as f64
