a4a53c8e83c36b0f8830f2ee9a3697775feb8a62
Richard Feldman <oss@rtfeldman.com>
2020-04-05 09:15:07 -0400

    Add quicksort example

---
+++ examples/quicksort/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cargo run qs.roc
+gcc -shared qs.o -o libroc_qs_main.so
+
+# Move it to a different place depending on Linux vs macOS
+unameVal="$(uname -s)"
+
+case "${unameVal}" in
+    Linux*)     sudo mv libroc_qs_main.so /usr/lib/;;
+    Darwin*)    sudo mv libroc_qs_main.so /usr/local/lib/;;
+    *)          echo "build.sh does not support this operating system!" exit 1;
+esac
+
+rustc host.rs -o qs
+
+./qs
