50357acd5545b58c1105516b81599d64b27d41d9
Richard Feldman <oss@rtfeldman.com>
2020-05-03 21:38:36 -0400

    Add editor/ crate

---
+++ editor/src/lib.rs
@@ -0,0 +1,8 @@
+use std::io;
+
+/// The editor is actually launched from the CLI if you pass it zero arguments.
+pub fn launch() -> io::Result<()> {
+    println!("TODO launch the editor.");
+
+    Ok(())
+}
