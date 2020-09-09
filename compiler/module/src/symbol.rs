5bbee6217bb448a44e00d507773ea90c1526776a
Dimitar Apostolov <d.apostolov@gmail.com>
2020-09-09 21:58:25 +0200

    Add NUM_POW symbol to Num module

--- compiler/module/src/symbol.rs
+++ compiler/module/src/symbol.rs
@@ -640,6 +640,7 @@ define_builtins! {
         35 NUM_SQRT: "sqrt"
         36 NUM_ROUND: "round"
         37 NUM_COMPARE: "compare"
+        38 NUM_POW: "pow"
     }
     2 BOOL: "Bool" => {
         0 BOOL_BOOL: "Bool" imported // the Bool.Bool type alias
