318e50f98f30af97fa9499e44b29498a64297d00
Richard Feldman <oss@rtfeldman.com>
2020-09-12 23:25:29 -0400

    Drop obsolete Str.concat tests

--- compiler/gen/tests/gen_str.rs
+++ compiler/gen/tests/gen_str.rs
@@ -14,14 +14,6 @@ mod helpers;
 #[cfg(test)]
 mod gen_str {
     #[test]
-    fn str_concat() {
-        assert_evals_to!("Str.concat \"a\" \"b\"", "ab", &'static str);
-        assert_evals_to!("Str.concat \"\" \"second str\"", "second str", &'static str);
-        assert_evals_to!("Str.concat \"first str\" \"\"", "first str", &'static str);
-        assert_evals_to!("Str.concat \"\" \"\"", "", &'static str);
-    }
-
-    #[test]
     fn str_concat_big_to_big() {
         assert_evals_to!(
             indoc!(
