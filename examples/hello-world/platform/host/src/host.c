bec01e74e5f6f7acc6cc82c78bab045d34be4234
Richard Feldman <oss@rtfeldman.com>
2020-10-01 22:49:39 -0400

    Add a TODO for fixing hello-world host.c

--- examples/hello-world/platform/host/src/host.c
+++ examples/hello-world/platform/host/src/host.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+// TODO: use an actual RocStr here instead of char* - which is not the correct
+// type here, and which only works out to be equivalent in this particular
+// example by pure coincidence. This should be easy to segfault by returning
+// (for example) small or empty strings from Hello.roc. We should fix this!
 extern char* main_1();
 
 int main() {
