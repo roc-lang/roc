ff8bb64bae1ff204eb85e27a4b89b30b943da9a6
Richard Feldman <oss@rtfeldman.com>
2020-10-04 08:32:03 -0400

    Update quicksort macOS host

--- examples/quicksort/platform/host/src/host.rs
+++ examples/quicksort/platform/host/src/host.rs
@@ -1,15 +1,17 @@
+#![crate_type = "staticlib"]
+
 use std::time::SystemTime;
 
-#[link(name = "roc_app")]
 extern "C" {
     #[allow(improper_ctypes)]
-    #[link_name = "quicksort#1"]
+    #[link_name = "quicksort_1"]
     fn quicksort(list: Box<[i64]>) -> Box<[i64]>;
 }
 
 const NUM_NUMS: usize = 1_000_000;
 
-pub fn main() {
+#[no_mangle]
+pub fn rust_main() -> isize {
     let nums: Box<[i64]> = {
         let mut nums = Vec::with_capacity(NUM_NUMS);
 
@@ -39,4 +41,7 @@ pub fn main() {
     // this pointer would segfault/cause badness. Therefore, we
     // leak it for now
     Box::leak(answer);
+
+    // Exit code
+    0
 }
