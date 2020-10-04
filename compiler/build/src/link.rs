d776798e32cfaafa4f88e8a827903ce192e55a05
Richard Feldman <oss@rtfeldman.com>
2020-10-04 14:00:21 -0400

    Attempt to statically link hosts

--- compiler/build/src/link.rs
+++ compiler/build/src/link.rs
@@ -37,29 +37,12 @@ fn link_linux(
         .args(&[
             "-arch",
             arch_str(target),
-            "/usr/lib/x86_64-linux-gnu/crti.o",
-            "/usr/lib/x86_64-linux-gnu/crtn.o",
-            "/usr/lib/x86_64-linux-gnu/Scrt1.o",
-            "-dynamic-linker",
-            "/lib64/ld-linux-x86-64.so.2",
+            // Inputs
+            dest_filename.to_str().unwrap(),   // app.o
+            host_input_path.to_str().unwrap(), // host.o
             // Output
             "-o",
             binary_path.to_str().unwrap(), // app
-            // Inputs
-            host_input_path.to_str().unwrap(), // host.o
-            dest_filename.to_str().unwrap(),   // app.o
-            // Libraries - see https://github.com/rtfeldman/roc/pull/554#discussion_r496365925
-            // for discussion and further references
-            "-lc",
-            "-lm",
-            "-lpthread",
-            "-ldl",
-            "-lrt",
-            "-lutil",
-            "-lc_nonshared",
-            "-lc++",
-            "-lunwind",
-            // "-lgcc", // TODO will eventually need compiler_rt from gcc or something - see https://github.com/rtfeldman/roc/pull/554#discussion_r496370840
         ])
         .spawn()
 }
--- examples/quicksort/platform/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-clang -c host/src/host.c -o c_host.o
-rustc host/src/host.rs -o rust_host.o
-ld -r c_host.o rust_host.o -o host.o
-
-rm -f c_host.o
-rm -f rust_host.o
