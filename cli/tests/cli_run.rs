d9550b00a57acbe182471854d3832c990c2f9f21
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2020-10-12 15:08:08 -0700

    Run both optimized and unoptimized builds with valgrind

--- cli/tests/cli_run.rs
+++ cli/tests/cli_run.rs
@@ -11,48 +11,64 @@ mod helpers;
 
 #[cfg(test)]
 mod cli_run {
-    use crate::helpers::{example_file, extract_valgrind_errors, run_roc, run_with_valgrind};
+    use crate::helpers::{example_file, extract_valgrind_errors, run_roc, run_with_valgrind, Out};
 
     #[test]
     fn run_hello_world() {
-        let out = run_roc(&[
+        fn check_hello_world_output(out: Out) {
+            if !out.stderr.is_empty() {
+                panic!(out.stderr);
+            }
+            assert!(out.status.success());
+
+            let valgrind_out =
+                run_with_valgrind(&[example_file("hello-world", "app").to_str().unwrap()]);
+            assert!(&valgrind_out.stdout.ends_with("Hello, World!!!!!!!!!!!!!\n"));
+            assert!(valgrind_out.status.success());
+            let memory_errors = extract_valgrind_errors(&valgrind_out.stderr);
+            if !memory_errors.is_empty() {
+                panic!("{:?}", memory_errors);
+            }
+        }
+        check_hello_world_output(run_roc(&[
             "build",
             example_file("hello-world", "Hello.roc").to_str().unwrap(),
-        ]);
-        if !out.stderr.is_empty() {
-            panic!(out.stderr);
-        }
-        assert!(out.status.success());
-
-        let valgrind_out =
-            run_with_valgrind(&[example_file("hello-world", "app").to_str().unwrap()]);
-        assert!(&valgrind_out.stdout.ends_with("Hello, World!!!!!!!!!!!!!\n"));
-        assert!(valgrind_out.status.success());
-        let memory_errors = extract_valgrind_errors(&valgrind_out.stderr);
-        if !memory_errors.is_empty() {
-            panic!("{:?}", memory_errors);
-        }
+        ]));
+        check_hello_world_output(run_roc(&[
+            "build",
+            "--optimize",
+            example_file("hello-world", "Hello.roc").to_str().unwrap(),
+        ]));
     }
 
     #[test]
     fn run_quicksort() {
-        let out = run_roc(&[
+        fn check_quicksort_output(out: Out) {
+            if !out.stderr.is_empty() {
+                panic!(out.stderr);
+            }
+            assert!(out.status.success());
+
+            let valgrind_out =
+                run_with_valgrind(&[example_file("quicksort", "app").to_str().unwrap()]);
+            assert!(&valgrind_out
+                .stdout
+                .ends_with("[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]\n"));
+            assert!(valgrind_out.status.success());
+            let memory_errors = extract_valgrind_errors(&valgrind_out.stderr);
+            if !memory_errors.is_empty() {
+                panic!("{:?}", memory_errors);
+            }
+        }
+
+        check_quicksort_output(run_roc(&[
             "build",
             example_file("quicksort", "Quicksort.roc").to_str().unwrap(),
-        ]);
-        if !out.stderr.is_empty() {
-            panic!(out.stderr);
-        }
-        assert!(out.status.success());
-
-        let valgrind_out = run_with_valgrind(&[example_file("quicksort", "app").to_str().unwrap()]);
-        assert!(&valgrind_out
-            .stdout
-            .ends_with("[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]\n"));
-        assert!(valgrind_out.status.success());
-        let memory_errors = extract_valgrind_errors(&valgrind_out.stderr);
-        if !memory_errors.is_empty() {
-            panic!("{:?}", memory_errors);
-        }
+        ]));
+        check_quicksort_output(run_roc(&[
+            "build",
+            "--optimize",
+            example_file("quicksort", "Quicksort.roc").to_str().unwrap(),
+        ]));
     }
 }
