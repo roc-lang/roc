77b26c31933ad5e4608a5f56ce2ee2d4055b1f3c
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2020-10-12 16:51:09 -0700

    Add RocStr and convert HelloWord example to use it

--- examples/hello-world/platform/build.sh
+++ examples/hello-world/platform/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-# We have a C host, so this is only one step.
-clang -c host.c -o host.o
+# compile c_host.o and rust_host.o
+clang -c host.c -o c_host.o
+rustc host.rs -o rust_host.o
+
+# link them together into host.o
+ld -r c_host.o rust_host.o -o host.o
+
+# clean up
+rm -f c_host.o
+rm -f rust_host.o
--- examples/hello-world/platform/host.c
+++ examples/hello-world/platform/host.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 
-// TODO: use an actual RocStr here instead of char* - which is not the correct
-// type here, and which only works out to be equivalent in this particular
-// example by pure coincidence. This should be easy to segfault by returning
-// (for example) small or empty strings from Hello.roc. We should fix this!
-extern char* main_1();
+extern int rust_main();
 
 int main() {
-  printf("Roc says: %s\n", main_1());
-
-  return 0;
+  return rust_main();
 }
