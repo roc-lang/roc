0dfeb76d72c041ce7b3dc89210f873f7e4efd139
Folkert <folkert@folkertdev.nl>
2020-10-17 21:58:00 +0200

    un-ignore tests

--- compiler/solve/tests/solve_uniq_expr.rs
+++ compiler/solve/tests/solve_uniq_expr.rs
@@ -1423,7 +1423,6 @@ mod solve_uniq_expr {
     }
 
     #[test]
-    #[ignore]
     fn quicksort() {
         // theory: partition is handled before swap, so swap is not known, and therefore not taken
         // out of its closure
@@ -2839,7 +2838,6 @@ mod solve_uniq_expr {
     }
 
     #[test]
-    #[ignore]
     fn astar_full_code() {
         // theory: things are canonicalized in an order that leaves too much captured
         with_larger_debug_stack(|| {
