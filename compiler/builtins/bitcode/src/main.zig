b3d530f2ae784b7c5a4a391a43993fc3e52099ad
Jared Ramirez <jaredramirez@hey.com>
2020-10-26 17:37:02 -0700

    Build bitcode with zig 🎉

--- compiler/builtins/bitcode/src/main.zig
+++ compiler/builtins/bitcode/src/main.zig
@@ -1,15 +1,14 @@
 const std = @import("std");
-const testing = std.testing;
 
-fn is_finite_(f: f64) bool {
+export fn is_finite_(f: f64) bool {
     return std.math.isFinite(f);
 }
 
-fn atan(f: f64) f64 {
+export fn atan_(f: f64) f64 {
     return std.math.atan(f);
 }
 
-fn pow_int_(base: i64, exp: i64) i64 {
+export fn pow_int_(base: i64, exp: i64) i64 {
     return std.math.pow(i64, base, exp);
 }
 
