80eb89739d8884708ffcbf413b5ae73c261c0e24
Richard Feldman <oss@rtfeldman.com>
2020-11-01 23:23:26 -0500

    Improve alloca helper API and docs

--- roc_std/src/alloca.rs
+++ roc_std/src/alloca.rs
@@ -13,7 +13,23 @@ extern "C" {
 /// This calls C's `alloca` function to allocate some bytes on the stack,
 /// then provides those bytes to the given callback function.
 ///
-/// Note that this function is `#[inline(always)]`, which means it will allocate
+/// It provides a `&mut c_void` to reflect the lifetime of that memory
+/// (it only lives for the duration of the callback), which you'll probably
+/// want to cast to something else. To cast `ptr` to the type you want,
+/// use `&mut *(ptr as *mut _ as *mut _)` - for example:
+///
+/// ```rust,ignore
+/// with_stack_bytes(size_of::<Foo>(), |ptr| {
+///     let foo: &mut Foo = &mut *(ptr as *mut _ as *mut _);
+/// ```
+///
+/// Yes, both `as *mut _` casts are necessary! The first one casts it to
+/// a `*mut c_void` and the second one casts it to the pointer type you want.
+/// Finally, the `&mut *` converts it from a pointer to a mutable reference.
+///
+/// # Safety
+///
+/// This function is `#[inline(always)]`, which means it will allocate
 /// the bytes on the stack of whoever calls this function.
 ///
 /// Naturally, if you give this a large number of bytes, it may cause
@@ -24,13 +40,14 @@ extern "C" {
 /// (using `free`) before the callback returns. In debug builds, this can lead
 /// to memory leaks if the callback panics - but release builds should be fine,
 /// because they only ever allocate on the stack.
+///
 #[inline(always)]
 pub unsafe fn with_stack_bytes<F, R>(bytes: usize, callback: F) -> R
 where
-    F: FnOnce(*mut c_void) -> R,
+    F: FnOnce(&mut c_void) -> R,
 {
     let ptr = malloc_or_alloca(bytes);
-    let answer = callback(ptr);
+    let answer = callback(&mut *ptr);
 
     free_or_noop(ptr);
 
@@ -84,15 +101,18 @@ mod tests {
         };
         let sum: u64 = unsafe {
             with_stack_bytes(size_of::<TestStruct>(), |ptr| {
-                let ptr = ptr as *mut TestStruct;
+                // Surprisingly, both casts are necessary; the first one
+                // turns it from &mut c_void to *mut c_void, and the second
+                // one turns it into *mut TestStruct
+                let new_ts: &mut TestStruct = &mut *(ptr as *mut _ as *mut _);
 
-                (*ptr).x = test_struct.x;
-                (*ptr).y = test_struct.y;
-                (*ptr).z = test_struct.z;
+                new_ts.x = test_struct.x;
+                new_ts.y = test_struct.y;
+                new_ts.z = test_struct.z;
 
-                assert_eq!(*ptr, test_struct);
+                assert_eq!(new_ts, &test_struct);
 
-                (*ptr).x as u64 + (*ptr).y as u64 + (*ptr).z as u64
+                new_ts.x as u64 + new_ts.y as u64 + new_ts.z as u64
             })
         };
 
