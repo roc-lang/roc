06eff10fbad2fcf206e5ce97a46b6308c7695f00
Folkert <folkert@folkertdev.nl>
2020-11-02 01:42:04 +0100

    update hello-world

--- examples/hello-world/platform/src/lib.rs
+++ examples/hello-world/platform/src/lib.rs
@@ -1,17 +1,27 @@
+use roc_std::RocCallResult;
 use roc_std::RocStr;
 use std::str;
 
 extern "C" {
     #[link_name = "main_1_exposed"]
-    fn main() -> RocStr;
+    fn say_hello(output: &mut RocCallResult<RocStr>) -> ();
 }
 
 #[no_mangle]
 pub fn rust_main() -> isize {
-    println!(
-        "Roc says: {}",
-        str::from_utf8(unsafe { main().as_slice() }).unwrap()
-    );
+    let answer = unsafe {
+        use std::mem::MaybeUninit;
+        let mut output: MaybeUninit<RocCallResult<RocStr>> = MaybeUninit::uninit();
+
+        say_hello(&mut *output.as_mut_ptr());
+
+        match output.assume_init().into() {
+            Ok(value) => value,
+            Err(msg) => panic!("roc failed with message {}", msg),
+        }
+    };
+
+    println!("Roc says: {}", str::from_utf8(answer.as_slice()).unwrap());
 
     // Exit code
     0
