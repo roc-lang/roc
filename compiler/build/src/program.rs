70bbf6b1a8b40fe506c11c88937f4c1d51ce68d5
Richard Feldman <oss@rtfeldman.com>
2020-11-08 16:54:33 -0500

    Fix a comment

--- compiler/build/src/program.rs
+++ compiler/build/src/program.rs
@@ -124,7 +124,7 @@ pub fn gen_from_mono_module(
         }
     }
 
-    // Uncomment this to see the module's optimized LLVM instruction output:
+    // Uncomment this to see the module's unoptimized LLVM instruction output:
     // env.module.print_to_stderr();
 
     mpm.run_on(module);
