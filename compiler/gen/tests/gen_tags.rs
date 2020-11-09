7e7fa4b4aac752f7bd96527d2776b06ac80c83e1
Folkert <folkert@folkertdev.nl>
2020-11-09 20:11:39 +0100

    fix tag test

--- compiler/gen/tests/gen_tags.rs
+++ compiler/gen/tests/gen_tags.rs
@@ -88,7 +88,7 @@ mod gen_tags {
     }
 
     #[test]
-    fn applied_tag_just_unit() {
+    fn applied_tag_just_enum() {
         assert_evals_to!(
             indoc!(
                 r#"
@@ -105,7 +105,7 @@ mod gen_tags {
                 "#
             ),
             (0, 2),
-            (i64, i64)
+            (i64, u8)
         );
     }
 
