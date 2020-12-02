1b6acf60bd4ab94fbfd25c20546175ff5c342e2d
Richard Feldman <oss@rtfeldman.com>
2020-12-01 20:27:34 -0500

    Check zig formatting after running tests

--- compiler/builtins/bitcode/run-tests.sh
+++ compiler/builtins/bitcode/run-tests.sh
@@ -4,3 +4,6 @@ set -euxo pipefail
 
 # Test every zig
 find src/*.zig -type f -print0 | xargs -n 1 -0 zig test --library c
+
+# fmt every zig
+find src/*.zig -type f -print0 | xargs -n 1 -0 zig fmt --check
