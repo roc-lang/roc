4cf5566456f84ba945511c10818f89cf2d513934
Folkert <folkert@folkertdev.nl>
2021-01-01 00:09:57 +0100

    we want to compare to literal float bits in gen

--- compiler/gen/src/lib.rs
+++ compiler/gen/src/lib.rs
@@ -1,6 +1,8 @@
 #![warn(clippy::all, clippy::dbg_macro)]
 // See github.com/rtfeldman/roc/issues/800 for discussion of the large_enum_variant check.
 #![allow(clippy::large_enum_variant)]
+// we actually want to compare against the literal float bits
+#![allow(clippy::clippy::float_cmp)]
 
 pub mod llvm;
 
