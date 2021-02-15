581628f46150932a61995a6454390a964800d5e5
Folkert <folkert@folkertdev.nl>
2021-02-16 00:14:48 +0100

    add gen_result test file

--- compiler/gen/tests/gen_set.rs
+++ compiler/gen/tests/gen_set.rs
@@ -13,7 +13,6 @@ mod helpers;
 
 #[cfg(test)]
 mod gen_set {
-    // use roc_std::RocStr;
 
     #[test]
     fn empty_len() {
@@ -223,101 +222,6 @@ mod gen_set {
             ),
             false,
             bool
-        );
-    }
-
-    #[test]
-    fn with_default() {
-        assert_evals_to!(
-            indoc!(
-                r#"
-                result : Result I64 {}
-                result = Ok 2
-
-                Result.withDefault result 0
-                "#
-            ),
-            2,
-            i64
-        );
-
-        assert_evals_to!(
-            indoc!(
-                r#"
-                result : Result I64 {}
-                result = Err {} 
-
-                Result.withDefault result 0
-                "#
-            ),
-            0,
-            i64
-        );
-    }
-
-    #[test]
-    fn result_map() {
-        assert_evals_to!(
-            indoc!(
-                r#"
-                result : Result I64 {}
-                result = Ok 2
-
-                result
-                    |> Result.map (\x -> x + 1)
-                    |> Result.withDefault 0
-                "#
-            ),
-            3,
-            i64
-        );
-
-        assert_evals_to!(
-            indoc!(
-                r#"
-                result : Result I64 {}
-                result = Err {} 
-
-                result
-                    |> Result.map (\x -> x + 1)
-                    |> Result.withDefault 0
-                "#
-            ),
-            0,
-            i64
-        );
-    }
-
-    #[test]
-    fn result_map_err() {
-        assert_evals_to!(
-            indoc!(
-                r#"
-                result : Result {} I64
-                result = Err 2
-
-                when Result.mapErr result (\x -> x + 1) is
-                    Err n -> n
-                    Ok _ -> 0
-                "#
-            ),
-            3,
-            i64
-        );
-
-        assert_evals_to!(
-            indoc!(
-                r#"
-                result : Result {} I64
-                result = Ok {}
-
-                when Result.mapErr result (\x -> x + 1) is
-                    Err n -> n
-                    Ok _ -> 0
-                "#
-            ),
-            0,
-            i64
         );
     }
 }
