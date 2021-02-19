2bbd569ba181bcc1ca1a785217c243d2dee5395e
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2021-02-19 18:37:14 +0100

    fixed comment position

--- editor/src/editor/mod.rs
+++ editor/src/editor/mod.rs
@@ -4,5 +4,5 @@ mod keyboard_input;
 pub mod main;
 mod mvc;
 mod render_ast;
-pub mod syntax_highlight;
-mod util; // TODO remove pub once we have ast based syntax highlighting
+pub mod syntax_highlight; // TODO remove pub once we have ast based syntax highlighting
+mod util;
