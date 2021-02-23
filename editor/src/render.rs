004474f3fc3fbf66d1ad6e7dd82553d577ba06e9
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2021-02-23 10:07:00 +0100

    finished conversion to use arena

--- editor/src/render.rs
+++ editor/src/render.rs
@@ -80,25 +80,31 @@ fn expr2_to_len<'a>(env: &Env<'a>, expr2: &Expr2) -> usize {
     }
 }
 
-pub fn expr2_to_str<'a>(arena: &'a Bump, env: &'a Env<'a>, expr2: &Expr2) -> &'a str {
+fn get_bump_str<'a, 'b>(arena: &'a Bump, env: &Env<'b>, pool_str: &PoolStr) -> BumpString<'a> {
+    let env_str = env.pool.get_str(pool_str);
+
+    BumpString::from_str_in(env_str, arena)
+}
+
+pub fn expr2_to_str<'a, 'b>(arena: &'a Bump, env: &Env<'b>, expr2: &Expr2) -> BumpString<'a> {
 
     match expr2 {
-        Expr2::SmallInt { text, .. } => env.pool.get_str(text),
-        Expr2::I128 { text, .. } => env.pool.get_str(text),
-        Expr2::U128 { text, .. } => env.pool.get_str(text),
-        Expr2::Float { text, .. } => env.pool.get_str(text),
-        Expr2::Str(text) => env.pool.get_str(text),
-        Expr2::GlobalTag { name, .. } => env.pool.get_str(name),
+        Expr2::SmallInt { text, .. } => get_bump_str(arena, env, text),
+        Expr2::I128 { text, .. } => get_bump_str(arena, env, text),
+        Expr2::U128 { text, .. } => get_bump_str(arena, env, text),
+        Expr2::Float { text, .. } => get_bump_str(arena, env, text),
+        Expr2::Str(text) => get_bump_str(arena, env, text),
+        Expr2::GlobalTag { name, .. } => get_bump_str(arena, env, name),
         Expr2::Call { expr: expr_id, .. } => {
             let expr = env.pool.get(*expr_id);
 
             expr2_to_str(arena, env, expr)
         }
         Expr2::Var(symbol) => {
-            //TODO make bump_format to use arena
+            //TODO make bump_format with arena
             let text = format!("{:?}", symbol);
 
-            &text
+            BumpString::from_str_in(&text, arena)
         }
         Expr2::List { elems, .. } => {
             let mut bump_str =
@@ -118,7 +124,7 @@ pub fn expr2_to_str<'a>(arena: &'a Bump, env: &'a Env<'a>, expr2: &Expr2) -> &'a
 
             bump_str.push(']');
 
-            &bump_str
+            bump_str
         }
         Expr2::Record { fields, .. } => {
             let mut bump_str =
@@ -134,7 +140,7 @@ pub fn expr2_to_str<'a>(arena: &'a Bump, env: &'a Env<'a>, expr2: &Expr2) -> &'a
 
                 bump_str.push_str(field_name);
                 bump_str.push(':');
-                bump_str.push_str(expr2_to_str(arena, env, sub_expr2));
+                bump_str.push_str(&expr2_to_str(arena, env, sub_expr2));
 
                 if idx + 1 < fields.len() {
                     bump_str.push_str(", ")
@@ -143,7 +149,7 @@ pub fn expr2_to_str<'a>(arena: &'a Bump, env: &'a Env<'a>, expr2: &Expr2) -> &'a
 
             bump_str.push('}');
 
-            &bump_str
+            bump_str
         }
         rest => todo!("implement expr2_to_str for {:?}", rest),
     }
