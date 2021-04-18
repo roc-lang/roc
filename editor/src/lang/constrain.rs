7ffe44d33635416f6d2e789832aae7289689f0f3
rvcas <x@rvcas.dev>
2021-04-17 20:34:59 -0400

    feat: very basic and incorrect SmallInt constraint

--- editor/src/lang/constrain.rs
+++ editor/src/lang/constrain.rs
@@ -1,10 +1,14 @@
-use crate::lang::pool::{Pool, PoolVec};
+use crate::lang::pool::{NodeId, Pool, PoolVec};
 use crate::lang::{ast::Expr2, expr::Env, types::Type2};
 
 use roc_can::expected::Expected;
+use roc_collections::all::SendMap;
 use roc_module::symbol::Symbol;
-use roc_region::all::Region;
-use roc_types::types::Category;
+use roc_region::all::{Located, Region};
+use roc_types::{
+    subs::Variable,
+    types::{Category, Reason},
+};
 
 #[derive(Debug)]
 pub enum Constraint {
@@ -12,10 +16,19 @@ pub enum Constraint {
     // Store(Type, Variable, &'static str, u32),
     // Lookup(Symbol, Expected<Type>, Region),
     // Pattern(Region, PatternCategory, Type, PExpected<Type>),
+    And(PoolVec<Constraint>),
+    Let(NodeId<LetConstraint>),
+    // SaveTheEnvironment,
     True, // Used for things that always unify, e.g. blanks and runtime errors
-          // SaveTheEnvironment,
-          // Let(Box<LetConstraint>),
-          // And(Vec<Constraint>),
+}
+
+#[derive(Debug)]
+pub struct LetConstraint {
+    pub rigid_vars: PoolVec<Variable>,
+    pub flex_vars: PoolVec<Variable>,
+    pub def_types: SendMap<Symbol, Located<Type2>>,
+    pub defs_constraint: Constraint,
+    pub ret_constraint: Constraint,
 }
 
 pub fn constrain_expr(
@@ -27,8 +40,50 @@ pub fn constrain_expr(
     use Constraint::*;
 
     match expr {
-        Expr2::EmptyRecord => Eq(Type2::EmptyRec, expected, Category::Record, region),
         Expr2::Str(_) => Eq(str_type(env.pool), expected, Category::Str, region),
+        Expr2::EmptyRecord => Eq(Type2::EmptyRec, expected, Category::Record, region),
+        Expr2::SmallInt { number, var, .. } => {
+            let flex_vars = PoolVec::with_capacity(1, env.pool);
+            let rigid_vars = PoolVec::empty(env.pool);
+
+            for flex_var_node_id in flex_vars.iter_node_ids() {
+                env.pool[flex_var_node_id] = *var;
+            }
+
+            let num_type = Type2::Variable(*var);
+
+            let and_nodes = vec![
+                Eq(
+                    num_type,
+                    Expected::ForReason(
+                        Reason::IntLiteral,
+                        Type2::Alias(
+                            Symbol::NUM_INT,
+                            PoolVec::empty(env.pool),
+                            env.pool.add(Type2::EmptyRec),
+                        ),
+                        region,
+                    ),
+                    Category::Int,
+                    region,
+                ),
+                Eq(Type2::Variable(*var), expected, Category::Int, region),
+            ];
+
+            let and_constraints = PoolVec::new(and_nodes.into_iter(), env.pool);
+
+            let defs_constraint = And(and_constraints);
+
+            let node_id = env.pool.add(LetConstraint {
+                rigid_vars,
+                flex_vars,
+                def_types: SendMap::default(),
+                defs_constraint,
+                ret_constraint: Constraint::True,
+            });
+
+            Let(node_id)
+        }
         _ => todo!("implement constaints for {:?}", expr),
     }
 }
