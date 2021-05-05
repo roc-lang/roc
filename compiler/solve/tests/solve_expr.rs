13dfa79c5d5b21d64e5670f01d78e2119d07fab9
Folkert <folkert@folkertdev.nl>
2021-05-05 15:14:30 +0200

    re-enable rigid test

--- compiler/solve/tests/solve_expr.rs
+++ compiler/solve/tests/solve_expr.rs
@@ -4370,12 +4370,11 @@ mod solve_expr {
     }
 
     #[test]
-    #[ignore]
     fn pattern_rigid_problem() {
         infer_eq_without_problem(
             indoc!(
                 r#"
-                app Test provides [ main ] imports []
+                app "test" provides [ main ] to "./platform"
 
                 RBTree k : [ Node k (RBTree k) (RBTree k), Empty ]
 
