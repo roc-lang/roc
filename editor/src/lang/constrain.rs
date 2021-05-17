eb6e4a21f36aa92e82f10c3050689f92f2a1c630
rvcas <x@rvcas.dev>
2021-05-17 19:45:11 -0400

    feat(Expr2): port over Access case for constraints

--- editor/src/lang/constrain.rs
+++ editor/src/lang/constrain.rs
@@ -431,6 +431,54 @@ pub fn constrain_expr<'a>(
 
             exists(arena, flex_vars, And(and_constraints))
         }
+        Expr2::Access {
+            expr: expr_id,
+            field,
+            field_var,
+            record_var,
+            ext_var,
+        } => {
+            let ext_type = Type2::Variable(*ext_var);
+
+            let field_type = Type2::Variable(*field_var);
+
+            let record_field =
+                types::RecordField::Demanded(env.pool.add(field_type.shallow_clone()));
+
+            let record_type = Type2::Record(
+                PoolVec::new(vec![(*field, record_field)].into_iter(), env.pool),
+                env.pool.add(ext_type),
+            );
+
+            let record_expected = Expected::NoExpectation(record_type);
+
+            let category = Category::Access(field.as_str(env.pool).into());
+
+            let record_con = Eq(
+                Type2::Variable(*record_var),
+                record_expected.shallow_clone(),
+                category.clone(),
+                region,
+            );
+
+            let access_expr = env.pool.get(*expr_id);
+
+            let constraint = constrain_expr(arena, env, access_expr, record_expected, region);
+
+            let mut flex_vars = BumpVec::with_capacity_in(3, arena);
+
+            flex_vars.push(*record_var);
+            flex_vars.push(*field_var);
+            flex_vars.push(*ext_var);
+
+            let mut and_constraints = BumpVec::with_capacity_in(3, arena);
+
+            and_constraints.push(constraint);
+            and_constraints.push(Eq(field_type, expected, category, region));
+            and_constraints.push(record_con);
+
+            exists(arena, flex_vars, And(and_constraints))
+        }
         _ => todo!("implement constaints for {:?}", expr),
     }
 }
