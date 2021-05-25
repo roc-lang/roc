76cc393520c9d3f8b39d79766908354584402fc7
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2021-05-25 18:55:06 +0200

    earthly conf file, return correct exit code

--- ci/safe-earthly-test-all.sh
+++ ci/safe-earthly-test-all.sh
@@ -3,7 +3,8 @@
 LOG_FILE="earthly_log.txt"
 touch $LOG_FILE
 
-script -efq $LOG_FILE -c "earthly --buildkit-cache-size-mb 25000 +test-all"
+script -efq $LOG_FILE -c "earthly --config ci/earthly-conf.yml +test-all"
+EXIT_CODE=$?
 
 if grep -q "failed to mount" "$LOG_FILE"; then
   echo ""
@@ -13,7 +14,7 @@ if grep -q "failed to mount" "$LOG_FILE"; then
   echo "------<<<<<<!!!!!!>>>>>>------"
   echo ""
   echo ""
-  earthly --no-cache --buildkit-cache-size-mb 25000 +test-all
+  earthly --config ci/earthly-conf.yml --no-cache +test-all
 else
-  return 1
+  exit $EXIT_CODE
 fi
