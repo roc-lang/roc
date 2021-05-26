cbde3215385ded2332263bbfbf6e20218c70b548
Richard Feldman <oss@rtfeldman.com>
2021-05-25 21:20:07 -0400

    Run all examples in cli_run

--- cli/tests/cli_run.rs
+++ cli/tests/cli_run.rs
@@ -9,13 +9,16 @@ extern crate roc_module;
 
 mod helpers;
 
+#[macro_use]
+extern crate maplit;
+
 #[cfg(test)]
 mod cli_run {
     use crate::helpers::{
-        example_file, extract_valgrind_errors, fixture_file, run_cmd, run_roc, run_with_valgrind,
-        ValgrindError, ValgrindErrorXWhat,
+        example_file, extract_valgrind_errors, run_cmd, run_roc, run_with_valgrind, ValgrindError,
+        ValgrindErrorXWhat,
     };
-    use serial_test::serial;
+    use std::fs;
     use std::path::Path;
 
     #[cfg(not(target_os = "macos"))]
@@ -27,26 +30,18 @@ mod cli_run {
     #[cfg(target_os = "macos")]
     const ALLOW_VALGRIND: bool = false;
 
-    fn check_output(
-        file: &Path,
-        executable_filename: &str,
-        flags: &[&str],
-        expected_ending: &str,
+    #[derive(Debug, PartialEq, Eq)]
+    struct Example<'a> {
+        filename: &'a str,
+        executable_filename: &'a str,
+        stdin: &'a [&'a str],
+        expected_ending: &'a str,
         use_valgrind: bool,
-    ) {
-        check_output_with_stdin(
-            file,
-            "",
-            executable_filename,
-            flags,
-            expected_ending,
-            use_valgrind,
-        )
     }
 
     fn check_output_with_stdin(
         file: &Path,
-        stdin_str: &str,
+        stdin: &[&str],
         executable_filename: &str,
         flags: &[&str],
         expected_ending: &str,
@@ -61,7 +56,7 @@ mod cli_run {
 
         let out = if use_valgrind && ALLOW_VALGRIND {
             let (valgrind_out, raw_xml) = run_with_valgrind(
-                stdin_str,
+                stdin,
                 &[file.with_file_name(executable_filename).to_str().unwrap()],
             );
 
@@ -103,7 +98,7 @@ mod cli_run {
         } else {
             run_cmd(
                 file.with_file_name(executable_filename).to_str().unwrap(),
-                stdin_str,
+                stdin,
                 &[],
             )
         };
@@ -117,218 +112,196 @@ mod cli_run {
     }
 
     #[test]
-    #[serial(hello_world)]
-    fn run_hello_world() {
-        check_output(
-            &example_file("hello-world", "Hello.roc"),
-            "hello-world",
-            &[],
-            "Hello, World!!!!!!!!!!!!!\n",
-            true,
-        );
-    }
+    fn check_all_examples() {
+        let mut all_examples = hashmap! {
+            "hello-world" => vec![
+                Example {
+                    filename: "Hello.roc",
+                    executable_filename: "hello-world",
+                    stdin: &[],
+                    expected_ending:"Hello, World!!!!!!!!!!!!!\n",
+                    use_valgrind: true,
+                }
+            ],
+            "quicksort" => vec![
+                Example {
+                    filename: "Quicksort.roc",
+                    executable_filename: "quicksort",
+                    stdin: &[],
+                    expected_ending: "[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2]\n",
+                    use_valgrind: true,
+                }
+            ],
+            "shared-quicksort" => vec![
+                Example {
+                    filename: "Quicksort.roc",
+                    executable_filename: "quicksort",
+                    stdin: &[],
+                    expected_ending: "[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2]\n",
+                    use_valgrind: true,
+                }
+            ],
+            "effect" => vec![
+                Example {
+                    filename: "Main.roc",
+                    executable_filename: "effect-example",
+                    stdin: &["hi there!"],
+                    expected_ending: "hi there!\n",
+                    use_valgrind: true,
+                }
+            ],
+            "multi-dep-str" => vec![
+                Example {
+                    filename: "Main.roc",
+                    executable_filename: "multi-dep-str",
+                    stdin: &[],
+                    expected_ending: "I am Dep2.str2\n",
+                    use_valgrind: true,
+                }
+            ],
+            "multi-dep-thunk" => vec![
+                Example {
+                    filename: "Main.roc",
+                    executable_filename: "multi-dep-thunk",
+                    stdin: &[],
+                    expected_ending: "I am Dep2.value2\n",
+                    use_valgrind: true,
+                }
+            ],
+            "tea" => vec![
+                Example {
+                    filename: "Main.roc",
+                    executable_filename: "tea-example",
+                    stdin: &[],
+                    expected_ending: "",
+                    use_valgrind: true,
+                }
+            ],
+            "cli" => vec![
+                Example {
+                    filename: "Echo.roc",
+                    executable_filename: "echo",
+                    stdin: &["Giovanni\n", "Giorgio\n"],
+                    expected_ending: "Giovanni Giorgio!\n",
+                    use_valgrind: true,
+                }
+            ],
+            "custom-malloc" => vec![
+                Example {
+                    filename: "Main.roc",
+                    executable_filename: "custom-malloc-example",
+                    stdin: &[],
+                    expected_ending: "ms!\nThe list was small!\n",
+                    use_valgrind: true,
+                }
+            ],
+            "task" => vec![
+                Example {
+                    filename: "Main.roc",
+                    executable_filename: "task-example",
+                    stdin: &[],
+                    expected_ending: "successfully wrote to file\n",
+                    use_valgrind: true,
+                }
+            ],
+            "benchmarks" => vec![
+                Example {
+                    filename: "NQueens.roc",
+                    executable_filename: "nqueens",
+                    stdin: &[],
+                    expected_ending: "4\n",
+                    use_valgrind: true,
+                },
+                Example {
+                    filename: "CFold.roc",
+                    executable_filename: "cfold",
+                    stdin: &[],
+                    expected_ending: "11 & 11\n",
+                    use_valgrind: true,
+                },
+                Example {
+                    filename: "Deriv.roc",
+                    executable_filename: "deriv",
+                    stdin: &[],
+                    expected_ending: "1 count: 6\n2 count: 22\n",
+                    use_valgrind: true,
+                },
+                Example {
+                    filename: "RBTreeInsert.roc",
+                    executable_filename: "rbtree-insert",
+                    stdin: &[],
+                    expected_ending: "Node Black 0 {} Empty Empty\n",
+                    use_valgrind: true,
+                },
+                Example {
+                    filename: "RBTreeDel.roc",
+                    executable_filename: "rbtree-del",
+                    stdin: &[],
+                    expected_ending: "30\n",
+                    use_valgrind: true,
+                },
+                Example {
+                    filename: "TestAStar.roc",
+                    executable_filename: "test-astar",
+                    stdin: &[],
+                    expected_ending: "True\n",
+                    use_valgrind: false,
+                },
+                Example {
+                    filename: "TestBase64.roc",
+                    executable_filename: "test-base64",
+                    stdin: &[],
+                    expected_ending: "encoded: SGVsbG8gV29ybGQ=\ndecoded: Hello World\n",
+                    use_valgrind: true,
+                },
+                Example {
+                    filename: "Closure.roc",
+                    executable_filename: "closure",
+                    stdin: &[],
+                    expected_ending: "",
+                    use_valgrind: true,
+                }
+            ],
+        };
 
-    #[test]
-    #[serial(hello_world)]
-    fn run_hello_world_optimized() {
-        check_output(
-            &example_file("hello-world", "Hello.roc"),
-            "hello-world",
-            &[],
-            "Hello, World!!!!!!!!!!!!!\n",
-            true,
-        );
-    }
+        for entry in fs::read_dir("../examples").unwrap() {
+            let entry = entry.unwrap();
 
-    #[test]
-    #[serial(quicksort)]
-    fn run_quicksort_not_optimized() {
-        check_output(
-            &example_file("quicksort", "Quicksort.roc"),
-            "quicksort",
-            &[],
-            "[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2]\n",
-            true,
-        );
-    }
+            if entry.file_type().unwrap().is_dir() {
+                let dir_name = entry.file_name().into_string().unwrap();
+                let examples = all_examples.remove(dir_name.as_str()).unwrap_or(Vec::new());
 
-    #[test]
-    #[serial(quicksort)]
-    fn run_quicksort_optimized() {
-        check_output(
-            &example_file("quicksort", "Quicksort.roc"),
-            "quicksort",
-            &["--optimize"],
-            "[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2]\n",
-            true,
-        );
-    }
+                if examples.is_empty() {
+                    panic!("The directory examples/{} does not have any corresponding tests in cli_run. Please add one, so if it ever stops compiling, we'll know about it right away!", dir_name);
+                }
 
-    #[test]
-    #[serial(quicksort)]
-    fn run_quicksort_optimized_valgrind() {
-        check_output(
-            &example_file("quicksort", "Quicksort.roc"),
-            "quicksort",
-            &["--optimize"],
-            "[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2]\n",
-            true,
-        );
-    }
+                for example in examples {
+                    println!(
+                        "Running {:?}...",
+                        &example_file(dir_name.as_str(), example.filename),
+                    );
+                    // Check with and without optimizations
+                    check_output_with_stdin(
+                        &example_file(dir_name.as_str(), example.filename),
+                        example.stdin,
+                        example.executable_filename,
+                        &[],
+                        example.expected_ending,
+                        example.use_valgrind,
+                    );
 
-    #[test]
-    #[serial(nqueens)]
-    fn run_nqueens_not_optimized() {
-        check_output_with_stdin(
-            &example_file("benchmarks", "NQueens.roc"),
-            "",
-            "nqueens",
-            &[],
-            "4\n",
-            true,
-        );
-    }
+                    check_output_with_stdin(
+                        &example_file(dir_name.as_str(), example.filename),
+                        example.stdin,
+                        example.executable_filename,
+                        &["--optimize"],
+                        example.expected_ending,
+                        example.use_valgrind,
+                    );
+                }
+            }
+        }
 
-    #[test]
-    #[serial(cfold)]
-    fn run_cfold_not_optimized() {
-        check_output(
-            &example_file("benchmarks", "CFold.roc"),
-            "cfold",
-            &[],
-            "11 & 11\n",
-            true,
-        );
-    }
-
-    #[test]
-    #[serial(deriv)]
-    fn run_deriv_not_optimized() {
-        check_output(
-            &example_file("benchmarks", "Deriv.roc"),
-            "deriv",
-            &[],
-            "1 count: 6\n2 count: 22\n",
-            true,
-        );
-    }
-
-    #[test]
-    #[serial(deriv)]
-    fn run_rbtree_insert_not_optimized() {
-        check_output(
-            &example_file("benchmarks", "RBTreeInsert.roc"),
-            "rbtree-insert",
-            &[],
-            "Node Black 0 {} Empty Empty\n",
-            true,
-        );
-    }
-
-    #[test]
-    #[serial(deriv)]
-    fn run_rbtree_delete_not_optimized() {
-        check_output(
-            &example_file("benchmarks", "RBTreeDel.roc"),
-            "rbtree-del",
-            &[],
-            "30\n",
-            true,
-        );
-    }
-
-    #[test]
-    #[serial(astar)]
-    fn run_astar_optimized_1() {
-        check_output(
-            &example_file("benchmarks", "TestAStar.roc"),
-            "test-astar",
-            &[],
-            "True\n",
-            false,
-        );
-    }
-
-    #[test]
-    #[serial(base64)]
-    fn base64() {
-        check_output(
-            &example_file("benchmarks", "TestBase64.roc"),
-            "test-base64",
-            &[],
-            "encoded: SGVsbG8gV29ybGQ=\ndecoded: Hello World\n",
-            true,
-        );
-    }
-
-    #[test]
-    #[serial(closure)]
-    fn closure() {
-        check_output(
-            &example_file("benchmarks", "Closure.roc"),
-            "closure",
-            &[],
-            "",
-            true,
-        );
-    }
-
-    //    #[test]
-    //    #[serial(effect)]
-    //    fn run_effect_unoptimized() {
-    //        check_output(
-    //            &example_file("effect", "Main.roc"),
-    //            &[],
-    //            "I am Dep2.str2\n",
-    //            true,
-    //        );
-    //    }
-
-    #[test]
-    #[serial(multi_dep_str)]
-    fn run_multi_dep_str_unoptimized() {
-        check_output(
-            &fixture_file("multi-dep-str", "Main.roc"),
-            "multi-dep-str",
-            &[],
-            "I am Dep2.str2\n",
-            true,
-        );
-    }
-
-    #[test]
-    #[serial(multi_dep_str)]
-    fn run_multi_dep_str_optimized() {
-        check_output(
-            &fixture_file("multi-dep-str", "Main.roc"),
-            "multi-dep-str",
-            &["--optimize"],
-            "I am Dep2.str2\n",
-            true,
-        );
-    }
-
-    #[test]
-    #[serial(multi_dep_thunk)]
-    fn run_multi_dep_thunk_unoptimized() {
-        check_output(
-            &fixture_file("multi-dep-thunk", "Main.roc"),
-            "multi-dep-thunk",
-            &[],
-            "I am Dep2.value2\n",
-            true,
-        );
-    }
-
-    #[test]
-    #[serial(multi_dep_thunk)]
-    fn run_multi_dep_thunk_optimized() {
-        check_output(
-            &fixture_file("multi-dep-thunk", "Main.roc"),
-            "multi-dep-thunk",
-            &["--optimize"],
-            "I am Dep2.value2\n",
-            true,
-        );
+        assert_eq!(all_examples, std::collections::HashMap::default());
     }
 }
