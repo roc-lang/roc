966bb80cdcaabdb7b75f4324e9dc7dd747c369ca
Jared Ramirez <jaredramirez@hey.com>
2021-05-31 15:40:26 -0700

    Format & rename some Nix stuff; Remove custom .envrc use nix stuff in favor of stdlib

--- nix/zig.nix
+++ nix/zig.nix
@@ -1,37 +1,37 @@
-{ pkgs, isMacOS, isAarch64 }:
+{ pkgs, isDarwin, isAarch64 }:
 
-if isMacOS then
+if isDarwin then
   let
     version = "0.7.1";
     osName =
-      if isMacOS
-        then "macos"
-        else "linux";
+      if isDarwin
+      then "macos"
+      else "linux";
     archName =
       if isAarch64
-        then "aarch64"
-        else "x86_64";
+      then "aarch64"
+      else "x86_64";
     archiveName = "zig-${osName}-${archName}-${version}";
     sha256 = "845cb17562978af0cf67e3993f4e33330525eaf01ead9386df9105111e3bc519";
   in
-  pkgs.stdenv.mkDerivation {
-    pname = "zig";
-    version = version;
-    buildInputs = [ pkgs.gzip ];
-    src = pkgs.fetchurl {
-      inherit sha256;
-      name = "${archiveName}.tar.xz";
-      url = "https://ziglang.org/download/${version}/${archiveName}.tar.xz";
-    };
-    phases = [ "installPhase" ];
-    installPhase = ''
-      mkdir -p $out/bin
-      tar -xf $src
-      cp ${archiveName}/zig $out/zig
-      cp -r ${archiveName}/lib $out/lib
-      ln -s "$out/zig" "$out/bin/zig"
-      chmod +x $out/bin/zig
-    '';
-  }
+    pkgs.stdenv.mkDerivation {
+      pname = "zig";
+      version = version;
+      buildInputs = [ pkgs.gzip ];
+      src = pkgs.fetchurl {
+        inherit sha256;
+        name = "${archiveName}.tar.xz";
+        url = "https://ziglang.org/download/${version}/${archiveName}.tar.xz";
+      };
+      phases = [ "unpackPhase" ];
+      unpackPhase = ''
+        mkdir -p $out/bin
+        tar -xf $src
+        cp ${archiveName}/zig $out/zig
+        cp -r ${archiveName}/lib $out/lib
+        ln -s "$out/zig" "$out/bin/zig"
+        chmod +x $out/bin/zig
+      '';
+    }
 else
   pkgs.zig
--- shell.nix
+++ shell.nix
@@ -1,116 +1,113 @@
 {}:
 
 let
-  splitSystem = builtins.split "-" builtins.currentSystem;
-  currentArch = builtins.elemAt splitSystem 0;
-  currentOS = builtins.elemAt splitSystem 2;
-in
-  with {
-    # Look here for information about how pin version of nixpkgs
-    #  → https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs
-    pkgs = import (
-      builtins.fetchGit {
-        # name = "nixpkgs-2021-04-23";
-        url = "https://github.com/nixos/nixpkgs/";
-        ref = "refs/heads/nixpkgs-unstable";
-        rev = "8d0340aee5caac3807c58ad7fa4ebdbbdd9134d6";
-      }
-    ) {};
+  # Look here for information about how pin version of nixpkgs
+  #  → https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs
+  # TODO: We should probably use flakes at somepoint
+  pkgs = import (
+    builtins.fetchGit {
+      # name = "nixpkgs-2021-04-23";
+      url = "https://github.com/nixos/nixpkgs/";
+      ref = "refs/heads/nixpkgs-unstable";
+      rev = "8d0340aee5caac3807c58ad7fa4ebdbbdd9134d6";
+    }
+  ) {};
 
-    isMacOS = currentOS == "darwin";
-    isLinux = currentOS == "linux";
-    isAarch64 = currentArch == "aarch64";
-  };
+  currentArch = builtins.elemAt (builtins.split "-" builtins.currentSystem) 0;
+  isAarch64 = currentArch == "aarch64";
 
-  with (pkgs);
+  darwinInputs =
+    with pkgs;
+    lib.optionals stdenv.isDarwin (
+      with pkgs.darwin.apple_sdk.frameworks; [
+        AppKit
+        CoreFoundation
+        CoreServices
+        CoreVideo
+        Foundation
+        Metal
+        Security
+      ]
+    );
 
-  let
-    darwin-inputs =
-      if isMacOS then
-        with pkgs.darwin.apple_sdk.frameworks; [
-          AppKit
-          CoreFoundation
-          CoreServices
-          CoreVideo
-          Foundation
-          Metal
-          Security
-        ]
-      else
-        [];
-
-    linux-inputs =
-      if isLinux then
-        [
-          valgrind
-          vulkan-headers
-          vulkan-loader
-          vulkan-tools
-          vulkan-validation-layers
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXrandr
-          xorg.libXi
-          xorg.libxcb
-        ]
-      else
-        [];
-
-    llvmPkgs = pkgs.llvmPackages_10;
-    zig = import ./nix/zig.nix { inherit pkgs isMacOS isAarch64; };
-    inputs = [
-      # build libraries
-      rustc
-      cargo
-      clippy
-      rustfmt
-      cmake
-      git
-      python3
-      llvmPkgs.llvm
-      llvmPkgs.clang
-      pkg-config
-      zig
-      # lib deps
-      llvmPkgs.libcxx
-      llvmPkgs.libcxxabi
-      libffi
-      libunwind
-      libxml2
-      ncurses
-      zlib
-      libiconv
-      # faster builds - see https://github.com/rtfeldman/roc/blob/trunk/BUILDING_FROM_SOURCE.md#use-lld-for-the-linker
-      llvmPkgs.lld
-      # dev tools
-      # rust-analyzer
-      # (import ./nix/zls.nix { inherit pkgs zig; })
+  linuxInputs =
+    with pkgs;
+    lib.optionals stdenv.isLinux [
+      valgrind
+      vulkan-headers
+      vulkan-loader
+      vulkan-tools
+      vulkan-validation-layers
+      xorg.libX11
+      xorg.libXcursor
+      xorg.libXrandr
+      xorg.libXi
+      xorg.libxcb
     ];
 
-  in
-    mkShell (
-      {
-        buildInputs = inputs ++ darwin-inputs ++ linux-inputs;
+  llvmPkgs = pkgs.llvmPackages_10;
 
-        # Additional Env vars
-        LLVM_SYS_100_PREFIX = "${llvmPkgs.llvm}";
-        LD_LIBRARY_PATH = stdenv.lib.makeLibraryPath
-          (
-            [
-              pkg-config
-              stdenv.cc.cc.lib
-              llvmPkgs.libcxx
-              llvmPkgs.libcxxabi
-              libunwind
-              libffi
-              ncurses
-              zlib
-            ] ++ linux-inputs
-          );
+  zig = import ./nix/zig.nix {
+    pkgs = pkgs;
+    isDarwin = pkgs.stdenv.isDarwin;
+    isAarch64 = isAarch64;
+  };
 
-        # Aliases don't work cross shell, so we do this
-        shellHook = ''
-          export PATH="$PATH:$PWD/nix/bin"
-        '';
-      }
-    )
+  inputs = with pkgs;[
+    # build libraries
+    rustc
+    cargo
+    clippy
+    rustfmt
+    cmake
+    git
+    python3
+    llvmPkgs.llvm
+    llvmPkgs.clang
+    pkg-config
+    zig
+
+    # lib deps
+    llvmPkgs.libcxx
+    llvmPkgs.libcxxabi
+    libffi
+    libunwind
+    libxml2
+    ncurses
+    zlib
+    libiconv
+
+    # faster builds - see https://github.com/rtfeldman/roc/blob/trunk/BUILDING_FROM_SOURCE.md#use-lld-for-the-linker
+    llvmPkgs.lld
+  ];
+in
+pkgs.mkShell
+  {
+    buildInputs = inputs ++ darwinInputs ++ linuxInputs;
+
+    # Additional Env vars
+    LLVM_SYS_100_PREFIX = "${llvmPkgs.llvm}";
+    LD_LIBRARY_PATH =
+      with pkgs;
+      lib.makeLibraryPath
+        (
+          [
+            pkg-config
+            stdenv.cc.cc.lib
+            llvmPkgs.libcxx
+            llvmPkgs.libcxxabi
+            libunwind
+            libffi
+            ncurses
+            zlib
+          ]
+          ++ linuxInputs
+        );
+
+    # Non Nix llvm installs names the bin llvm-as-${version}, so we
+    # alias `llvm` to `llvm-as-${version}` here.
+    # This the name of the file in nix/bin will need to be updated whenever llvm is updated
+    shellHook = ''
+      export PATH="$PATH:$PWD/nix/bin"
+    '';
+  }
