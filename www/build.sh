cb7ee34f21ebc7274bc9d67da8f3d289a64d454c
Richard Feldman <oss@rtfeldman.com>
2021-06-05 19:38:07 -0400

    Generate builtins when buiding www site

--- www/build.sh
+++ www/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 rm -rf build/
 cp -r public/ build/
+
+pushd ..
+echo 'Generating docs...'
+cargo run docs compiler/builtins/docs/Bool.roc
+mv generated-docs/ www/build/builtins
+popd
