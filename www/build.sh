e09696113c5a768d8c15902a73077d713eecfd8e
Richard Feldman <oss@rtfeldman.com>
2021-06-05 22:20:32 -0400

    Allow running bash www/build.sh

--- www/build.sh
+++ www/build.sh
@@ -2,6 +2,12 @@
 
 set -euxo pipefail
 
+# cd into the directory where this script lives.
+# This allows us to run this script from the root project directory,
+# which is what Netlify wants to do.
+SCRIPT_RELATIVE_DIR=$(dirname "${BASH_SOURCE[0]}")
+cd $SCRIPT_RELATIVE_DIR
+
 rm -rf build/
 cp -r public/ build/
 
