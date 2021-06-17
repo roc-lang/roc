fe9a2c1fc274e505810e47f744823b319eebed51
Richard Feldman <oss@rtfeldman.com>
2021-06-16 23:53:04 -0400

    Average zig benchmarks

--- compiler/builtins/bitcode/benchmark/dec.zig
+++ compiler/builtins/bitcode/benchmark/dec.zig
@@ -11,17 +11,46 @@ pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     timer = try Timer.start();
 
-    const add7_ns = add7();
-    try stdout.print("7 additions took {}ns\n", .{add7_ns});
+    try stdout.print("7 additions took ", .{});
+    try avg_runs(add7);
 
-    const sub7_ns = sub7();
-    try stdout.print("7 subtractions took {}ns\n", .{sub7_ns});
+    try stdout.print("7 subtractions took ", .{});
+    try avg_runs(sub7);
 
-    const mul7_ns = mul7();
-    try stdout.print("7 multiplications took {}ns\n", .{mul7_ns});
+    try stdout.print("7 multiplications took ", .{});
+    try avg_runs(mul7);
 
-    const div7_ns = div7();
-    try stdout.print("7 divisions took {}ns\n", .{div7_ns});
+    try stdout.print("7 divisions took ", .{});
+    try avg_runs(div7);
+}
+
+fn avg_runs(func: fn() u64) !void {
+    const stdout = std.io.getStdOut().writer();
+    var first_run = func();
+    var lowest = first_run;
+    var highest = first_run;
+    var sum = first_run;
+
+    // 31 runs
+    var runs = [_]u64{ first_run, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    var next_run: usize = 1; // we already did first_run
+
+    while (next_run < runs.len) {
+        const run = func();
+
+        lowest = std.math.min(lowest, run);
+        highest = std.math.max(highest, run);
+
+        runs[next_run] = run;
+
+        next_run += 1;
+    }
+
+    std.sort.sort(u64, &runs, {}, comptime std.sort.asc(u64));
+
+    const median = runs[runs.len / 2];
+
+    try stdout.print("{}ns (lowest: {}ns, highest: {}ns)\n", .{median, lowest, highest});
 }
 
 fn add7() u64 {
