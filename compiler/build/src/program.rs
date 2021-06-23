5ed33da026b06f9ac8ff4aca2586f8044c43ed59
Folkert <folkert@folkertdev.nl>
2021-06-23 14:05:23 +0200

    add instname flag to debugir

--- compiler/build/src/program.rs
+++ compiler/build/src/program.rs
@@ -193,7 +193,7 @@ pub fn gen_from_mono_module(
         // run the debugir https://github.com/vaivaswatha/debugir tool
         match Command::new("debugir")
             .env_clear()
-            .args(&[app_ll_file.to_str().unwrap()])
+            .args(&["-instnamer", app_ll_file.to_str().unwrap()])
             .output()
         {
             Ok(_) => {}
