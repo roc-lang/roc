e11d2db80e2c700b8be78896446f1cbf9c0aa55f
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2021-08-25 16:53:45 -0700

    Switch test link app to a Roc zig host

--- linker/tests/app.cc
+++
@@ -1,3 +0,0 @@
-const char* init() { return "Application initializing...\n"; }
-const char* app() { return "Hello World from the application\n"; }
-const char* cleanup() { return "Cleaning up application...\n"; }
