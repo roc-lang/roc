ba3b7cee1c67ee35915c57d92ab2c7544c816e16
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2021-09-19 11:32:30 -0700

    Add randomization to ensure at least minor changes

--- examples/hello-zig/Hello.roc
+++ examples/hello-zig/Hello.roc
@@ -3,4 +3,4 @@ app "hello-world"
     imports []
     provides [ main ] to base
 
-main = "Hello, World!"
+main = "Hello, World!\n"
--- time_hello.py
+++ time_hello.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import numpy as np
 import random
+import re
 import sys
 import subprocess
+import secrets
 import time
 import tqdm
+random.seed()
 
 # Build and strip roc executable
 print('Building roc compiler...')
@@ -47,7 +50,16 @@ if result_code != 0:
 print('\nBenchmarking...')
 
 N = int(sys.argv[1])
-program_names = ['roc', 'python3']
+program_names = [
+    'roc',
+    'python3',
+    # 'zig'
+]
+program_files = [
+    'examples/hello-zig/Hello.roc',
+    'hello.py',
+    # 'hello.zig'
+]
 programs = [[
     'target/release/roc',
     '--dev',
@@ -57,11 +69,27 @@ programs = [[
 ], [
     '/usr/bin/python3',
     'hello.py'
-]]
+],
+    # [
+    # 'zig',
+    # 'run',
+    # 'hello.zig'
+    # ]
+]
+regex = re.compile(r"Hello, .*!", re.IGNORECASE)
 
 times = np.zeros((len(programs), N))
 enumerated_programs = list(enumerate(programs))
 for j in tqdm.trange(N):
+    new_str = f'Hello, {secrets.token_urlsafe(16)[:6]}!'
+    for file in program_files:
+        with open(file, 'r') as in_file:
+            content = in_file.read()
+        content = regex.sub(new_str, content)
+        current_str = new_str
+        with open(file, 'w') as out_file:
+            out_file.write(content)
+
     random.shuffle(enumerated_programs)
     for i, program in enumerated_programs:
         start = time.time()
@@ -77,3 +105,11 @@ for i, name in enumerate(program_names):
     print(f'\tMean: {np.mean(times[i])} ms')
     print(f'\tStddev: {np.std(times[i])} ms')
     print(f'\tStderr: {np.std(times[i])/(N**0.5)} ms')
+
+# Reset files
+for file in program_files:
+    with open(file, 'r') as in_file:
+        content = in_file.read()
+    content = regex.sub('Hello, World!', content)
+    with open(file, 'w') as out_file:
+        out_file.write(content)
