054f9ad4ad3aebb3904788942def7780b85688f9
Richard Feldman <oss@rtfeldman.com>
2021-10-15 22:47:43 -0400

    Generate dummy shared lib for macOS

--- linker/src/lib.rs
+++ linker/src/lib.rs
@@ -219,9 +219,22 @@ fn generate_dynamic_lib(
     let dummy_obj_file = Builder::new().prefix("roc_lib").suffix(".o").tempfile()?;
     let dummy_obj_file = dummy_obj_file.path();
 
-    // TODO deal with other architectures here.
-    let mut out_object =
-        write::Object::new(BinaryFormat::Elf, Architecture::X86_64, Endianness::Little);
+    let obj_target = match target.binary_format {
+        target_lexicon::BinaryFormat::Elf => BinaryFormat::Elf,
+        target_lexicon::BinaryFormat::Macho => BinaryFormat::MachO,
+        _ => {
+            // We should have verified this via supported() before calling this function
+            unreachable!()
+        }
+    };
+    let obj_arch = match target.architecture {
+        target_lexicon::Architecture::X86_64 => Architecture::X86_64,
+        _ => {
+            // We should have verified this via supported() before calling this function
+            unreachable!()
+        }
+    };
+    let mut out_object = write::Object::new(obj_target, obj_arch, Endianness::Little);
 
     let text_section = out_object.section_id(write::StandardSection::Text);
     for sym in exposed_to_host {
@@ -251,9 +264,21 @@ fn generate_dynamic_lib(
     )
     .expect("failed to write object to file");
 
-    let (ld_flag_shared, ld_flag_soname) = match target.binary_format {
-        target_lexicon::BinaryFormat::Elf => ("-shared", "-soname"),
-        target_lexicon::BinaryFormat::Macho => ("-dylib", "-install_name"),
+    let (ld_prefix_args, ld_flag_soname) = match target.binary_format {
+        target_lexicon::BinaryFormat::Elf => (vec!["-shared"], "-soname"),
+        target_lexicon::BinaryFormat::Macho => {
+            // This path only exists on macOS Big Sur, and it causes ld errors
+            // on Catalina if it's specified with -L, so we replace it with a
+            // redundant -lSystem if the directory isn't there.
+            let big_sur_path = "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib";
+            let big_sur_fix = if Path::new(big_sur_path).exists() {
+                "-L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib"
+            } else {
+                "-lSystem" // We say -lSystem twice in the case of non-Big-Sur OSes, but it's fine.
+            };
+
+            (vec![big_sur_fix, "-lSystem", "-dylib"], "-install_name")
+        }
         _ => {
             // The supported() function should have been called earlier
             // to verify this is a supported platform!
@@ -262,8 +287,8 @@ fn generate_dynamic_lib(
     };
 
     let output = Command::new("ld")
+        .args(ld_prefix_args)
         .args(&[
-            ld_flag_shared,
             ld_flag_soname,
             dummy_lib_path.file_name().unwrap().to_str().unwrap(),
             dummy_obj_file.to_str().unwrap(),
