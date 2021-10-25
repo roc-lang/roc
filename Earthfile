e9d7f0ce8e936a7b923c6d7cd7201705a331422b
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2021-10-25 20:41:52 +0200

    finished developer_install.sh

--- Earthfile
+++ Earthfile
@@ -8,3 +8,36 @@ install-script:
     FROM +prep-debian
     COPY developer_install.sh ./
     RUN ./developer_install.sh
+
+ci-extras:
+    FROM +install-script
+    # for compilation caching
+    RUN cargo install sccache
+    RUN sccache -V
+    ENV RUSTC_WRAPPER=/usr/local/cargo/bin/sccache
+    ENV SCCACHE_DIR=/earthbuild/sccache_dir
+
+test-zig:
+    FROM +install-script
+    COPY --dir compiler/builtins/bitcode ./
+    RUN cd bitcode && ./run-tests.sh
+
+copy-dirs:
+    FROM +ci-extras
+    COPY --dir cli compiler docs editor ast code_markup utils roc_std vendor examples linker Cargo.toml Cargo.lock version.txt ./
+
+check-clippy:
+    FROM +copy-dirs
+    RUN cargo clippy -V
+    RUN --mount=type=cache,target=$SCCACHE_DIR \
+        cargo clippy -- -D warnings
+
+check-rustfmt:
+    FROM +copy-dirs
+    RUN cargo fmt --version
+    RUN cargo fmt --all -- --check
+
+check-typos:
+    RUN cargo install typos-cli --version 1.0.11 # version set to prevent confusion if the version is updated automatically
+    COPY --dir .github ci cli compiler docs editor examples ast code_markup utils linker nightly_benches packages roc_std www *.md LEGAL_DETAILS shell.nix version.txt ./
+    RUN typos
--- developer_install.sh
+++ developer_install.sh
@@ -5,18 +5,39 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-apt -y install wget curl
+apt -y install wget curl build-essential pkg-config libasound2-dev zlib1g-dev libxcb-shape0-dev libxcb-xfixes0-dev libxkbcommon-dev
+# libasound2-dev for editor sounds
+# libxcb-shape0-dev libxcb-xfixes0-dev for editor clipboard
+# libxkbcommon-dev for editor, not sure what exactly :p
+# build-essential for cc (for rust)
+# zlib1g-dev for inkwell
+
 # rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rustup.sh
+chmod +x rustup.sh
+./rustup.sh -y
+source $HOME/.cargo/env
+rustup install 1.54
+rustup component add clippy
+rustup component add rustfmt
 # zig
 wget -c https://ziglang.org/download/0.8.0/zig-linux-x86_64-0.8.0.tar.xz --no-check-certificate
 tar -xf zig-linux-x86_64-0.8.0.tar.xz
-ln -s ./zig-linux-x86_64-0.8.0/zig /usr/bin/zig
+ln -s ${PWD}/zig-linux-x86_64-0.8.0/zig /usr/bin/zig
 # llvm
+apt -y install lsb-release software-properties-common
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
 ./llvm.sh 12
-# use lld as a faster linker
-# TODO ln -s /usr/bin/lld-12 /usr/bin/ld.lld
-# TODO RUSTFLAGS="-C link-arg=-fuse-ld=lld -C target-cpu=native"
+ln -s /usr/bin/llvm-as-12 /usr/bin/llvm-as
+ln -s /usr/bin/clang-12 /usr/bin/clang # for compiling the c platform
+# use lld as rust linker for faster linking
+ln -s /usr/bin/lld-12 /usr/bin/ld.lld
+
 apt -y install valgrind
+
+echo "Done installing."
+echo ""
+echo "Add the following to your ~/.bashrc file or equivalent: RUSTFLAGS=\"-C link-arg=-fuse-ld=lld -C target-cpu=native\"."
+echo ""
+echo "Test your installation by running `cargo test --release`."
