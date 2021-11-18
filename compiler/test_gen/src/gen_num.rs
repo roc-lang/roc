2eae1a857be31021998ae97c3de10f3a9aaaac78
satotake <doublequotation@gmail.com>
2021-11-18 11:20:37 +0000

    Fix comment wording

--- compiler/test_gen/src/gen_num.rs
+++ compiler/test_gen/src/gen_num.rs
@@ -1216,9 +1216,9 @@ fn tail_call_elimination() {
 #[test]
 #[cfg(any(feature = "gen-dev"))]
 fn int_negate_dev() {
-    // Dev backend yet to have `Num.maxInt` or `Num.minInt`.
-    // TODO Remove this test and add "gen-dev" feature the below
-    // after implementing the both.
+    // TODO
+    // dev backend yet to have `Num.maxInt` or `Num.minInt`.
+    // add the "gen-dev" feature to the test below after implementing them both.
     assert_evals_to!("Num.neg 123", -123, i64);
     assert_evals_to!("Num.neg -123", 123, i64);
     assert_evals_to!("Num.neg 0", 0, i64);
