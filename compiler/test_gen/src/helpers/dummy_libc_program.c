e9343d8b2acb4e76ea08eeb3c5667287190e9525
Brian Carroll <brian.carroll.ireland@gmail.com>
2021-12-11 09:33:18 +0000

    Edit dummy libc program

--- compiler/test_gen/src/helpers/dummy_libc_program.c
+++ compiler/test_gen/src/helpers/dummy_libc_program.c
@@ -2,7 +2,6 @@
 
 void main() {
     printf("Hello, I am a C program and I use libc.\n");
-    printf("Would you like to use libc too, but just can't find the right path?\n");
-    printf("Well, simply compile me from build.rs using Zig's --global-cache-dir,\n");
-    printf("and have libc.a delivered right to your build directory!\n");
+    printf("If you compile me, you'll compile libc too. That's handy for cross-compilation including Wasm.\n");
+    printf("Use `zig build-exe` with `--global-cache-dir my/build/directory` to put libc.a where you want it.\n");
 }
