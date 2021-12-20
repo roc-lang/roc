d8651524b2dd55cb790874adcd81f028d5c874af
Joshua Warner <joshuawarner32@gmail.com>
2021-12-19 19:16:10 -0800

    wip; baseline tokenization for numbers/idents/whitespace

--- compiler/parse/src/token.rs
+++ compiler/parse/src/token.rs
@@ -74,21 +74,71 @@ pub enum Token {
 }
 
 impl Token {
-    pub fn lex_single(expected: Token, bytes: &[u8]) -> Option<(Token, usize)> {
-        if bytes.len() == 0 {
-            return None;
+    pub fn lex_single(expected: Token, mut bytes: &[u8]) -> Option<(Token, usize)> {
+        loop {
+            if bytes.len() == 0 {
+                return None;
+            }
+
+            let res = match bytes[0] {
+                b'(' => (Token::OpenParen, 1),
+                b')' => (Token::CloseParen, 1),
+                b'{' => (Token::OpenCurly, 1),
+                b'}' => (Token::CloseCurly, 1),
+                b'[' => (Token::OpenSquare, 1),
+                b']' => (Token::CloseSquare, 1),
+                b'a'..=b'z' => lex_ident(true, bytes),
+                b'A'..=b'Z' => lex_ident(false, bytes),
+                b'0'..=b'9' => lex_int(bytes),
+                b' ' => {
+                    bytes = skip_whitespace(bytes);
+                    continue;
+                }
+                b => todo!("handle {:?}; expected {:?}", b as char, expected),
+            };
+
+            return Some(res)
         }
-
-        let res = match bytes[0] {
-            b'(' => (Token::OpenParen, 1),
-            b')' => (Token::CloseParen, 1),
-            b'{' => (Token::OpenCurly, 1),
-            b'}' => (Token::CloseCurly, 1),
-            b'[' => (Token::OpenSquare, 1),
-            b']' => (Token::CloseSquare, 1),
-            b => todo!("handle {:?}; expected {:?}", b as char, expected),
-        };
-
-        Some(res)
     }
+}
+
+fn skip_whitespace(mut bytes: &[u8]) -> &[u8] {
+    while bytes.len() > 0 && bytes[0] == b' ' {
+        bytes = &bytes[1..];
+    }
+    bytes
+}
+
+fn is_ident_continue(ch: u8) -> bool {
+    matches!(ch, b'a'..=b'z'|b'A'..=b'Z'|b'0'..=b'9'|b'_')
+}
+
+fn lex_ident(uppercase: bool, bytes: &[u8]) -> (Token, usize) {
+    let mut i = 0;
+    while i < bytes.len() && is_ident_continue(bytes[i]) {
+        i += 1;
+    }
+    let tok = match &bytes[0..i] {
+        b"if" => Token::KeywordIf,
+        b"then" => Token::KeywordThen,
+        b"else" => Token::KeywordElse,
+        b"when" => Token::KeywordWhen,
+        b"as" => Token::KeywordAs,
+        b"is" => Token::KeywordIs,
+        b"expect" => Token::KeywordExpect,
+        _ => Token::Ident,
+    };
+    (tok, i)
+}
+
+fn is_int_continue(ch: u8) -> bool {
+    matches!(ch, b'0'..=b'9')
+}
+
+fn lex_int(bytes: &[u8]) -> (Token, usize) {
+    let mut i = 0;
+    while i < bytes.len() && is_int_continue(bytes[i]) {
+        i += 1;
+    }
+    (Token::Number, i)
 }
