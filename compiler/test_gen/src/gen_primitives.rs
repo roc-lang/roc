e1c0acb6509e6fd38c7f42549d3f2e62d3318209
Joshua Warner <joshuawarner32@gmail.com>
2022-01-02 09:12:54 -0800

    Fix region formatting test issues

--- compiler/test_gen/src/gen_primitives.rs
+++ compiler/test_gen/src/gen_primitives.rs
@@ -1972,7 +1972,7 @@ fn hof_conditional() {
 #[test]
 #[cfg(any(feature = "gen-llvm"))]
 #[should_panic(
-    expected = "Roc failed with message: \"Shadowing { original_region: |L 3-3, C 4-5|, shadow: |L 6-6, C 8-9| Ident"
+    expected = "Roc failed with message: \"Shadowing { original_region: @57-58, shadow: @90-91 Ident"
 )]
 fn pattern_shadowing() {
     assert_evals_to!(
@@ -2448,9 +2448,7 @@ fn backpassing_result() {
 
 #[test]
 #[cfg(any(feature = "gen-llvm", feature = "gen-wasm"))]
-#[should_panic(
-    expected = "Shadowing { original_region: |L 3-3, C 4-5|, shadow: |L 5-5, C 6-7| Ident"
-)]
+#[should_panic(expected = "Shadowing { original_region: @57-58, shadow: @74-75 Ident")]
 fn function_malformed_pattern() {
     assert_evals_to!(
         indoc!(
