c8f8699c27229696473e46755f4e53ef0ef365b1
Dan Knutson <dan.knutson@gmail.com>
2022-01-17 14:47:22 -0600

    update test output again

--- compiler/builtins/bitcode/src/main.zig
+++ compiler/builtins/bitcode/src/main.zig
@@ -130,11 +130,6 @@ comptime {
     exportStrFn(str.strTrimLeft, "trim_left");
     exportStrFn(str.strTrimRight, "trim_right");
 
-    // TODO GIESCH
-    // use/fix these
-    exportStrFn(str.dropLeftNBytesUnsafe, "drop_left_n_bytes_unsafe");
-    exportStrFn(str.dropRightNBytesUnsafe, "drop_right_n_bytes_unsafe");
-
     inline for (INTEGERS) |T| {
         str.exportFromInt(T, ROC_BUILTINS ++ "." ++ STR ++ ".from_int.");
         num.exportParseInt(T, ROC_BUILTINS ++ "." ++ STR ++ ".to_int.");
@@ -143,6 +138,11 @@ comptime {
     inline for (FLOATS) |T| {
         num.exportParseFloat(T, ROC_BUILTINS ++ "." ++ STR ++ ".to_float.");
     }
+
+    // TODO GIESCH
+    // use/fix these
+    exportStrFn(str.dropLeftNBytesUnsafe, "drop_left_n_bytes_unsafe");
+    exportStrFn(str.dropRightNBytesUnsafe, "drop_right_n_bytes_unsafe");
 }
 
 // Utils
