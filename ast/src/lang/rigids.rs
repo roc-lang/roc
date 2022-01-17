791434a88bb1611c1d3f1e67fe4c94e37f769b09
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2022-01-17 11:33:35 +0100

    fmt

--- ast/src/lang/rigids.rs
+++ ast/src/lang/rigids.rs
@@ -11,7 +11,8 @@ use roc_module::ident::Lowercase;
 use roc_types::subs::Variable;
 
 #[derive(Debug)]
-pub struct Rigids { // Rigid type variable = type variable where type is specified by the programmer
+pub struct Rigids {
+    // Rigid type variable = type variable where type is specified by the programmer
     pub names: PoolVec<(Option<PoolStr>, Variable)>, // 8B
     padding: [u8; 1],
 }
