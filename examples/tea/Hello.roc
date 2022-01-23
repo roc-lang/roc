f007564943eb2dd8d42eebeaa6ea8037e273db8b
Richard Feldman <oss@rtfeldman.com>
2022-01-22 19:49:09 -0500

    give TEA example graphics

--- examples/tea/Hello.roc
+++ examples/tea/Hello.roc
@@ -1,14 +1,12 @@
-app "hello"
+app "hello-rust"
     packages { pf: "platform" }
     imports []
     provides [ render ] to pf
 
-render :
-    { width : F32, height : F32 } ->
-    [
-        Rectangle { top : F32, left : F32, bottom : F32, right : F32 },
-        Circle { top : F32, left : F32, radius : F32 },
-        # Text { top : F32, left : F32, text : Str },
-    ]
-render = \window ->
-    Rectangle { top: 10, left: 10, bottom: 100, right: 100 }
+greeting =
+    hi = "Hello"
+    name = "World!"
+
+    "\(hi), \(name)!\n"
+
+render = greeting
--- examples/tea/platform/src/lib.rs
+++ examples/tea/platform/src/lib.rs
@@ -1,34 +1,21 @@
 #![allow(non_snake_case)]
 
-use core::alloc::Layout;
 use core::ffi::c_void;
-use core::mem::{ManuallyDrop, MaybeUninit};
-use libc;
 use roc_std::RocStr;
 use std::ffi::CStr;
 use std::os::raw::c_char;
 
+mod graphics;
+mod tea;
+
 extern "C" {
     #[link_name = "roc__renderForHost_1_exposed"]
-    fn roc_render(output: *mut u8) -> ();
-
-    #[link_name = "roc__renderForHost_size"]
-    fn roc_render_size() -> i64;
-
-    #[link_name = "roc__renderForHost_1_Fx_caller"]
-    fn call_Fx(flags: *const u8, closure_data: *const u8, output: *mut u8) -> ();
-
-    #[allow(dead_code)]
-    #[link_name = "roc__renderForHost_1_Fx_size"]
-    fn size_Fx() -> i64;
-
-    #[link_name = "roc__renderForHost_1_Fx_result_size"]
-    fn size_Fx_result() -> i64;
+    fn roc_render() -> RocStr;
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn roc_alloc(size: usize, _alignment: u32) -> *mut c_void {
-    libc::malloc(size)
+    return libc::malloc(size);
 }
 
 #[no_mangle]
@@ -38,12 +25,12 @@ pub unsafe extern "C" fn roc_realloc(
     _old_size: usize,
     _alignment: u32,
 ) -> *mut c_void {
-    libc::realloc(c_ptr, new_size)
+    return libc::realloc(c_ptr, new_size);
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn roc_dealloc(c_ptr: *mut c_void, _alignment: u32) {
-    libc::free(c_ptr)
+    return libc::free(c_ptr);
 }
 
 #[no_mangle]
@@ -71,56 +58,10 @@ pub unsafe extern "C" fn roc_memset(dst: *mut c_void, c: i32, n: usize) -> *mut 
 
 #[no_mangle]
 pub extern "C" fn rust_main() -> i32 {
-    let size = unsafe { roc_render_size() } as usize;
-    let layout = Layout::array::<u8>(size).unwrap();
+    let roc_str = unsafe { roc_render() };
 
-    unsafe {
-        // TODO allocate on the stack if it's under a certain size
-        let buffer = std::alloc::alloc(layout);
-
-        roc_render(buffer);
-
-        let result = call_the_closure(buffer);
-
-        std::alloc::dealloc(buffer, layout);
-
-        dbg!(result);
-    };
+    tea::render(roc_str);
 
     // Exit code
     0
-}
-
-unsafe extern "C" fn call_the_closure(closure_data_ptr: *const u8) -> i64 {
-    let size = size_Fx_result() as usize;
-    let layout = Layout::array::<u8>(size).unwrap();
-    let buffer = std::alloc::alloc(layout) as *mut u8;
-
-    call_Fx(
-        // This flags pointer will never get dereferenced
-        MaybeUninit::uninit().as_ptr(),
-        closure_data_ptr as *const u8,
-        buffer as *mut u8,
-    );
-
-    std::alloc::dealloc(buffer, layout);
-
-    0
-}
-
-#[no_mangle]
-pub extern "C" fn roc_fx_getLine() -> RocStr {
-    use std::io::{self, BufRead};
-
-    let stdin = io::stdin();
-    let line1 = stdin.lock().lines().next().unwrap().unwrap();
-
-    RocStr::from_slice(line1.as_bytes())
-}
-
-#[no_mangle]
-pub extern "C" fn roc_fx_putLine(line: ManuallyDrop<RocStr>) {
-    let bytes = line.as_slice();
-    let string = unsafe { std::str::from_utf8_unchecked(bytes) };
-    println!("{}", string);
 }
