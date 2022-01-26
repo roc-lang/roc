68b6cc7d9f8c850aebfe1c38ad742895ea2716d0
Folkert <folkert@folkertdev.nl>
2022-01-26 14:01:53 +0100

    add some helpers

--- compiler/target/src/lib.rs
+++ compiler/target/src/lib.rs
@@ -13,3 +13,27 @@ pub enum Architecture {
     Arm,
     Wasm32,
 }
+
+impl Architecture {
+    pub const fn ptr_width(&self) -> u32 {
+        use Architecture::*;
+
+        match self {
+            X86_64 | Aarch64 | Arm => 8,
+            X86_32 | Wasm32 => 4,
+        }
+    }
+}
+
+impl From<target_lexicon::Architecture> for Architecture {
+    fn from(target: target_lexicon::Architecture) -> Self {
+        match target {
+            target_lexicon::Architecture::X86_64 => Architecture::X86_64,
+            target_lexicon::Architecture::X86_32(_) => Architecture::X86_32,
+            target_lexicon::Architecture::Aarch64(_) => Architecture::Aarch64,
+            target_lexicon::Architecture::Arm(_) => Architecture::Arm,
+            target_lexicon::Architecture::Wasm32 => Architecture::Wasm32,
+            _ => unreachable!("unsupported architecture"),
+        }
+    }
+}
