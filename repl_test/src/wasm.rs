a4d83f69169d46298bdc5b9284d1ab3cb2837b7a
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-02-20 09:03:59 +0000

    repl_test: developing wasm test setup

--- repl_test/src/wasm.rs
+++ repl_test/src/wasm.rs
@@ -1,26 +1,29 @@
 use std::{
     cell::RefCell,
+    env, fs,
     ops::{Deref, DerefMut},
+    path::Path,
     thread_local,
 };
-use wasmer::Instance;
+use wasmer::{imports, Function, Instance, Module, Store};
+use wasmer_wasi::WasiState;
+
+const COMPILER_PATH_ENV_VAR: &str = "COMPILER_PATH";
 
 thread_local! {
-    static REPL_STATE: RefCell<Option<ReplState>> = RefCell::new(None)
+    static REPL_STATE: RefCell<Option<Env<'static>>> = RefCell::new(None)
 }
 
-struct ReplState {
+struct Env {
+    src: &'static str,
     compiler: Instance,
     app: Option<Instance>,
     result_addr: Option<u32>,
 }
 
-fn instantiate_compiler(wasm_module_bytes: &[u8]) {
-    use wasmer::{Module, Store};
-    use wasmer_wasi::WasiState;
-
+fn wasmer_create_app(app_bytes: &[u8]) {
     let store = Store::default();
-    let wasmer_module = Module::new(&store, &wasm_module_bytes).unwrap();
+    let wasmer_module = Module::new(&store, &app_bytes).unwrap();
 
     // First, we create the `WasiEnv`
     let mut wasi_env = WasiState::new("hello").finalize().unwrap();
@@ -29,35 +32,10 @@ fn instantiate_compiler(wasm_module_bytes: &[u8]) {
     // and attach it to the Wasm instance.
     let import_object = wasi_env
         .import_object(&wasmer_module)
-        .unwrap_or_else(|_| wasmer::imports!());
+        .unwrap_or_else(|_| imports!());
 
-    let instance = wasmer::Instance::new(&wasmer_module, &import_object).unwrap();
-    REPL_STATE.with(|f| {
-        if let Some(state) = f.borrow_mut().deref_mut() {
-            state.app = Some(instance)
-        } else {
-            panic!("REPL state not found")
-        }
-    })
-}
+    let instance = Instance::new(&wasmer_module, &import_object).unwrap();
 
-fn wasmer_create_app(wasm_module_bytes: &[u8]) {
-    use wasmer::{Module, Store};
-    use wasmer_wasi::WasiState;
-
-    let store = Store::default();
-    let wasmer_module = Module::new(&store, &wasm_module_bytes).unwrap();
-
-    // First, we create the `WasiEnv`
-    let mut wasi_env = WasiState::new("hello").finalize().unwrap();
-
-    // Then, we get the import object related to our WASI
-    // and attach it to the Wasm instance.
-    let import_object = wasi_env
-        .import_object(&wasmer_module)
-        .unwrap_or_else(|_| wasmer::imports!());
-
-    let instance = wasmer::Instance::new(&wasmer_module, &import_object).unwrap();
     REPL_STATE.with(|f| {
         if let Some(state) = f.borrow_mut().deref_mut() {
             state.app = Some(instance)
@@ -120,10 +98,69 @@ fn wasmer_get_result_and_memory(buffer_alloc_addr: u32) -> u32 {
     })
 }
 
+fn wasmer_copy_input_string(src_buffer_addr: u32) {
+    REPL_STATE.with(|f| {
+        if let Some(state) = f.borrow().deref() {
+            let compiler_memory = state.compiler.exports.get_memory("memory").unwrap();
+            let compiler_memory_bytes: &mut [u8] = unsafe { compiler_memory.data_unchecked_mut() };
+
+            let buf_addr = src_buffer_addr as usize;
+            let len = state.src.len();
+            compiler_memory_bytes[buf_addr..][..len].copy_from_slice(state.src.as_bytes());
+        }
+    })
+}
+
+fn init_compiler() -> Vec<u8> {
+    let path_str = env::var(COMPILER_PATH_ENV_VAR).unwrap();
+    let path = Path::new(&path_str);
+    let wasm_module_bytes = fs::read(&path).unwrap();
+
+    let store = Store::default();
+    let wasmer_module = Module::new(&store, &wasm_module_bytes).unwrap();
+
+    let import_object = imports! {
+        "env" => {
+            "wasmer_create_app" => Function::new_native(&store, wasmer_create_app),
+            "wasmer_run_app" => Function::new_native(&store, wasmer_run_app),
+            "wasmer_get_result_and_memory" => Function::new_native(&store, wasmer_get_result_and_memory),
+            "wasmer_copy_input_string" => Function::new_native(&store, wasmer_copy_input_string),
+        }
+    };
+
+    Instance::new(&wasmer_module, &import_object).unwrap()
+}
+
+fn run(src: &str) -> String {
+    let compiler = init_compiler();
+
+    let entrypoint = compiler
+        .exports
+        .get_function("entrypoint_from_test")
+        .unwrap();
+
+    REPL_STATE.with(|f| {
+        let new_state = Env {
+            src,
+            compiler,
+            app: None,
+            result_addr: None,
+        };
+        let current_state = f.borrow_mut().deref_mut();
+        assert_eq!(current_state, None);
+        *current_state = Some(new_state);
+    });
+
+    let actual = String::new();
+    actual
+}
+
 pub fn expect_success(input: &str, expected: &str) {
-    todo!()
+    let actual = run(input);
+    assert_eq!(actual, expected);
 }
 
 pub fn expect_failure(input: &str, expected: &str) {
-    todo!()
+    let actual = run(input);
+    assert_eq!(actual, expected);
 }
--- repl_wasm/src/interface_test.rs
+++ repl_wasm/src/interface_test.rs
@@ -2,6 +2,7 @@ extern "C" {
     fn wasmer_create_app(wasm_module_bytes: &[u8]);
     fn wasmer_run_app() -> usize;
     fn wasmer_get_result_and_memory(buffer_alloc_addr: *mut u8) -> usize;
+    fn wasmer_copy_input_string(src_buffer_addr: *mut u8);
 }
 
 /// Async wrapper to match the equivalent JS function
@@ -20,9 +21,16 @@ pub fn js_get_result_and_memory(buffer_alloc_addr: *mut u8) -> usize {
     unsafe { wasmer_get_result_and_memory(buffer_alloc_addr) }
 }
 
-/// Synchronous entrypoint for Wasmer tests
-/// In tests we avoid the complexity of async calls across the Wasm/native boundary
-pub fn entrypoint_from_test(src: String) -> Result<String, String> {
+/// Entrypoint for Wasmer tests
+/// - Synchronous API, to avoid the need to manage async calls across the Wasm/native boundary.
+///   In the JS version, wasm_bindgen deals with this, as well as translating Rust Future <-> JS Promise.
+/// - Manually copy the source bytes and convert to String. Again, wasm_bindgen does this for JS.
+pub fn entrypoint_from_test(src_len: usize) -> Result<String, String> {
+    let mut src_buffer = std::vec![0; src_len];
+    let src = unsafe {
+        wasmer_copy_input_string(src_buffer.as_mut_ptr());
+        String::from_utf8_unchecked(src_buffer)
+    };
     let result_async = crate::repl::entrypoint_from_js(src);
     executor::block_on(result_async)
 }
