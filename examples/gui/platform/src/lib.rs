4168fc7c6e422ca6dc34f51f11799d5154bab4d9
Richard Feldman <oss@rtfeldman.com>
2022-02-23 21:13:37 -0500

    Drop some unused code

--- examples/gui/platform/src/lib.rs
+++ examples/gui/platform/src/lib.rs
@@ -10,55 +10,6 @@ extern "C" {
     fn roc_render() -> RocElem;
 }
 
-// enum Action<State> {
-//     Update(State),
-//     DoNothing,
-// }
-
-// enum Elem<State> {
-//     Button(Key, Box<dyn Fn() -> Action<State>>, Box<Elem<State>>),
-//     Text(Key, RocStr),
-//     TextInput {
-//         key: Key,
-//         /// current text that's been entered
-//         text: RocStr,
-//         /// event handler to run when the user changes the text
-//         on_change: Box<dyn Fn(RocStr) -> Action<State>>,
-//     },
-//     Col(Key, Vec<Elem<State>>),
-//     Row(Key, Vec<Elem<State>>),
-// }
-
-/// Either a number between 0 and `isize::MAX`,
-/// or a "null" value (meaning no number was specified).
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct Key(usize);
-
-impl Key {
-    const NULL: Self = Self(usize::MAX);
-
-    fn new(val: usize) -> Self {
-        debug_assert!(val <= isize::MAX as usize);
-
-        Key(val)
-    }
-
-    fn null() -> Self {
-        Self::NULL
-    }
-
-    fn is_null(self) -> bool {
-        self == Self::NULL
-    }
-}
-
-struct AppState {
-    /// glyph index of caret position
-    caret: usize,
-    /// number of glyphs selected (0 means it's a normal caret)
-    selected: usize,
-}
-
 #[no_mangle]
 pub extern "C" fn rust_main() -> i32 {
     let root_elem = unsafe { roc_render() };
