203fdab20908879fc50a3cba7692110292587d1e
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-02-25 19:27:08 +0000

    repl_wasm: rename modules interface -> externs

--- repl_wasm/src/lib.rs
+++ repl_wasm/src/lib.rs
@@ -4,16 +4,16 @@ mod repl;
 // Interface with external JS in the browser
 //
 #[cfg(not(feature = "wasmer"))]
-mod interface_js;
+mod externs_js;
 #[cfg(not(feature = "wasmer"))]
-pub use interface_js::{entrypoint_from_js, js_create_app, js_get_result_and_memory, js_run_app};
+pub use externs_js::{entrypoint_from_js, js_create_app, js_get_result_and_memory, js_run_app};
 
 //
 // Interface with test code outside the Wasm module
 //
 #[cfg(feature = "wasmer")]
-mod interface_test;
+mod externs_test;
 #[cfg(feature = "wasmer")]
-pub use interface_test::{
+pub use externs_test::{
     entrypoint_from_test, js_create_app, js_get_result_and_memory, js_run_app,
 };
