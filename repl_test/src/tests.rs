85ade20b800e3242d577616b30fe10fcd3a7c917
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-02-26 13:45:35 +0000

    Formatting

--- repl_test/src/tests.rs
+++ repl_test/src/tests.rs
@@ -1,4 +1,3 @@
-
 #[allow(unused_imports)]
 use indoc::indoc;
 
--- repl_wasm/src/lib.rs
+++ repl_wasm/src/lib.rs
@@ -14,6 +14,4 @@ pub use externs_js::{entrypoint_from_js, js_create_app, js_get_result_and_memory
 #[cfg(feature = "wasmer")]
 mod externs_test;
 #[cfg(feature = "wasmer")]
-pub use externs_test::{
-    entrypoint_from_test, js_create_app, js_get_result_and_memory, js_run_app,
-};
+pub use externs_test::{entrypoint_from_test, js_create_app, js_get_result_and_memory, js_run_app};
