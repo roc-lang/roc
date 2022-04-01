47a9929196b45c104a72fbd9e10010fbf03b324b
Brian J. Cardiff <bcardiff@gmail.com>
2022-04-01 19:43:12 -0300

    Allow get-flags.sh to be run from any directory

--- examples/hello-crystal/get-flags.sh
+++ examples/hello-crystal/get-flags.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-OUTPUT=$(crystal build platform/host.cr --no-color 2>&1);
+EXAMPLE_DIR="$(dirname "${BASH_SOURCE[0]}")"
+
+OUTPUT=$(cd $EXAMPLE_DIR;crystal build platform/host.cr --no-color 2>&1);
 LAST_LINE=$(echo $OUTPUT | tail -n 1)
 
 if [[ $OUTPUT =~ ^.*-rdynamic[[:space:]](.*)\`$ ]]
