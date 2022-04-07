95f2c831dbbff5c26a6d332a977587204d61dc97
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2022-04-07 09:38:25 -0400

    zig 0.9 errors for effects

--- examples/interactive/effects-platform/host.zig
+++ examples/interactive/effects-platform/host.zig
@@ -1,3 +1,4 @@
+const builtin = @import("builtin");
 const std = @import("std");
 const str = @import("str");
 const RocStr = str.RocStr;
@@ -15,7 +16,7 @@ comptime {
     // -fcompiler-rt in link.rs instead of doing this. Note that this
     // workaround is present in many host.zig files, so make sure to undo
     // it everywhere!
-    if (std.builtin.os.tag == .macos) {
+    if (builtin.os.tag == .macos) {
         _ = @import("compiler_rt");
     }
 }
@@ -29,25 +30,30 @@ extern fn roc__mainForHost_1_Fx_caller(*const u8, [*]u8, [*]u8) void;
 extern fn roc__mainForHost_1_Fx_size() i64;
 extern fn roc__mainForHost_1_Fx_result_size() i64;
 
-extern fn malloc(size: usize) callconv(.C) ?*c_void;
-extern fn realloc(c_ptr: [*]align(@alignOf(u128)) u8, size: usize) callconv(.C) ?*c_void;
+extern fn malloc(size: usize) callconv(.C) ?*anyopaque;
+extern fn realloc(c_ptr: [*]align(@alignOf(u128)) u8, size: usize) callconv(.C) ?*anyopaque;
 extern fn free(c_ptr: [*]align(@alignOf(u128)) u8) callconv(.C) void;
 extern fn memcpy(dst: [*]u8, src: [*]u8, size: usize) callconv(.C) void;
 extern fn memset(dst: [*]u8, value: i32, size: usize) callconv(.C) void;
 
-export fn roc_alloc(size: usize, alignment: u32) callconv(.C) ?*c_void {
+export fn roc_alloc(size: usize, alignment: u32) callconv(.C) ?*anyopaque {
+    _ = alignment;
     return malloc(size);
 }
 
-export fn roc_realloc(c_ptr: *c_void, new_size: usize, old_size: usize, alignment: u32) callconv(.C) ?*c_void {
+export fn roc_realloc(c_ptr: *anyopaque, new_size: usize, old_size: usize, alignment: u32) callconv(.C) ?*anyopaque {
+    _ = old_size;
+    _ = alignment;
     return realloc(@alignCast(16, @ptrCast([*]u8, c_ptr)), new_size);
 }
 
-export fn roc_dealloc(c_ptr: *c_void, alignment: u32) callconv(.C) void {
+export fn roc_dealloc(c_ptr: *anyopaque, alignment: u32) callconv(.C) void {
+    _ = alignment;
     free(@alignCast(16, @ptrCast([*]u8, c_ptr)));
 }
 
-export fn roc_panic(c_ptr: *c_void, tag_id: u32) callconv(.C) void {
+export fn roc_panic(c_ptr: *anyopaque, tag_id: u32) callconv(.C) void {
+    _ = tag_id;
     const stderr = std.io.getStdErr().writer();
     const msg = @ptrCast([*:0]const u8, c_ptr);
     stderr.print("Application crashed with message\n\n    {s}\n\nShutting down\n", .{msg}) catch unreachable;
@@ -67,7 +73,6 @@ const Unit = extern struct {};
 pub export fn main() u8 {
     const allocator = std.heap.page_allocator;
 
-    const stdout = std.io.getStdOut().writer();
     const stderr = std.io.getStdErr().writer();
 
     // NOTE the return size can be zero, which will segfault. Always allocate at least 8 bytes
@@ -80,14 +85,14 @@ pub export fn main() u8 {
     }
 
     var ts1: std.os.timespec = undefined;
-    std.os.clock_gettime(std.os.CLOCK_REALTIME, &ts1) catch unreachable;
+    std.os.clock_gettime(std.os.CLOCK.REALTIME, &ts1) catch unreachable;
 
     roc__mainForHost_1_exposed_generic(output);
 
     call_the_closure(output);
 
     var ts2: std.os.timespec = undefined;
-    std.os.clock_gettime(std.os.CLOCK_REALTIME, &ts2) catch unreachable;
+    std.os.clock_gettime(std.os.CLOCK.REALTIME, &ts2) catch unreachable;
 
     const delta = to_seconds(ts2) - to_seconds(ts1);
 
@@ -132,7 +137,7 @@ fn call_the_closure(closure_data_pointer: [*]u8) void {
 pub export fn roc_fx_getLine() str.RocStr {
     if (roc_fx_getLine_help()) |value| {
         return value;
-    } else |err| {
+    } else |_| {
         return str.RocStr.empty();
     }
 }
@@ -187,9 +192,4 @@ fn roc_fx_getInt_help() !i64 {
     const line: []u8 = (try stdin.readUntilDelimiterOrEof(&buf, '\n')) orelse "";
 
     return std.fmt.parseInt(i64, line, 10);
-}
-
-fn readLine() []u8 {
-    const stdin = std.io.getStdIn().reader();
-    return (stdin.readUntilDelimiterOrEof(&line_buf, '\n') catch unreachable) orelse "";
 }
