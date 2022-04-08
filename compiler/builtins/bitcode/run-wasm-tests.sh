024fd81a0b14d85e876fae79363e5d5301dc2819
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-04-08 16:38:46 +0100

    Further improve zig wasm builtins test script

--- compiler/builtins/bitcode/run-wasm-tests.sh
+++ compiler/builtins/bitcode/run-wasm-tests.sh
@@ -8,6 +8,4 @@ set -euxo pipefail
 # Zig will try to run the test binary it produced, but it is a wasm object and hence your OS won't
 # know how to run it. In the error message, it prints the binary it tried to run. We use some fun
 # unix tools to get that path, then feed it to wasmer
-warning_about_non_native_binary=$(zig test -target wasm32-wasi-musl -O ReleaseFast src/main.zig 2>&1) || true
-wasm_test_binary=$(echo $warning_about_non_native_binary | grep -o '[^ ]*test\.wasm')
-wasmer $wasm_test_binary dummyArgForZigTestBinary
+zig test -target wasm32-wasi-musl -O ReleaseFast src/main.zig --test-cmd wasmer --test-cmd-bin
