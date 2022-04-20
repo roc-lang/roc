19dce40cd18b31e5a58db394716fa81c7b48e569
Folkert <folkert@folkertdev.nl>
2022-04-20 18:11:44 +0200

    fix formatting

--- compiler/can/src/annotation.rs
+++ compiler/can/src/annotation.rs
@@ -1,6 +1,6 @@
 use crate::env::Env;
 use crate::scope::Scope;
-use roc_collections::{ImMap,  MutSet, SendMap, VecMap, VecSet};
+use roc_collections::{ImMap, MutSet, SendMap, VecMap, VecSet};
 use roc_module::ident::{Ident, Lowercase, TagName};
 use roc_module::symbol::{IdentIds, ModuleId, Symbol};
 use roc_parse::ast::{AssignedField, ExtractSpaces, Pattern, Tag, TypeAnnotation, TypeHeader};
