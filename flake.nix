30c8cbc6207948dbd528ef61c7cc2bfd2054d03c
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2022-04-20 19:53:30 +0200

    made nix flake work for darwin-aarch64(M1)

--- flake.nix
+++ flake.nix
@@ -4,67 +4,83 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
     nixpkgs-unstable = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
-    # zig = { url = "github:roarkanize/zig-overlay"; };
+    zig = { url = "github:roarkanize/zig-overlay"; };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      unstable-pkgs = nixpkgs-unstable.legacyPackages.x86_64-linux;
-      llvmPkgs = pkgs.llvmPackages_12;
+  outputs = { self, nixpkgs, nixpkgs-unstable, zig, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        unstable-pkgs = nixpkgs-unstable.legacyPackages.${system};
+        llvmPkgs = pkgs.llvmPackages_12;
 
-      linuxInputs = with pkgs; [
-        valgrind # used in cli tests, see cli/tests/cli_run.rs
-        vulkan-headers
-        vulkan-loader
-        vulkan-tools
-        vulkan-validation-layers
-        xorg.libX11
-        xorg.libXcursor
-        xorg.libXrandr
-        xorg.libXi
-        xorg.libxcb
-        alsa-lib
-      ];
+        linuxInputs = with pkgs;
+          lib.optionals stdenv.isLinux [
+            valgrind # used in cli tests, see cli/tests/cli_run.rs
+            vulkan-headers
+            vulkan-loader
+            vulkan-tools
+            vulkan-validation-layers
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            xorg.libxcb
+            alsa-lib
+          ];
 
-      sharedInputs = (with pkgs; [
-        # build libraries
-        cmake
-        git
-        python3
-        llvmPkgs.llvm.dev
-        llvmPkgs.clang
-        libxkbcommon
-        pkg-config
-        zig # roc builtins are implemented in zig, see compiler/builtins/bitcode/
-
-        # lib deps
-        libffi
-        libxml2
-        ncurses
-        zlib
-        libiconv
-
-        # faster builds - see https://github.com/rtfeldman/roc/blob/trunk/BUILDING_FROM_SOURCE.md#use-lld-for-the-linker
-        llvmPkgs.lld
-        # debugir
-      ]) ++ (with unstable-pkgs; [
-        rustc
-        cargo
-        clippy
-        rustfmt
+        darwinInputs = with pkgs;
+          lib.optionals stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [
+            AppKit
+            CoreFoundation
+            CoreServices
+            CoreVideo
+            Foundation
+            Metal
+            Security
       ]);
-    in {
 
-      devShell.x86_64-linux = pkgs.mkShell {
-        buildInputs = sharedInputs ++ linuxInputs;
+        # zig 0.8.1 from pkgs is broken on aarch64-darwin, hence the workaround
+        zig-toolchain = zig.packages.${system}."0.8.1";
 
-        LLVM_SYS_120_PREFIX = "${llvmPkgs.llvm.dev}";
-        NIX_GLIBC_PATH = if pkgs.stdenv.isLinux then "${pkgs.glibc_multi.out}/lib" else "";
-        LD_LIBRARY_PATH = with pkgs;
-          lib.makeLibraryPath
-          ([ pkg-config stdenv.cc.cc.lib libffi ncurses zlib ] ++ linuxInputs);
-      };
+        sharedInputs = (with pkgs; [
+          # build libraries
+          cmake
+          git
+          python3
+          llvmPkgs.llvm.dev
+          llvmPkgs.clang
+          libxkbcommon
+          pkg-config
+          zig-toolchain # roc builtins are implemented in zig, see compiler/builtins/bitcode/
 
-    };
+          # lib deps
+          libffi
+          libxml2
+          ncurses
+          zlib
+          libiconv
+
+          # faster builds - see https://github.com/rtfeldman/roc/blob/trunk/BUILDING_FROM_SOURCE.md#use-lld-for-the-linker
+          llvmPkgs.lld
+          # debugir
+        ]) ++ (with unstable-pkgs; [
+          rustc
+          cargo
+          clippy
+          rustfmt
+        ]);
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = sharedInputs ++ darwinInputs ++ linuxInputs;
+
+          LLVM_SYS_120_PREFIX = "${llvmPkgs.llvm.dev}";
+          NIX_GLIBC_PATH = if pkgs.stdenv.isLinux then "${pkgs.glibc_multi.out}/lib" else "";
+          LD_LIBRARY_PATH = with pkgs;
+            lib.makeLibraryPath
+            ([ pkg-config stdenv.cc.cc.lib libffi ncurses zlib ]);
+        };
+      }
+  );  
 }
