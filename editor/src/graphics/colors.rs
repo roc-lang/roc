8875a9ee9ce19f8d32e3e049fb99e21d0f35eec1
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2022-04-26 19:00:41 +0200

    auto use preferred color format

--- editor/src/graphics/colors.rs
+++ editor/src/graphics/colors.rs
@@ -1,4 +1,4 @@
-use palette::{FromColor, Hsv, Srgb};
+use palette::{FromColor, Hsv, LinSrgb, Srgb};
 
 pub type RgbaTup = (f32, f32, f32, f32);
 pub const WHITE: RgbaTup = (1.0, 1.0, 1.0, 1.0);
@@ -21,11 +21,11 @@ pub fn from_hsb(hue: usize, saturation: usize, brightness: usize) -> RgbaTup {
 }
 
 pub fn from_hsba(hue: usize, saturation: usize, brightness: usize, alpha: f32) -> RgbaTup {
-    let rgb = Srgb::from_color(Hsv::new(
+    let rgb = LinSrgb::from(Srgb::from_color(Hsv::new(
         hue as f32,
         (saturation as f32) / 100.0,
         (brightness as f32) / 100.0,
-    ));
+    )));
 
     (rgb.red, rgb.green, rgb.blue, alpha)
 }
