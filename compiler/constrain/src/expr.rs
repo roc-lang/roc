12234c36ade740c97b21d4824a4ea3f2dc4740bb
Ayaz Hafiz <ayaz@rwx.com>
2022-04-28 14:26:24 -0400

    Remove unnecessary var introduction

--- compiler/constrain/src/expr.rs
+++ compiler/constrain/src/expr.rs
@@ -1183,11 +1183,6 @@ fn constrain_when_branch_help(
         delayed_is_open_constraints: Vec::new(),
     };
 
-    // TODO: can i remove this?
-    // state
-    //     .vars
-    //     .push(when_branch.redundant.variable_for_introduction());
-
     // TODO investigate for error messages, is it better to unify all branches with a variable,
     // then unify that variable with the expectation?
     for (i, loc_pattern) in when_branch.patterns.iter().enumerate() {
