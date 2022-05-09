f07c56a25d67822d96ea6ed2d8129c2f8b1bce6e
Ayaz Hafiz <ayaz@rwx.com>
2022-05-09 11:57:08 -0400

    Known-size iterator

--- compiler/collections/src/lib.rs
+++ compiler/collections/src/lib.rs
@@ -3,6 +3,7 @@
 #![allow(clippy::large_enum_variant)]
 
 pub mod all;
+mod known_size_iterator;
 mod reference_matrix;
 mod small_string_interner;
 pub mod soa;
@@ -10,6 +11,7 @@ mod vec_map;
 mod vec_set;
 
 pub use all::{default_hasher, BumpMap, ImEntry, ImMap, ImSet, MutMap, MutSet, SendMap};
+pub use known_size_iterator::KnownSizeIterator;
 pub use reference_matrix::{ReferenceMatrix, Sccs};
 pub use small_string_interner::SmallStringInterner;
 pub use vec_map::VecMap;
