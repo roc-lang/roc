3e7928096d7196fd0880fa87db533cde9ef1bac7
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-05-14 10:09:50 +0100

    Clean up Netlify script

--- www/netlify.sh
+++ www/netlify.sh
@@ -8,13 +8,9 @@ rustup update
 rustup default stable
 rustup target add wasm32-unknown-unknown wasm32-wasi
 
-ZIG_DIR="zig-linux-x86_64-0.9.1"
-wget https://ziglang.org/download/0.9.1/${ZIG_DIR}.tar.xz
-
-uname -a
-
-xz --decompress ${ZIG_DIR}.tar.xz
-tar xvf ${ZIG_DIR}.tar
-export PATH="${ZIG_DIR}:${PATH}"
+ZIG_DIRNAME="zig-linux-x86_64-0.9.1"
+wget https://ziglang.org/download/0.9.1/${ZIG_DIRNAME}.tar.xz
+tar --extract --xz --file=${ZIG_DIRNAME}.tar.xz
+export PATH="$(pwd)/${ZIG_DIRNAME}:${PATH}"
 
 bash build.sh
