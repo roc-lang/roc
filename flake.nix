9cc8a067aeace820ff5b9661e12d7043495e75f4
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2022-05-16 17:38:37 +0200

    progress trying out cargo2nix

--- flake.nix
+++ flake.nix
@@ -2,24 +2,54 @@
   description = "Roc flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    cargo2nix.url = "github:cargo2nix/cargo2nix/master"; # improves caching of dependent rust crates
+    flake-utils.url = "github:numtide/flake-utils"; # to easily make configs for all architectures
     rust-overlay.url = "github:oxalica/rust-overlay"; # rust from nixpkgs has some libc problems, this is patched in the rust-overlay
     zig.url = "github:roarkanize/zig-overlay"; # zig 8.1 is broken on nixpkgs for M1 macs
-    flake-utils.url = "github:numtide/flake-utils"; # to easily make configs for all architectures
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, zig, flake-utils }:
+  outputs = { self, nixpkgs, cargo2nix, rust-overlay, zig, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { 
-          inherit system overlays;
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+                        (import "${cargo2nix}/overlay") # contains a custom rustBuilder
+                        rust-overlay.overlay
+                      ];
         };
+
         llvmPkgs = pkgs.llvmPackages_13;
+
+        # create the workspace & dependencies package set
+        rustPkgs = pkgs.rustBuilder.makePackageSet' {
+          rustChannel = "1.60.0"; # TODO get this from rust-toolchain file
+          packageFun = import ./Cargo.nix;
+          packageOverrides = pkgs: pkgs.rustBuilder.overrides.all ++ [
+            (pkgs.rustBuilder.rustLib.makeOverride {
+              name = "wasi_libc_sys";
+              overrideAttrs = drv: {
+                buildInputs = drv.buildInputs or [ ] ++ [
+                  pkgs.zig
+                ];
+              };
+            })
+          ] ++ [
+            (pkgs.rustBuilder.rustLib.makeOverride {
+              name = "llvm-sys";
+              overrideAttrs = drv: {
+                buildInputs = drv.buildInputs or [ ] ++ [
+                  llvmPkgs.llvm.dev
+                ];
+              };
+            })
+          ];
+        };        
 
         # get current working directory
         cwd = builtins.toString ./.;
-        rust = pkgs.rust-bin.fromRustupToolchainFile "${cwd}/rust-toolchain.toml";
+        #rust = pkgs.rust-bin.fromRustupToolchainFile "${cwd}/rust-toolchain.toml";
 
         linuxInputs = with pkgs; [
           valgrind # used in cli tests, see cli/tests/cli_run.rs
@@ -60,9 +90,13 @@
           llvmPkgs.lld
           # debugir
 
-          rust
+          rust-bin
         ]);
-      in {
+
+        workspaceShell = rustPkgs.workspaceShell {
+          buildInputs = sharedInputs ++ linuxInputs;
+        };
+      in rec {
 
         devShell = pkgs.mkShell {
           buildInputs = sharedInputs ++ linuxInputs;
@@ -75,6 +109,14 @@
           NIXPKGS_ALLOW_UNFREE = 1; # to run the editor with NVIDIA's closed source drivers
         };
 
+        packages = {
+          # nix build .#roc
+          # nix build .#packages.x86_64-linux.roc
+          roc = (rustPkgs.workspace.roc_cli {}).bin;
+        };
+
+        # nix build
+        defaultPackage = packages.roc;
     }
   );
 }
