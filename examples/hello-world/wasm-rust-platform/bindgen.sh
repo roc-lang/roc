8d15ad299a4dc65afba1dc86169b2ac4d887ae7b
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-05-18 07:42:36 +0100

    Update wasm-bindgen script for Rust web example

--- examples/hello-world/wasm-rust-platform/bindgen.sh
+++ examples/hello-world/wasm-rust-platform/bindgen.sh
@@ -2,11 +2,12 @@
 
 if ! which wasm-bindgen > /dev/null
 then
-    cargo install wasm-bindgen
+    cargo install wasm-bindgen-cli
 fi
 
 # Generate JS bindings for the combined app+host binary
-wasm-bindgen roc/helloWeb.wasm --target web --no-typescript --out-dir www
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+wasm-bindgen "${SCRIPT_DIR}/app/helloWeb.wasm" --target web --no-typescript --out-dir "${SCRIPT_DIR}/www"
 
 # comment out a badly formatted import from wasm-bindgen
-sed -i  's/^import/\/\/import/' www/helloWeb.js
+sed -i  's/^import/\/\/import/' "${SCRIPT_DIR}/www/helloWeb.js"
