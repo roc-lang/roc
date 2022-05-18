2424dedd67c2fc3ee65b5ab55960df4a5bcbb48d
Richard Feldman <oss@rtfeldman.com>
2022-05-17 23:10:33 -0400

    Fix bindgen fixtures with surgical linking

--- bindgen/tests/fixtures/basic-record/src/lib.rs
+++ bindgen/tests/fixtures/basic-record/src/lib.rs
@@ -1,5 +1,4 @@
 mod bindings;
-mod roc_externs;
 
 extern "C" {
     #[link_name = "roc__mainForHost_1_exposed_generic"]
@@ -44,9 +43,51 @@ pub extern "C" fn rust_main() -> i32 {
     0
 }
 
-pub use roc_externs::roc_alloc;
-pub use roc_externs::roc_dealloc;
-pub use roc_externs::roc_memcpy;
-pub use roc_externs::roc_memset;
-pub use roc_externs::roc_panic;
-pub use roc_externs::roc_realloc;
+// Externs required by roc_std and by the Roc app
+
+use core::ffi::c_void;
+use std::ffi::CStr;
+use std::os::raw::c_char;
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_alloc(size: usize, _alignment: u32) -> *mut c_void {
+    return libc::malloc(size);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_realloc(
+    c_ptr: *mut c_void,
+    new_size: usize,
+    _old_size: usize,
+    _alignment: u32,
+) -> *mut c_void {
+    return libc::realloc(c_ptr, new_size);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_dealloc(c_ptr: *mut c_void, _alignment: u32) {
+    return libc::free(c_ptr);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_panic(c_ptr: *mut c_void, tag_id: u32) {
+    match tag_id {
+        0 => {
+            let slice = CStr::from_ptr(c_ptr as *const c_char);
+            let string = slice.to_str().unwrap();
+            eprintln!("Roc hit a panic: {}", string);
+            std::process::exit(1);
+        }
+        _ => todo!(),
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_memcpy(dst: *mut c_void, src: *mut c_void, n: usize) -> *mut c_void {
+    libc::memcpy(dst, src, n)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_memset(dst: *mut c_void, c: i32, n: usize) -> *mut c_void {
+    libc::memset(dst, c, n)
+}
