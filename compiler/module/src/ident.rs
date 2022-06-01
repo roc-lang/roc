32cd3d16d2dc97df87b5e4cadb638ddc9fcd9575
Ayaz Hafiz <ayaz@rwx.com>
2022-06-01 17:13:25 -0500

    Assert sizeof non-wasm

--- compiler/module/src/ident.rs
+++ compiler/module/src/ident.rs
@@ -56,9 +56,8 @@ pub type TagIdIntType = u16;
 #[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct TagName(pub Uppercase);
 
-roc_error_macros::assert_sizeof_aarch64!(TagName, 16);
+roc_error_macros::assert_sizeof_non_wasm!(TagName, 16);
 roc_error_macros::assert_sizeof_wasm!(TagName, 12);
-roc_error_macros::assert_sizeof_default!(TagName, 24);
 
 impl TagName {
     pub fn as_ident_str(&self) -> IdentStr {
