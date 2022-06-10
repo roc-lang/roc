aaaff06a11fbaceeb1d876caa4b71524f6197e9b
Richard Feldman <oss@rtfeldman.com>
2022-06-10 12:42:59 -0400

    wip debugging heap corruption detected

--- bindgen/tests/fixtures/mutually-recursive-union/src/lib.rs
+++ bindgen/tests/fixtures/mutually-recursive-union/src/lib.rs
@@ -30,13 +30,17 @@ pub extern "C" fn rust_main() -> i32 {
     //     }
     // }
 
-    unsafe {
-        dbg!(&tag_union.default.as_Job());
-    }
+    // unsafe {
+    //     dbg!(&tag_union.default.as_Job());
+    // }
 
-    unsafe {
-        dbg!(&tag_union.default.as_Job().inputFiles);
-    }
+    // unsafe {
+    //     dbg!(&tag_union.default.as_Job().inputFiles);
+    // }
+
+    // unsafe {
+    //     dbg!(&tag_union);
+    // }
 
     // Tool : [
     //     SystemTool { name : Str, num : U32 },
@@ -64,20 +68,20 @@ pub extern "C" fn rust_main() -> i32 {
     assert!(tag_union.partial_cmp(&tag_union) == Some(Ordering::Equal)); // PartialOrd
     assert!(tag_union.cmp(&tag_union) == Ordering::Equal); // Ord
 
-    // print!(
-    //     indoc!(
-    //         r#"
-    //             rbt was: {:?}
-    //         "# // `Concat (String "Hello, ") (String "World!")` is: {:?}
-    //            // `String "this is a test"` is: {:?}
-    //     ),
-    //     tag_union,
-    //     // Expr::Concat(
-    //     //     Expr::String("Hello, ".into()),
-    //     //     Expr::String("World!".into()),
-    //     // ),
-    //     // Expr::String("this is a test".into()),
-    // ); // Debug
+    print!(
+        indoc!(
+            r#"
+                rbt was: {:?}
+            "# // `Concat (String "Hello, ") (String "World!")` is: {:?}
+               // `String "this is a test"` is: {:?}
+        ),
+        tag_union,
+        // Expr::Concat(
+        //     Expr::String("Hello, ".into()),
+        //     Expr::String("World!".into()),
+        // ),
+        // Expr::String("this is a test".into()),
+    ); // Debug
 
     let mut set = HashSet::new();
 
--- examples/hello-world/helloWorld.roc
+++ examples/hello-world/helloWorld.roc
@@ -1,11 +1,18 @@
 app "helloWorld"
-    packages { pf: "c-platform" }
+    # packages { pf: "c-platform" }
     # To switch platforms, comment-out the line above and un-comment one below.
-    # packages { pf: "rust-platform" }
+    packages { pf: "rust-platform" }
     # packages { pf: "swift-platform" }
     # packages { pf: "web-platform" } # See ./web-platform/README.md
     # packages { pf: "zig-platform" }
     imports []
     provides [main] to pf
 
-main = "Hello, World!\n"
+main = {
+    default: Job {
+        command: Command {
+            tool: SystemTool { name: "test", num: 42 }
+        },
+        inputFiles : ["foo"]
+    }
+}
