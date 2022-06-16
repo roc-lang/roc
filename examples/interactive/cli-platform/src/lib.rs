37613889381a98c3a7ec34a609861bba55036b73
Richard Feldman <oss@rtfeldman.com>
2022-06-15 21:48:09 -0400

    Add new roc_* functions to examples

--- examples/interactive/cli-platform/src/lib.rs
+++ examples/interactive/cli-platform/src/lib.rs
@@ -70,6 +70,33 @@ pub unsafe extern "C" fn roc_memset(dst: *mut c_void, c: i32, n: usize) -> *mut 
 }
 
 #[no_mangle]
+pub unsafe extern "C" fn roc_shm_open(name: *const i8, oflag: i32, mode: i32) -> i32 {
+    libc::shm_open(name, oflag, mode)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_mmap(
+    addr: *mut c_void,
+    len: usize,
+    prot: i32,
+    flags: i32,
+    fd: i32,
+    offset: i64,
+) -> *mut c_void {
+    libc::mmap(addr, len, prot, flags, fd, offset)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_kill(pid: i32, sig: i32) -> i32 {
+    libc::kill(pid, sig)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_getppid() -> i32 {
+    libc::getppid()
+}
+
+#[no_mangle]
 pub extern "C" fn rust_main() -> i32 {
     let size = unsafe { roc_main_size() } as usize;
     let layout = Layout::array::<u8>(size).unwrap();
@@ -122,4 +149,31 @@ pub extern "C" fn roc_fx_getLine() -> RocStr {
 pub extern "C" fn roc_fx_putLine(line: &RocStr) {
     let string = line.as_str();
     println!("{}", string);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_shm_open(name: *const i8, oflag: i32, mode: i32) -> i32 {
+    libc::shm_open(name, oflag, mode)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_mmap(
+    addr: *mut c_void,
+    len: usize,
+    prot: i32,
+    flags: i32,
+    fd: i32,
+    offset: i64,
+) -> *mut c_void {
+    libc::mmap(addr, len, prot, flags, fd, offset)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_kill(pid: i32, sig: i32) -> i32 {
+    libc::kill(pid, sig)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_getppid() -> i32 {
+    libc::getppid()
 }
