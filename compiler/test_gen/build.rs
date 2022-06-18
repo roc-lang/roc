7727cc7b83f583ef071b642d5add359ecba6f7b1
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-06-18 10:59:11 +0100

    wasm: make sure test_gen/build exists before using it

--- compiler/test_gen/build.rs
+++ compiler/test_gen/build.rs
@@ -25,6 +25,8 @@ fn build_wasm_linking_test_host() {
     println!("cargo:rerun-if-changed={}", LINKING_TEST_HOST_SOURCE);
     println!("cargo:rerun-if-changed={}", LINKING_TEST_IMPORT_SOURCE);
 
+    fs::create_dir("build").unwrap();
+
     if Path::new(LINKING_TEST_HOST_WASM).exists() {
         fs::remove_file(LINKING_TEST_HOST_WASM).unwrap();
     }
