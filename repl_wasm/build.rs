e3380277ebed816f93d9f6132ca86cb0a2f9a321
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-06-19 15:00:11 +0100

    Make repl_wasm more Windows-friendly

--- repl_wasm/build.rs
+++ repl_wasm/build.rs
@@ -1,19 +1,18 @@
 use std::env;
-use std::ffi::OsStr;
+use std::fs;
+use std::ffi::OsString;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
 use roc_builtins::bitcode;
 
 const PLATFORM_FILENAME: &str = "repl_platform";
-const PRE_LINKED_BINARY: &str = "data/pre_linked_binary.o";
+const PRE_LINKED_BINARY: [&str; 2] = ["src", "pre_linked_binary.o"];
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     let source_path = format!("src/{}.c", PLATFORM_FILENAME);
     println!("cargo:rerun-if-changed={}", source_path);
-
-    std::fs::create_dir_all("./data").unwrap();
 
     // Zig can produce *either* an object containing relocations OR an object containing libc code
     // But we want both, so we have to compile twice with different flags, then link them
@@ -28,26 +27,25 @@ fn main() {
     libc_pathbuf.pop();
     let libc_dir = libc_pathbuf.to_str().unwrap();
 
-    let args = &[
-        "wasm-ld",
-        bitcode::BUILTINS_WASM32_OBJ_PATH,
-        &platform_obj,
-        &compiler_rt_obj,
-        "-L",
-        libc_dir,
-        "-lc",
-        "-o",
-        PRE_LINKED_BINARY,
-        "--export-all",
-        "--no-entry",
-        "--relocatable",
-    ];
+    let pre_linked_binary_path: PathBuf = PRE_LINKED_BINARY.iter().collect();
 
-    let zig = zig_executable();
-
-    // println!("{} {}", zig, args.join(" "));
-
-    run_command(&zig, args);
+    Command::new(&zig_executable())
+        .args([
+            "wasm-ld",
+            bitcode::BUILTINS_WASM32_OBJ_PATH,
+            &platform_obj,
+            compiler_rt_obj.to_str().unwrap(),
+            "-L",
+            libc_dir,
+            "-lc",
+            "-o",
+            pre_linked_binary_path.to_str().unwrap(),
+            "--export-all",
+            "--no-entry",
+            "--relocatable",
+        ])
+        .output()
+        .unwrap();
 }
 
 fn zig_executable() -> String {
@@ -60,27 +58,27 @@ fn zig_executable() -> String {
 fn build_wasm_platform(out_dir: &str, source_path: &str) -> String {
     let platform_obj = format!("{}/{}.o", out_dir, PLATFORM_FILENAME);
 
-    run_command(
-        &zig_executable(),
-        &[
+    Command::new(&zig_executable())
+        .args([
             "build-lib",
             "-target",
             "wasm32-wasi",
             "-lc",
             source_path,
             &format!("-femit-bin={}", &platform_obj),
-        ],
-    );
+        ])
+        .output()
+        .unwrap();
 
     platform_obj
 }
 
-fn build_wasm_libc_compilerrt(out_dir: &str, source_path: &str) -> (String, String) {
+fn build_wasm_libc_compilerrt(out_dir: &str, source_path: &str) -> (PathBuf, PathBuf) {
     let zig_cache_dir = format!("{}/zig-cache-wasm32", out_dir);
+    let zig_cache_path = PathBuf::from(&zig_cache_dir);
 
-    run_command(
-        &zig_executable(),
-        &[
+    Command::new(&zig_executable())
+        .args([
             "build-lib",
             "-dynamic", // ensure libc code is actually generated (not just linked against header)
             "-target",
@@ -90,52 +88,33 @@ fn build_wasm_libc_compilerrt(out_dir: &str, source_path: &str) -> (String, Stri
             "-femit-bin=/dev/null",
             "--global-cache-dir",
             &zig_cache_dir,
-        ],
-    );
+        ])
+        .output()
+        .unwrap();
 
-    (
-        run_command("find", &[&zig_cache_dir, "-name", "libc.a"])
-            .split('\n')
-            .next()
-            .unwrap()
-            .to_string(),
-        run_command("find", &[&zig_cache_dir, "-name", "compiler_rt.o"])
-            .split('\n')
-            .next()
-            .unwrap()
-            .to_string(),
-    )
+    let libc_path = find(&zig_cache_path, &OsString::from("libc.a"))
+        .unwrap()
+        .unwrap();
+
+    let compiler_rt_path = find(&zig_cache_path, &OsString::from("compiler_rt.o"))
+        .unwrap()
+        .unwrap();
+
+    (libc_path, compiler_rt_path)
 }
 
-fn run_command(command_str: &str, args: &[&str]) -> String {
-    let output_result = Command::new(OsStr::new(&command_str))
-        .current_dir(Path::new("."))
-        .args(args)
-        .output();
-
-    let fail = |err: String| {
-        panic!(
-            "\n\nFailed command:\n\t{} {}\n\n{}",
-            command_str,
-            args.join(" "),
-            err
-        );
-    };
-
-    match output_result {
-        Ok(output) => match output.status.success() {
-            true => std::str::from_utf8(&output.stdout)
-                .unwrap()
-                .trim()
-                .to_string(),
-            false => {
-                let error_str = match std::str::from_utf8(&output.stderr) {
-                    Ok(stderr) => stderr.to_string(),
-                    Err(_) => format!("Failed to run \"{}\"", command_str),
-                };
-                fail(error_str)
+fn find(dir: &Path, filename: &OsString) -> std::io::Result<Option<PathBuf>> {
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+            let found = find(&path, filename)?;
+            if found.is_some() {
+                return Ok(found);
             }
-        },
-        Err(reason) => fail(reason.to_string()),
+        } else if &entry.file_name() == filename {
+            return Ok(Some(path));
+        }
     }
+    Ok(None)
 }
