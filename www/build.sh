17ceaa007c3d1d0911466649adb0ca264732d3e3
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-06-26 16:11:53 +0100

    www: go back to downloading Web REPL from external URL, to avoid Netlify timeout

--- www/build.sh
+++ www/build.sh
@@ -14,15 +14,21 @@ cp -r public/ build/
 # grab the source code and copy it to Netlify's server; if it's not there, fail the build.
 pushd build
 wget https://github.com/rtfeldman/elm-css/files/8849069/roc-source-code.zip
+
+# Download the latest pre-built Web REPL as a zip file. (Build takes longer than Netlify's timeout.)
+# TODO: When roc repo is public, download it from nightly builds.
+wget https://github.com/brian-carroll/mock-repl/archive/refs/heads/deploy.zip
+unzip deploy.zip
+mv mock-repl-deploy/* .
+rmdir mock-repl-deploy
+rm deploy.zip
+
 popd
 
 pushd ..
 echo 'Generating docs...'
 cargo --version
 rustc --version
-
-# removing `-C link-arg=-fuse-ld=lld` from RUSTFLAGS because this causes an error when compiling `roc_repl_wasm`
-RUSTFLAGS="-C target-cpu=native"
 
 # We set ROC_DOCS_ROOT_DIR=builtins so that links will be generated relative to
 # "/builtins/" rather than "/" - which is what we want based on how the server
@@ -32,11 +38,5 @@ export ROC_DOCS_URL_ROOT=/builtins
 cargo run --bin roc-docs compiler/builtins/roc/*.roc
 mv generated-docs/*.* www/build # move all the .js, .css, etc. files to build/
 mv generated-docs/ www/build/builtins # move all the folders to build/builtins/
-
-echo "Building Web REPL..."
-repl_www/build.sh www/build
-
-echo "Asset sizes:"
-ls -lh www/build/*
 
 popd
