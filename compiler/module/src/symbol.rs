a9c76f7f0ab09a0a69002184b712c3d70280db5d
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2022-07-02 17:15:48 +0200

    upgraded to rust 1.61 + clippy fix

--- compiler/module/src/symbol.rs
+++ compiler/module/src/symbol.rs
@@ -181,7 +181,7 @@ impl fmt::Debug for Symbol {
                     // might be used in a panic error message, and if we panick
                     // while we're already panicking it'll kill the process
                     // without printing any of the errors!
-                    println!("DEBUG INFO: Failed to acquire lock for Debug reading from DEBUG_IDENT_IDS_BY_MODULE_ID, presumably because a thread panicked: {:?}", err);
+                    writeln!(f, "DEBUG INFO: Failed to acquire lock for Debug reading from DEBUG_IDENT_IDS_BY_MODULE_ID, presumably because a thread panicked: {:?}", err)?;
 
                     fallback_debug_fmt(*self, f)
                 }
