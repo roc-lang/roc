91bf84719ff92b94370c104d91d10da2e6d52cec
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2022-07-03 10:28:56 -0700

    correct host.test.js

--- examples/platform-switching/web-assembly-platform/host.test.js
+++ examples/platform-switching/web-assembly-platform/host.test.js
@@ -16,7 +16,7 @@ global.fetch = (filename) =>
 const { roc_web_platform_run } = require("./host");
 
 roc_web_platform_run("./rocLovesWebAssembly.wasm", (string_from_roc) => {
-  const expected = "Hello, World!\n";
+  const expected = "Roc <3 Web Assembly!\n";
   if (string_from_roc !== expected) {
     console.error(`Expected "${expected}", but got "${string_from_roc}"`);
     process.exit(1);
