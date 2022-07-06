20a9a9765a9e484ab856efbb2eb7043217dcc971
Ayaz Hafiz <ayaz@rwx.com>
2022-07-06 08:57:21 -0400

    Disable wasm test for now

--- crates/compiler/test_gen/src/gen_list.rs
+++ crates/compiler/test_gen/src/gen_list.rs
@@ -2960,7 +2960,7 @@ fn with_capacity() {
 }
 
 #[test]
-#[cfg(any(feature = "gen-llvm", feature = "gen-wasm"))]
+#[cfg(any(feature = "gen-llvm"))]
 fn call_function_in_empty_list() {
     assert_evals_to!(
         indoc!(
