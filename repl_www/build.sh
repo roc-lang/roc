5ce8bd89b6051b4df3fc5a61b83ed428d2b79022
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-07-07 23:43:46 +0100

    repl_www: fix build script

--- repl_www/build.sh
+++ repl_www/build.sh
@@ -20,23 +20,23 @@ mkdir -p $WWW_ROOT
 
 # We want a release build, but with debug info (to get stack traces for Wasm backend `todo!()`)
 # This configuration is called `--profiling`
-wasm-pack build --profiling --target web repl_wasm -- --features console_error_panic_hook -v
-cp -v repl_wasm/pkg/roc_repl_wasm.js $WWW_ROOT
+wasm-pack build --profiling --target web crates/repl_wasm -- --features console_error_panic_hook
+cp -v crates/repl_wasm/pkg/roc_repl_wasm.js $WWW_ROOT
 
 # To disable optimizations while debugging, run `REPL_DEBUG=1 repl_www/build.sh`
 if [ "${REPL_DEBUG:-}" == "" ] && which wasm-opt
 then
-    wasm-opt -Os --debuginfo repl_wasm/pkg/roc_repl_wasm_bg.wasm -o $WWW_ROOT/roc_repl_wasm_bg.wasm
+    wasm-opt -Os --debuginfo crates/repl_wasm/pkg/roc_repl_wasm_bg.wasm -o $WWW_ROOT/roc_repl_wasm_bg.wasm
 else
     echo "wasm-opt is not installed. Skipping .wasm optimization."
-    cp -v repl_wasm/pkg/roc_repl_wasm_bg.wasm $WWW_ROOT
+    cp -v crates/repl_wasm/pkg/roc_repl_wasm_bg.wasm $WWW_ROOT
 fi
 
 # Copy the JS from wasm_bindgen, replacing its invalid `import` statement with a `var`.
 # The JS import from the invalid path 'env', seems to be generated when there are unresolved symbols.
 BINDGEN_FILE="roc_repl_wasm.js"
 echo 'var __wbg_star0 = { now: Date.now };' > $WWW_ROOT/$BINDGEN_FILE
-grep -v '^import' repl_wasm/pkg/$BINDGEN_FILE >> $WWW_ROOT/$BINDGEN_FILE
+grep -v '^import' crates/repl_wasm/pkg/$BINDGEN_FILE >> $WWW_ROOT/$BINDGEN_FILE
 
 # Copy static files
 if [[ $WWW_ROOT != repl_www/public ]]
