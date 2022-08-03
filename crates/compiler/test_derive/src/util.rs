429ebbc8306844105eaf39328c72f5b1c4570fb9
Ayaz Hafiz <ayaz@rwx.com>
2022-08-03 14:57:16 -0500

    Have v! macro specify what it needs itself

--- crates/compiler/test_derive/src/util.rs
+++ crates/compiler/test_derive/src/util.rs
@@ -53,82 +53,107 @@ fn module_source_and_path(builtin: DeriveBuiltin) -> (ModuleId, &'static str, Pa
     }
 }
 
-/// Writing out the types into content is inconvenient, so we use a DSL for testing.
+/// DSL for creating [`Content`][crate::subs::Content].
 #[macro_export]
 macro_rules! v {
-    ({ $($field:ident: $make_v:expr,)* $(?$opt_field:ident : $make_opt_v:expr,)* }) => {
-        |subs: &mut Subs| {
-            $(let $field = $make_v(subs);)*
-            $(let $opt_field = $make_opt_v(subs);)*
-            let fields = vec![
-                $( (stringify!($field).into(), RecordField::Required($field)) ,)*
-                $( (stringify!($opt_field).into(), RecordField::Required($opt_field)) ,)*
-            ];
-            let fields = RecordFields::insert_into_subs(subs, fields);
-            synth_var(subs, Content::Structure(FlatType::Record(fields, Variable::EMPTY_RECORD)))
-        }
-    };
-    ([ $($tag:ident $($payload:expr)*),* ]) => {
-        |subs: &mut Subs| {
-            $(
-            let $tag = vec![ $( $payload(subs), )* ];
-            )*
-            let tags = UnionTags::insert_into_subs::<_, Vec<Variable>>(subs, vec![ $( (TagName(stringify!($tag).into()), $tag) ,)* ]);
-            synth_var(subs, Content::Structure(FlatType::TagUnion(tags, Variable::EMPTY_TAG_UNION)))
-        }
-    };
-    ([ $($tag:ident $($payload:expr)*),* ] as $rec_var:ident) => {
-        |subs: &mut Subs| {
-            let $rec_var = subs.fresh_unnamed_flex_var();
-            let rec_name_index =
-                SubsIndex::push_new(&mut subs.field_names, stringify!($rec).into());
+     ({ $($field:ident: $make_v:expr,)* $(?$opt_field:ident : $make_opt_v:expr,)* }) => {{
+         #[allow(unused)]
+         use roc_types::types::RecordField;
+         use roc_types::subs::{Subs, RecordFields, Content, FlatType, Variable};
+         |subs: &mut Subs| {
+             $(let $field = $make_v(subs);)*
+             $(let $opt_field = $make_opt_v(subs);)*
+             let fields = vec![
+                 $( (stringify!($field).into(), RecordField::Required($field)) ,)*
+                 $( (stringify!($opt_field).into(), RecordField::Required($opt_field)) ,)*
+             ];
+             let fields = RecordFields::insert_into_subs(subs, fields);
+             roc_derive::synth_var(subs, Content::Structure(FlatType::Record(fields, Variable::EMPTY_RECORD)))
+         }
+     }};
+     ([ $($tag:ident $($payload:expr)*),* ]$( $ext:tt )?) => {{
+         #[allow(unused)]
+         use roc_types::subs::{Subs, UnionTags, Content, FlatType, Variable};
+         #[allow(unused)]
+         use roc_module::ident::TagName;
+         |subs: &mut Subs| {
+             $(
+             let $tag = vec![ $( $payload(subs), )* ];
+             )*
+             let tags = UnionTags::insert_into_subs::<_, Vec<Variable>>(subs, vec![ $( (TagName(stringify!($tag).into()), $tag) ,)* ]);
+             let mut ext = Variable::EMPTY_TAG_UNION;
+             $( ext = $crate::v!($ext)(subs); )?
+             roc_derive::synth_var(subs, Content::Structure(FlatType::TagUnion(tags, ext)))
+         }
+     }};
+     ([ $($tag:ident $($payload:expr)*),* ] as $rec_var:ident) => {{
+         use roc_types::subs::{Subs, SubsIndex, Variable, Content, FlatType, UnionTags};
+         use roc_module::ident::TagName;
+         |subs: &mut Subs| {
+             let $rec_var = subs.fresh_unnamed_flex_var();
+             let rec_name_index =
+                 SubsIndex::push_new(&mut subs.field_names, stringify!($rec).into());
 
-            $(
-            let $tag = vec![ $( $payload(subs), )* ];
-            )*
-            let tags = UnionTags::insert_into_subs::<_, Vec<Variable>>(subs, vec![ $( (TagName(stringify!($tag).into()), $tag) ,)* ]);
-            let tag_union_var = synth_var(subs, Content::Structure(FlatType::RecursiveTagUnion($rec_var, tags, Variable::EMPTY_TAG_UNION)));
+             $(
+             let $tag = vec![ $( $payload(subs), )* ];
+             )*
+             let tags = UnionTags::insert_into_subs::<_, Vec<Variable>>(subs, vec![ $( (TagName(stringify!($tag).into()), $tag) ,)* ]);
+             let tag_union_var = roc_derive::synth_var(subs, Content::Structure(FlatType::RecursiveTagUnion($rec_var, tags, Variable::EMPTY_TAG_UNION)));
 
-            subs.set_content(
-                $rec_var,
-                Content::RecursionVar {
-                    structure: tag_union_var,
-                    opt_name: Some(rec_name_index),
-                },
-            );
-            tag_union_var
-        }
-    };
-    (Symbol::$sym:ident $($arg:expr)*) => {
-        |subs: &mut Subs| {
-            let $sym = vec![ $( $arg(subs) ,)* ];
-            let var_slice = SubsSlice::insert_into_subs(subs, $sym);
-            synth_var(subs, Content::Structure(FlatType::Apply(Symbol::$sym, var_slice)))
-        }
-    };
-    (Symbol::$alias:ident $($arg:expr)* => $real_var:expr) => {
-        |subs: &mut Subs| {
-            let args = vec![$( $arg(subs) )*];
-            let alias_variables = AliasVariables::insert_into_subs::<Vec<_>, Vec<_>>(subs, args, vec![]);
-            let real_var = $real_var(subs);
-            synth_var(subs, Content::Alias(Symbol::$alias, alias_variables, real_var, AliasKind::Structural))
-        }
-    };
-    (@Symbol::$alias:ident $($arg:expr)* => $real_var:expr) => {
-        |subs: &mut Subs| {
-            let args = vec![$( $arg(subs) )*];
-            let alias_variables = AliasVariables::insert_into_subs::<Vec<_>, Vec<_>>(subs, args, vec![]);
-            let real_var = $real_var(subs);
-            synth_var(subs, Content::Alias(Symbol::$alias, alias_variables, real_var, AliasKind::Opaque))
-        }
-    };
-    (*$rec_var:ident) => {
-        |_: &mut Subs| { $rec_var }
-    };
-    ($var:ident) => {
-        |_: &mut Subs| { Variable::$var }
-    };
-}
+             subs.set_content(
+                 $rec_var,
+                 Content::RecursionVar {
+                     structure: tag_union_var,
+                     opt_name: Some(rec_name_index),
+                 },
+             );
+             tag_union_var
+         }
+     }};
+     (Symbol::$sym:ident $($arg:expr)*) => {{
+         use roc_types::subs::{Subs, SubsSlice, Content, FlatType};
+         use roc_module::symbol::Symbol;
+         |subs: &mut Subs| {
+             let $sym = vec![ $( $arg(subs) ,)* ];
+             let var_slice = SubsSlice::insert_into_subs(subs, $sym);
+             roc_derive::synth_var(subs, Content::Structure(FlatType::Apply(Symbol::$sym, var_slice)))
+         }
+     }};
+     (Symbol::$alias:ident $($arg:expr)* => $real_var:expr) => {{
+         use roc_types::subs::{Subs, AliasVariables, Content};
+         use roc_types::types::AliasKind;
+         use roc_module::symbol::Symbol;
+         |subs: &mut Subs| {
+             let args = vec![$( $arg(subs) )*];
+             let alias_variables = AliasVariables::insert_into_subs::<Vec<_>, Vec<_>>(subs, args, vec![]);
+             let real_var = $real_var(subs);
+             roc_derive::synth_var(subs, Content::Alias(Symbol::$alias, alias_variables, real_var, AliasKind::Structural))
+         }
+     }};
+     (@Symbol::$alias:ident $($arg:expr)* => $real_var:expr) => {{
+         use roc_types::subs::{Subs, AliasVariables, Content};
+         use roc_types::types::AliasKind;
+         use roc_module::symbol::Symbol;
+         |subs: &mut Subs| {
+             let args = vec![$( $arg(subs) )*];
+             let alias_variables = AliasVariables::insert_into_subs::<Vec<_>, Vec<_>>(subs, args, vec![]);
+             let real_var = $real_var(subs);
+             roc_derive::synth_var(subs, Content::Alias(Symbol::$alias, alias_variables, real_var, AliasKind::Opaque))
+         }
+     }};
+     (*) => {{
+         use roc_types::subs::{Subs, Content};
+         |subs: &mut Subs| { roc_derive::synth_var(subs, Content::FlexVar(None)) }
+     }};
+     (^$rec_var:ident) => {{
+         use roc_types::subs::{Subs};
+         |_: &mut Subs| { $rec_var }
+     }};
+     ($var:ident) => {{
+         use roc_types::subs::{Subs};
+         |_: &mut Subs| { Variable::$var }
+     }};
+ }
 
 pub(crate) fn check_key<S1, S2>(builtin: DeriveBuiltin, eq: bool, synth1: S1, synth2: S2)
 where
