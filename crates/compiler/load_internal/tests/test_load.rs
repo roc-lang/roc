0c284f2bbf509897668089fdad4377255e16e314
Folkert <folkert@folkertdev.nl>
2022-08-09 15:44:08 +0200

    clippy

--- crates/compiler/load_internal/tests/test_load.rs
+++ crates/compiler/load_internal/tests/test_load.rs
@@ -278,6 +278,10 @@ fn expect_types(mut loaded_module: LoadedModule, mut expected_types: HashMap<&st
                 // at least at the moment this does not happen
                 panic!("Unexpected expectation in module declarations");
             }
+            ExpectationFx => {
+                // at least at the moment this does not happen
+                panic!("Unexpected expectation in module declarations");
+            }
         };
     }
 
@@ -363,7 +367,7 @@ fn interface_with_deps() {
                 def_count += 1;
             }
             MutualRecursion { .. } => { /* do nothing, not a def */ }
-            Expectation => { /* do nothing, not a def */ }
+            Expectation | ExpectationFx => { /* do nothing, not a def */ }
         }
     }
 
