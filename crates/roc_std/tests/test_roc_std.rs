31390c8ba34ef0565555dac17cc27cfbb7625479
Brian Hicks <brian@brianthicks.com>
2022-08-10 17:32:31 -0500

    implement Serialize/Deserialize for RocList

--- crates/roc_std/tests/test_roc_std.rs
+++ crates/roc_std/tests/test_roc_std.rs
@@ -180,6 +180,31 @@ mod test_roc_std {
     }
 
     #[test]
+    #[cfg(feature = "serde")]
+    fn short_list_roundtrip() {
+        let items: [u8; 4] = [1, 3, 3, 7];
+        let orig = RocList::from_slice(&items);
+
+        let serialized = serde_json::to_string(&orig).expect("failed to serialize string");
+        let deserialized =
+            serde_json::from_str::<RocList<u8>>(&serialized).expect("failed to deserialize string");
+
+        assert_eq!(orig, deserialized);
+    }
+
+    #[test]
+    #[cfg(feature = "serde")]
+    fn long_list_roundtrip() {
+        let orig = RocList::from_iter(1..100);
+
+        let serialized = serde_json::to_string(&orig).expect("failed to serialize string");
+        let deserialized =
+            serde_json::from_str::<RocList<u8>>(&serialized).expect("failed to deserialize string");
+
+        assert_eq!(orig, deserialized);
+    }
+
+    #[test]
     fn list_from_iter() {
         let elems: [i64; 5] = [1, 2, 3, 4, 5];
         let from_slice = RocList::from_slice(&elems);
