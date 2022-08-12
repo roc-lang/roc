f95c5aee12fdea0a77524c3b2aa024762abd2586
Ayaz Hafiz <ayaz@rwx.com>
2022-08-12 15:12:18 -0700

    Check in derived record impl

--- crates/compiler/test_derive/src/decoding.rs
+++ crates/compiler/test_derive/src/decoding.rs
@@ -54,6 +54,60 @@ fn list() {
 fn record_2_fields() {
     derive_test(Decoder, v!({first: v!(STR), second: v!(STR),}), |golden| {
         assert_snapshot!(golden, @r###"
+        # derived for { first : Str, second : Str }
+        # Decoder { first : val, second : val1 } fmt | fmt has DecoderFormatting, val has Decoding, val1 has Decoding
+        # List U8, fmt -[[22(22)]]-> { rest : List U8, result : [Err [TooShort], Ok { first : val, second : val1 }] } | fmt has DecoderFormatting, val has Decoding, val1 has Decoding
+        # Specialization lambda sets:
+        #   @<1>: [[22(22)]]
+        #Derived.decoder_{first,second} =
+          Decode.custom
+            \#Derived.bytes3, #Derived.fmt3 ->
+              Decode.decodeWith
+                #Derived.bytes3
+                (Decode.record
+                  { second: Err NoField, first: Err NoField }
+                  \#Derived.stateRecord2, #Derived.field ->
+                    when #Derived.field is
+                      "first" ->
+                        Keep (Decode.custom
+                          \#Derived.bytes, #Derived.fmt ->
+                            when Decode.decodeWith
+                                #Derived.bytes
+                                Decode.decoder
+                                #Derived.fmt is
+                              #Derived.rec ->
+                                {
+                                  result: when #Derived.rec.result is
+                                      Ok #Derived.val ->
+                                        Ok { stateRecord2 & first: Ok #Derived.val }
+                                      Err #Derived.err -> Err #Derived.err,
+                                  rest: #Derived.rec.rest
+                                })
+                      "second" ->
+                        Keep (Decode.custom
+                          \#Derived.bytes2, #Derived.fmt2 ->
+                            when Decode.decodeWith
+                                #Derived.bytes2
+                                Decode.decoder
+                                #Derived.fmt2 is
+                              #Derived.rec2 ->
+                                {
+                                  result: when #Derived.rec2.result is
+                                      Ok #Derived.val2 ->
+                                        Ok { stateRecord2 & second: Ok #Derived.val2 }
+                                      Err #Derived.err2 -> Err #Derived.err2,
+                                  rest: #Derived.rec2.rest
+                                })
+                      _ -> Skip
+                  \#Derived.stateRecord ->
+                    when #Derived.stateRecord.first is
+                      Ok #Derived.first ->
+                        when #Derived.stateRecord.second is
+                          Ok #Derived.second ->
+                            Ok { second: #Derived.second, first: #Derived.first }
+                          _ -> Err TooShort
+                      _ -> Err TooShort)
+                #Derived.fmt3
         "###
         )
     })
