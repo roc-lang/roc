3fbe7e690270b950825d84bb88a7422e5733d2de
Folkert <folkert@folkertdev.nl>
2022-08-17 21:13:14 +0200

    have glue compile and run a dylib

--- crates/glue/src/load.rs
+++ crates/glue/src/load.rs
@@ -1,7 +1,15 @@
 use crate::rust_glue;
 use crate::types::{Env, Types};
 use bumpalo::Bump;
+use inkwell::context::Context;
+use roc_collections::MutSet;
+use roc_gen_llvm::llvm::build::LlvmBackendMode;
+use roc_gen_llvm::llvm::externs::add_default_roc_externs;
+use roc_gen_llvm::run_roc::RocCallResult;
+use roc_gen_llvm::run_roc_dylib;
+use roc_load::{EntryPoint, MonomorphizedModule};
 use roc_load::{ExecutionMode, LoadConfig, LoadedModule, LoadingProblem, Threading};
+use roc_mono::ir::OptLevel;
 use roc_reporting::report::RenderTarget;
 use roc_target::{Architecture, OperatingSystem, TargetInfo};
 use std::fs::File;
@@ -16,6 +24,10 @@ pub fn generate(
     output_path: &Path,
     opt_script: Option<&Path>,
 ) -> io::Result<i32> {
+    if let Some(script) = opt_script {
+        return generate_script(input_path, output_path, script);
+    }
+
     match load_types(input_path.to_path_buf(), Threading::AllAvailable) {
         Ok(types_and_targets) => {
             let mut file = File::create(output_path).unwrap_or_else(|err| {
@@ -65,6 +77,161 @@ pub fn generate(
             }
         },
     }
+}
+
+use core::ffi::c_void;
+
+/// # Safety
+/// The Roc application needs this.
+#[no_mangle]
+pub unsafe fn roc_alloc(size: usize, _alignment: u32) -> *mut c_void {
+    libc::malloc(size)
+}
+
+/// # Safety
+/// The Roc application needs this.
+#[no_mangle]
+pub unsafe fn roc_realloc(
+    c_ptr: *mut c_void,
+    new_size: usize,
+    _old_size: usize,
+    _alignment: u32,
+) -> *mut c_void {
+    libc::realloc(c_ptr, new_size)
+}
+
+/// # Safety
+/// The Roc application needs this.
+#[no_mangle]
+pub unsafe fn roc_dealloc(c_ptr: *mut c_void, _alignment: u32) {
+    libc::free(c_ptr)
+}
+
+fn generate_script(input_path: &Path, output_path: &Path, script: &Path) -> io::Result<i32> {
+    let arena = bumpalo::Bump::new();
+
+    let load_config = LoadConfig {
+        target_info: TargetInfo::default_x86_64(),
+        render: RenderTarget::ColorTerminal,
+        threading: Threading::AllAvailable,
+        exec_mode: ExecutionMode::Executable,
+    };
+
+    let loaded =
+        roc_load::load_and_monomorphize(&arena, script.to_owned(), Default::default(), load_config)
+            .unwrap();
+
+    let (lib, main_fn_name) = mono_module_to_dylib(
+        &arena,
+        Triple::host(),
+        loaded,
+        OptLevel::Normal,
+        LlvmBackendMode::CliTest,
+    )
+    .unwrap();
+
+    use roc_std::RocStr;
+    let main_function = run_roc_dylib!(arena.alloc(lib), main_fn_name, &RocStr, RocStr);
+
+    let mut main_result = RocCallResult::default();
+    let input = RocStr::from("foo bar baz");
+
+    unsafe { main_function(&input, &mut main_result) };
+
+    let result: Result<_, _> = main_result.into();
+
+    dbg!(result);
+
+    Ok(0)
+}
+
+fn mono_module_to_dylib<'a>(
+    arena: &'a Bump,
+    target: Triple,
+    loaded: MonomorphizedModule<'a>,
+    opt_level: OptLevel,
+    mode: LlvmBackendMode,
+) -> Result<(libloading::Library, String), libloading::Error> {
+    let target_info = TargetInfo::from(&target);
+
+    let MonomorphizedModule {
+        toplevel_expects,
+        procedures,
+        entry_point,
+        interns,
+        ..
+    } = loaded;
+
+    let context = Context::create();
+    let builder = context.create_builder();
+    let module = arena.alloc(roc_gen_llvm::llvm::build::module_from_builtins(
+        &target, &context, "",
+    ));
+
+    let module = arena.alloc(module);
+    let (module_pass, _function_pass) =
+        roc_gen_llvm::llvm::build::construct_optimization_passes(module, opt_level);
+
+    let (dibuilder, compile_unit) = roc_gen_llvm::llvm::build::Env::new_debug_info(module);
+
+    // Compile and add all the Procs before adding main
+    let env = roc_gen_llvm::llvm::build::Env {
+        arena,
+        builder: &builder,
+        dibuilder: &dibuilder,
+        compile_unit: &compile_unit,
+        context: &context,
+        interns,
+        module,
+        target_info,
+        mode,
+        // important! we don't want any procedures to get the C calling convention
+        exposed_to_host: MutSet::default(),
+    };
+
+    // Add roc_alloc, roc_realloc, and roc_dealloc, since the repl has no
+    // platform to provide them.
+    add_default_roc_externs(&env);
+
+    let opt_entry_point = match entry_point {
+        EntryPoint::Executable { symbol, layout, .. } => {
+            Some(roc_mono::ir::EntryPoint { symbol, layout })
+        }
+        EntryPoint::Test => None,
+    };
+
+    let (main_name, _) = roc_gen_llvm::llvm::build::build_procedures_return_main(
+        &env,
+        opt_level,
+        procedures,
+        opt_entry_point.unwrap(),
+    );
+
+    env.dibuilder.finalize();
+
+    // we don't use the debug info, and it causes weird errors.
+    module.strip_debug_info();
+
+    // Uncomment this to see the module's un-optimized LLVM instruction output:
+    // env.module.print_to_stderr();
+
+    module_pass.run_on(env.module);
+
+    // Uncomment this to see the module's optimized LLVM instruction output:
+    // env.module.print_to_stderr();
+
+    // Verify the module
+    if let Err(errors) = env.module.verify() {
+        let path = std::env::temp_dir().join("test.ll");
+        env.module.print_to_file(&path).unwrap();
+        panic!(
+            "Errors defining module:\n{}\n\nUncomment things nearby to see more details. IR written to `{:?}`",
+            errors.to_string(), path,
+        );
+    }
+
+    roc_build::link::llvm_module_to_dylib(env.module, &target, opt_level)
+        .map(|lib| (lib, main_name.to_owned()))
 }
 
 pub fn load_types(
