1f778db26c17cf09a138ba0427840b9c07488f43
Folkert <folkert@folkertdev.nl>
2022-08-25 21:50:51 +0200

    define roc_alloc and friends in cli/src/lib.rs on windows

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -1191,3 +1191,37 @@ impl std::str::FromStr for Target {
         }
     }
 }
+
+#[cfg(windows)]
+use windows_roc_platform_functions::*;
+
+#[cfg(windows)]
+mod windows_roc_platform_functions {
+    use core::ffi::c_void;
+
+    /// # Safety
+    /// The Roc application needs this.
+    #[no_mangle]
+    pub unsafe fn roc_alloc(size: usize, _alignment: u32) -> *mut c_void {
+        libc::malloc(size)
+    }
+
+    /// # Safety
+    /// The Roc application needs this.
+    #[no_mangle]
+    pub unsafe fn roc_realloc(
+        c_ptr: *mut c_void,
+        new_size: usize,
+        _old_size: usize,
+        _alignment: u32,
+    ) -> *mut c_void {
+        libc::realloc(c_ptr, new_size)
+    }
+
+    /// # Safety
+    /// The Roc application needs this.
+    #[no_mangle]
+    pub unsafe fn roc_dealloc(c_ptr: *mut c_void, _alignment: u32) {
+        libc::free(c_ptr)
+    }
+}
