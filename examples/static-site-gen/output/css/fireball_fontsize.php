aeac8035c1fde3458bdcb41819e7270637558d8c
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-09-05 18:43:32 +0100

    Delete example (open license is for markdown not for the site)

--- examples/static-site-gen/output/css/fireball_fontsize.php
+++
@@ -1,4 +0,0 @@
-body   { font-size: 12px; }
-@media only screen and (max-device-width: 480px)  { 
-	body { font-size: 17px; }
-}
--- examples/static-site-gen/output/css/ie_sucks.css
+++
@@ -1,5 +0,0 @@
-html>body div#Sidebar {
-	/* Set the real margins, hidden from IE/Win: */
-	top: 189px;
-	margin: 0;
-	}
