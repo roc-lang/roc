c1677eed55e3418121f8300ed35b88e5b5200cd4
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2022-09-06 10:50:52 +0200

    zig build verbose
    Signed-off-by: Anton-4 <17049058+Anton-4@users.noreply.github.com>

--- crates/compiler/builtins/build.rs
+++ crates/compiler/builtins/build.rs
@@ -92,7 +92,7 @@ fn generate_object_file(bitcode_path: &Path, zig_object: &str, object_file_name:
         run_command(
             &bitcode_path,
             &zig_executable(),
-            &["build", zig_object, "-Drelease=true"],
+            &["build", zig_object, "-Drelease=true", "--verbose"],
             0,
         );
 
