564de99a43b22c2e8d0f31a11d9d664e252196ef
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-09-08 23:09:04 +0100

    gen_wasm: Allow host's final Custom sections to be in any order

--- crates/wasm_module/src/lib.rs
+++ crates/wasm_module/src/lib.rs
@@ -144,10 +144,36 @@ impl<'a> WasmModule<'a> {
         let _data_count = OpaqueSection::parse((arena, SectionId::DataCount), bytes, &mut cursor)?;
         let code = CodeSection::parse(arena, bytes, &mut cursor)?;
         let data = DataSection::parse(arena, bytes, &mut cursor)?;
-        let linking = LinkingSection::parse(arena, bytes, &mut cursor)?;
-        let reloc_code = RelocationSection::parse((arena, "reloc.CODE"), bytes, &mut cursor)?;
-        let reloc_data = RelocationSection::parse((arena, "reloc.DATA"), bytes, &mut cursor)?;
-        let names = NameSection::parse(arena, bytes, &mut cursor)?;
+
+        // Initialise the Custom sections that we care about. All empty.
+        let mut linking = LinkingSection::new(arena);
+        let mut reloc_code = RelocationSection::new(arena, "reloc.CODE");
+        let mut reloc_data = RelocationSection::new(arena, "reloc.DATA");
+        let mut names = NameSection::new(arena);
+
+        // Consume all remaining Custom sections
+        while let Ok((section_name, section_end)) = Self::peek_custom_section(arena, bytes, cursor)
+        {
+            match section_name {
+                "linking" => {
+                    linking = LinkingSection::parse(arena, bytes, &mut cursor)?;
+                }
+                "reloc.CODE" => {
+                    reloc_code =
+                        RelocationSection::parse((arena, "reloc.CODE"), bytes, &mut cursor)?;
+                }
+                "reloc.DATA" => {
+                    reloc_data =
+                        RelocationSection::parse((arena, "reloc.DATA"), bytes, &mut cursor)?;
+                }
+                "name" => {
+                    names = NameSection::parse(arena, bytes, &mut cursor)?;
+                }
+                _ => {
+                    cursor = section_end;
+                }
+            }
+        }
 
         let mut module_errors = String::new();
         if types.is_empty() {
@@ -205,6 +231,37 @@ impl<'a> WasmModule<'a> {
             reloc_data,
             names,
         })
+    }
+
+    fn peek_custom_section<'b>(
+        arena: &'a Bump,
+        module_bytes: &[u8],
+        immutable_cursor: usize,
+    ) -> Result<(&'a str, usize), ParseError> {
+        let mut cursor = immutable_cursor;
+
+        if cursor >= module_bytes.len() {
+            return Err(ParseError {
+                message: "EOF".into(),
+                offset: cursor,
+            });
+        }
+        if module_bytes[cursor] != SectionId::Custom as u8 {
+            return Err(ParseError {
+                message: format!(
+                    "Expected Custom section but found section ID 0x{:02x}",
+                    module_bytes[cursor]
+                ),
+                offset: cursor,
+            });
+        }
+        cursor += 1;
+
+        let section_size = u32::parse((), module_bytes, &mut cursor)?;
+        let section_end = cursor + section_size as usize;
+        let section_name = <&'a str>::parse(arena, module_bytes, &mut cursor)?;
+
+        Ok((section_name, section_end))
     }
 
     pub fn eliminate_dead_code(&mut self, arena: &'a Bump, called_fns: BitVec<usize>) {
