24feec62b858936baf8ae5a1ca053a158f1282a3
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-09-08 19:55:21 +0100

    builtins: Make Zig test scripts use ROC_ZIG env var

--- crates/compiler/builtins/bitcode/run-tests.sh
+++ crates/compiler/builtins/bitcode/run-tests.sh
@@ -4,7 +4,7 @@
 set -euxo pipefail
 
 # Execute zig tests (see build.zig)
-zig build test
+${ROC_ZIG:-zig} build test
 
 # check formatting of zig files
 find src/*.zig -type f -print0 | xargs -n 1 -0 zig fmt --check || (echo "zig fmt --check FAILED! Check the previous lines to see which files were improperly formatted." && exit 1)
