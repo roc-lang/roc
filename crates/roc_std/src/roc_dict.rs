98a8cc02e6e834ae8471605628831d235d56e1b9
Brian Hicks <brian@brianthicks.com>
2022-09-14 15:36:32 -0500

    follow Roc's layout rules for RocDictItem

--- crates/roc_std/src/roc_dict.rs
+++ crates/roc_std/src/roc_dict.rs
@@ -2,16 +2,11 @@ use crate::roc_list::RocList;
 use core::{
     fmt::{self, Debug},
     hash::Hash,
+    mem::{align_of, ManuallyDrop},
 };
 
 #[derive(Default, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct RocDict<K, V>(RocList<RocDictItem<K, V>>);
-
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct RocDictItem<K, V> {
-    key: K,
-    value: V,
-}
 
 impl<K, V> RocDict<K, V> {
     pub fn len(&self) -> usize {
@@ -27,15 +22,15 @@ impl<K, V> RocDict<K, V> {
     }
 
     pub fn iter(&self) -> impl Iterator<Item = (&K, &V)> {
-        self.0.iter().map(|item| (&item.key, &item.value))
+        self.0.iter().map(|item| (item.key(), item.value()))
     }
 
     pub fn iter_keys(&self) -> impl Iterator<Item = &K> {
-        self.0.iter().map(|item| &item.key)
+        self.0.iter().map(|item| item.key())
     }
 
     pub fn iter_values(&self) -> impl Iterator<Item = &V> {
-        self.0.iter().map(|item| &item.value)
+        self.0.iter().map(|item| item.value())
     }
 }
 
@@ -83,7 +78,7 @@ impl<'a, K, V> Iterator for IntoIter<'a, K, V> {
             .items
             .0
             .get(self.index)
-            .map(|item| (&item.key, &item.value));
+            .map(|item| (item.key(), item.value()));
 
         self.index += 1;
 
@@ -104,5 +99,73 @@ impl<K: Debug, V: Debug> Debug for RocDict<K, V> {
         f.debug_map()
             .entries(self.iter().map(|(k, v)| (k, v)))
             .finish()
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(C)]
+struct KeyFirst<K, V> {
+    key: K,
+    value: V,
+}
+
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(C)]
+struct ValueFirst<K, V> {
+    value: V,
+    key: K,
+}
+
+#[derive(Eq)]
+union RocDictItem<K, V> {
+    key_first: ManuallyDrop<KeyFirst<K, V>>,
+    value_first: ManuallyDrop<ValueFirst<K, V>>,
+}
+
+impl<K, V> RocDictItem<K, V> {
+    fn key(&self) -> &K {
+        if align_of::<K>() >= align_of::<V>() {
+            unsafe { &self.key_first.key }
+        } else {
+            unsafe { &self.value_first.key }
+        }
+    }
+
+    fn value(&self) -> &V {
+        if align_of::<K>() >= align_of::<V>() {
+            unsafe { &self.key_first.value }
+        } else {
+            unsafe { &self.value_first.value }
+        }
+    }
+}
+
+impl<K: PartialEq, V: PartialEq> PartialEq for RocDictItem<K, V> {
+    fn eq(&self, other: &Self) -> bool {
+        self.key() == other.key() && self.value() == other.value()
+    }
+}
+
+impl<K: PartialOrd, V: PartialOrd> PartialOrd for RocDictItem<K, V> {
+    fn partial_cmp(&self, other: &Self) -> Option<core::cmp::Ordering> {
+        self.key()
+            .partial_cmp(other.key())
+            .map(|key_cmp| match self.value().partial_cmp(other.value()) {
+                Some(value_cmp) => key_cmp.then(value_cmp),
+                None => key_cmp,
+            })
+    }
+}
+
+impl<K: Ord, V: Ord> Ord for RocDictItem<K, V> {
+    fn cmp(&self, other: &Self) -> core::cmp::Ordering {
+        self.key().cmp(other.key()).then(self.value().cmp(other.value()))
+    }
+}
+
+impl<K: Hash, V: Hash> Hash for RocDictItem<K, V> {
+    fn hash<H: core::hash::Hasher>(&self, state: &mut H) {
+        self.key().hash(state);
+        self.value().hash(state);
     }
 }
