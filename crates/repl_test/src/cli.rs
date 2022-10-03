a46d4fa1c64a54d5df10e6a28752198bf54a1313
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2022-10-03 15:31:34 +0200

    further improved tips UX, fixed cli.rs assert

--- crates/repl_test/src/cli.rs
+++ crates/repl_test/src/cli.rs
@@ -3,7 +3,7 @@ use std::io::Write;
 use std::path::PathBuf;
 use std::process::{Command, ExitStatus, Stdio};
 
-use roc_repl_cli::{INSTRUCTIONS, WELCOME_MESSAGE};
+use roc_repl_cli::{TIPS, WELCOME_MESSAGE};
 use roc_test_utils::assert_multiline_str_eq;
 
 const ERROR_MESSAGE_START: char = '─';
@@ -75,7 +75,7 @@ fn repl_eval(input: &str) -> Out {
 
     // Remove the initial instructions from the output.
 
-    let expected_instructions = format!("{}{}", WELCOME_MESSAGE, INSTRUCTIONS);
+    let expected_instructions = format!("{}{}", WELCOME_MESSAGE, TIPS);
     let stdout = String::from_utf8(output.stdout).unwrap();
 
     assert!(
