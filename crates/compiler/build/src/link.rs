d93902cd1815e0277b9e10fbe056f02468665570
Folkert <folkert@folkertdev.nl>
2022-10-17 21:22:13 +0200

    fix compilation on windows for windows

--- crates/compiler/build/src/link.rs
+++ crates/compiler/build/src/link.rs
@@ -203,7 +203,7 @@ pub fn build_zig_host_native(
             "build-exe",
             "-fPIE",
             shared_lib_path.to_str().unwrap(),
-            &bitcode::get_builtins_host_obj_path(),
+            &bitcode::get_builtins_windows_obj_path(),
         ]);
     } else {
         command.args(&["build-obj", "-fPIC"]);
@@ -221,6 +221,7 @@ pub fn build_zig_host_native(
         // include libc
         "--library",
         "c",
+        "-dynamic",
         // cross-compile?
         "-target",
         target,
