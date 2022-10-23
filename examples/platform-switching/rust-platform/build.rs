0b1a0f113333f5a6b465a75daf8f093a9cc894bf
Folkert <folkert@folkertdev.nl>
2022-10-23 13:10:44 +0200

    import lib with cc

--- examples/platform-switching/rust-platform/build.rs
+++ examples/platform-switching/rust-platform/build.rs
@@ -2,5 +2,5 @@ fn main() {
     // println!("cargo:rustc-link-lib=dylib=libapp");
     // println!("cargo:rustc-link-search=.");
 
-    cc::Build::new().file("libapp.dll").compile("app");
+    cc::Build::new().object("foo.lib").compile("app");
 }
