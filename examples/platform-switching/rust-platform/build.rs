ecb11a7583b1395d712ff194e1287d13f6e9c72b
Folkert <folkert@folkertdev.nl>
2022-10-23 12:44:10 +0200

    try cc crate

--- examples/platform-switching/rust-platform/build.rs
+++ examples/platform-switching/rust-platform/build.rs
@@ -1,4 +1,6 @@
 fn main() {
-    println!("cargo:rustc-link-lib=dylib=libapp");
-    println!("cargo:rustc-link-search=.");
+    // println!("cargo:rustc-link-lib=dylib=libapp");
+    // println!("cargo:rustc-link-search=.");
+
+    cc::Build::new().file("libapp.dll").compile("app");
 }
