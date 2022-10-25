85e58440dee27baa472e80d7530cff934f36ee88
Ayaz Hafiz <ayaz@rwx.com>
2022-10-25 18:18:33 -0500

    Allow too many args

--- crates/compiler/types/src/pretty_print.rs
+++ crates/compiler/types/src/pretty_print.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::too_many_arguments)]
+
 use crate::subs::{
     self, AliasVariables, Content, FlatType, GetSubsSlice, Label, Subs, SubsIndex, UnionLabels,
     UnionTags, UnsortedUnionLabels, Variable,
