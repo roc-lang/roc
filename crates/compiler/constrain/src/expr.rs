ce7648b344b9ce971cb2e9092dc177c7f2f91e5b
Ayaz Hafiz <ayaz@rwx.com>
2022-10-25 11:43:17 -0500

    Coalesce type signature vars in typed def constraining

--- crates/compiler/constrain/src/expr.rs
+++ crates/compiler/constrain/src/expr.rs
@@ -2340,7 +2340,6 @@ fn constrain_typed_def(
         rigids: ftv,
     };
 
-    // TODO there is a signature_index below, can we coalesce it here?
     let signature_index = constraints.push_type(signature.clone());
 
     let annotation_expected = FromAnnotation(
@@ -2456,7 +2455,6 @@ fn constrain_typed_def(
                     signature_closure_type_index,
                 ))
             };
-            let signature_index = constraints.push_type(signature);
             let cons = [
                 constraints.let_constraint(
                     [],
