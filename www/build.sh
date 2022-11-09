338de0ac41add6d2dd180da4fba292f7c560eace
Richard Feldman <oss@rtfeldman.com>
2022-11-09 09:55:18 -0500

    wip download fonts in build.sh

--- www/build.sh
+++ www/build.sh
@@ -3,6 +3,11 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euxo pipefail
 
+# download fonts just-in-time so we don't have to bloat the repo with them.
+# TODO convert .woff fonts to .woff2
+# wget -O SourceCodePro.woff https://fonts.gstatic.com/s/sourcecodepro/v22/HI_diYsKILxRpg3hIP6sJ7fM7PqPMcMnZFqUwX28DMyQtMdrSlcZZJmOpwVS.woff
+# wget -O lato.woff2 https://fonts.gstatic.com/s/lato/v23/S6uyw4BMUTPHjx4wXiWtFCc.woff2
+
 # cd into the directory where this script lives.
 # This allows us to run this script from the root project directory,
 # which is what Netlify wants to do.
