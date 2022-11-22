7865075f768484d27a60ddea993c27312bca9188
Ayaz Hafiz <ayaz@rwx.com>
2022-11-22 12:57:32 -0600

    Mark mono test

--- crates/compiler/test_mono/src/tests.rs
+++ crates/compiler/test_mono/src/tests.rs
@@ -2005,7 +2005,8 @@ fn match_list() {
 }
 
 #[mono_test]
-fn foo1() {
+#[ignore = "https://github.com/roc-lang/roc/issues/4561"]
+fn recursive_function_and_union_with_inference_hole() {
     let _tracing_guards = roc_tracing::setup_tracing!();
 
     indoc!(
