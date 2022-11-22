16fd39c4f7d1b5957adeda5a95e77eddf4dffd6a
Richard Feldman <oss@rtfeldman.com>
2022-11-22 18:51:01 -0500

    Missed a couple of .to_str().unwrap()s

--- crates/compiler/build/src/link.rs
+++ crates/compiler/build/src/link.rs
@@ -155,7 +155,7 @@ pub fn build_zig_host_native(
             "-fPIE",
             "-rdynamic", // make sure roc_alloc and friends are exposed
             shared_lib_path.to_str().unwrap(),
-            builtins_host_file.path(),
+            builtins_host_file.path().to_str().unwrap(),
         ]);
     } else {
         zig_cmd.args(["build-obj", "-fPIC"]);
