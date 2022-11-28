46737b4a41d8ddfbd54e8634affe38b10e7e1456
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-11-28 21:32:12 +0000

    wasm_interp: rename test_opcodes -> test_basics, and split out test_convert

---
+++ crates/wasm_interp/tests/test_convert.rs
@@ -0,0 +1,78 @@
+#![cfg(test)]
+
+use roc_wasm_interp::test_utils::test_op_example;
+
+// #[test]
+// fn test_i32wrapi64() {}
+
+// #[test]
+// fn test_i32truncsf32() {}
+
+// #[test]
+// fn test_i32truncuf32() {}
+
+// #[test]
+// fn test_i32truncsf64() {}
+
+// #[test]
+// fn test_i32truncuf64() {}
+
+// #[test]
+// fn test_i64extendsi32() {}
+
+// #[test]
+// fn test_i64extendui32() {}
+
+// #[test]
+// fn test_i64truncsf32() {}
+
+// #[test]
+// fn test_i64truncuf32() {}
+
+// #[test]
+// fn test_i64truncsf64() {}
+
+// #[test]
+// fn test_i64truncuf64() {}
+
+// #[test]
+// fn test_f32convertsi32() {}
+
+// #[test]
+// fn test_f32convertui32() {}
+
+// #[test]
+// fn test_f32convertsi64() {}
+
+// #[test]
+// fn test_f32convertui64() {}
+
+// #[test]
+// fn test_f32demotef64() {}
+
+// #[test]
+// fn test_f64convertsi32() {}
+
+// #[test]
+// fn test_f64convertui32() {}
+
+// #[test]
+// fn test_f64convertsi64() {}
+
+// #[test]
+// fn test_f64convertui64() {}
+
+// #[test]
+// fn test_f64promotef32() {}
+
+// #[test]
+// fn test_i32reinterpretf32() {}
+
+// #[test]
+// fn test_i64reinterpretf64() {}
+
+// #[test]
+// fn test_f32reinterpreti32() {}
+
+// #[test]
+// fn test_f64reinterpreti64() {}
