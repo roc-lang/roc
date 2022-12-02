304b88cdb229a8375f76e12a25663b17dee5df07
Ayaz Hafiz <ayaz@rwx.com>
2022-12-02 16:20:56 -0600

    Fix bug in expect test

--- crates/repl_eval/src/gen.rs
+++ crates/repl_eval/src/gen.rs
@@ -1,5 +1,5 @@
 use bumpalo::Bump;
-use roc_load::{ExecutionMode, LoadConfig, Threading};
+use roc_load::{ExecutionMode, LoadConfig, LoadMonomorphizedError, Threading};
 use roc_packaging::cache::{self, RocCacheDir};
 use roc_problem::Severity;
 use roc_reporting::report::Palette;
@@ -73,7 +73,13 @@ pub fn compile_to_mono<'a, 'i, I: Iterator<Item = &'i str>>(
 
     let mut loaded = match loaded {
         Ok(v) => v,
-        Err(LoadingProblem::FormattedReport(report)) => {
+        Err(LoadMonomorphizedError::ErrorModule(m)) => {
+            todo!(
+                "error while loading module: {:?}",
+                (m.can_problems, m.type_problems)
+            );
+        }
+        Err(LoadMonomorphizedError::LoadingProblem(LoadingProblem::FormattedReport(report))) => {
             return (
                 None,
                 Problems {
