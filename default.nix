8e851b13ad5014f3ac3197ab1b35382355997d4b
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2022-12-03 15:07:35 +0100

    use cargoLock instead of cargSha256
    This prevents needing to update the cargoSha256 every time the cargo lock file changes.

--- default.nix
+++ default.nix
@@ -4,7 +4,6 @@
     sha256 = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked.narHash;
   }
 , pkgs ? import nixpkgsSource { }
-, cargoSha256 ? "sha256-AH/cWRbshJI2pweoz24AXcDcz/+fM6cGHJU7V9GH/w4"
 ,
 }:
 # we only this file to release a nix package, use flake.nix for development
@@ -20,7 +19,17 @@ rustPlatform.buildRustPackage {
 
   src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
 
-  inherit cargoSha256;
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "confy-0.5.0" = "sha256-BVTczVbURL1Id/k/5ArlDQTZxLuI3XxQl7BdIx230U4=";
+      "criterion-0.3.5" = "sha256-7REd3phV6PBzqWwKF8hwttw4FTq2tKGxxAAJDpLC50A=";
+      "inkwell-0.1.0" = "sha256-vhFbiGP8FxMoLkapctPYmkfV+tO6x4KkoC3FwCHcy/4=";
+      "plotters-0.3.1" = "sha256-noy/RSjoEPZZbOJTZw1yxGcX5S+2q/7mxnUrzDyxOFw=";
+      "rustyline-9.1.1" = "sha256-aqQqz6nSp+Qn44gm3jXmmQUO6/fYTx7iLph2tbA24Bs=";
+      "wasm3-0.5.0" = "sha256-RwAZ4hjdDk/MvY1S1ymduqn+ubNPQdkMcXJHU2Zxq8k=";
+    };
+  };
 
   LLVM_SYS_130_PREFIX = "${llvmPkgs.llvm.dev}";
 
