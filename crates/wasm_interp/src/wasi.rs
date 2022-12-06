d43129d5782b5abda57dd60f709379a4603726e4
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-12-06 22:15:08 +0000

    wasm_interp: code comment about empty implementation of WASI clock functions

--- crates/wasm_interp/src/wasi.rs
+++ crates/wasm_interp/src/wasi.rs
@@ -61,8 +61,8 @@ impl<'a> WasiDispatcher<'a> {
             }
             "environ_get" => todo!("WASI {}({:?})", function_name, arguments),
             "environ_sizes_get" => todo!("WASI {}({:?})", function_name, arguments),
-            "clock_res_get" => success_code,
-            "clock_time_get" => success_code,
+            "clock_res_get" => success_code, // this dummy implementation seems to be good enough
+            "clock_time_get" => success_code, // this dummy implementation seems to be good enough
             "fd_advise" => todo!("WASI {}({:?})", function_name, arguments),
             "fd_allocate" => todo!("WASI {}({:?})", function_name, arguments),
             "fd_close" => todo!("WASI {}({:?})", function_name, arguments),
