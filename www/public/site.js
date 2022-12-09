6eef9119828bb401538c3311d87919e83cea9dec
Richard Feldman <oss@rtfeldman.com>
2022-12-08 22:05:07 -0500

    Close the ToC when you press Esc

--- www/public/site.js
+++ www/public/site.js
@@ -1,5 +1,15 @@
+const tutorialTocToggle = document.querySelector("#tutorial-toc-toggle");
+
 document.querySelectorAll("#tutorial-toc li a").forEach((elem) => {
+    // Clicking any of the ToC links closes the ToC
     elem.addEventListener("click", (event) => {
-        document.querySelector("#tutorial-toc-toggle").checked = false;
+        tutorialTocToggle.checked = false;
     })
+});
+
+document.addEventListener("keydown", (event) => {
+    // Escape closes the ToC
+    if (event.key == "Escape") {
+        tutorialTocToggle.checked = false;
+    }
 });
