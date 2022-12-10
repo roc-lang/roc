0316403218a9adf1a936c02126654002ed756e3d
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2022-12-10 18:12:42 +0100

    add missing scripts

---
+++ ci/build_basic_cli_w_latest_roc.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
+git clone https://github.com/roc-lang/basic-cli.git
+
+# get the url of the latest release for linux_x86_64
+RELEASE_URL=$(./ci/get_latest_release_url.sh $1)
+
+# get the archive from the url
+mkdir roc_nightly && cd roc_nightly && curl -OL $RELEASE_URL
+
+# decompress the tar
+ls | grep "roc_nightly.*tar\.gz" | xargs tar -xzvf
+
+# build the basic cli platform
+./roc build basic-cli/examples/file.roc
--- ci/get_latest_release_url.sh
+++ ci/get_latest_release_url.sh
@@ -1,15 +1,15 @@
-#!/usr/bin/env bash
+ #!/usr/bin/env bash
 
 # assumes roc_releases.json is present
 
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euxo pipefail
 
-LATEST_RELEASE_URL=`cat roc_releases.json | jq --arg arch $1 --arg today $(date +'%Y-%m-%d') '.[0] | .assets | map(.browser_download_url) | map(select(. | contains("\($arch)-\($today)"))) | .[0]'`
+LATEST_RELEASE_URL=`cat roc_releases.json | jq --arg arch $1 '.[0] | .assets | map(.browser_download_url) | map(select(. | contains("\($arch)-"))) | last'`
 
 if [[ "$LATEST_RELEASE_URL" == "null" ]]
 then
-  echo "I expected to get a url but I got null instead. Today's release may not have been uploaded?"
+  echo "I expected to get a url but I got null instead. The provided argument for this bash script may not have had any matches when used as arch in the jq query above."
   exit 1
 else
   echo $LATEST_RELEASE_URL | sed s/\"//g
