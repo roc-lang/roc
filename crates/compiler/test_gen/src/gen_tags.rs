f08d9c9173b01f4e70e172e98c8ce3c5f6c7078a
Ayaz Hafiz <ayaz@rwx.com>
2022-12-13 12:10:08 -0600

    Add gen test for other #4733 reproducer

--- crates/compiler/test_gen/src/gen_tags.rs
+++ crates/compiler/test_gen/src/gen_tags.rs
@@ -2106,3 +2106,30 @@ fn lambda_set_with_imported_toplevels_issue_4733() {
         i64
     );
 }
+
+#[test]
+#[cfg(any(feature = "gen-llvm", feature = "gen-wasm"))]
+fn non_unary_union_with_lambda_set_with_imported_toplevels_issue_4733() {
+    assert_evals_to!(
+        indoc!(
+            r#"
+            app "test" provides [main] to "./platform"
+
+            fn = \s ->
+                instr = 
+                    if s == "*" then (Op Num.mul)
+                    else if s == "+" then (Op Num.add)
+                    else Noop
+
+                when instr is
+                    Op op -> (\a -> op a a)
+                    _ -> (\a -> a)
+
+
+            main = ((fn "*") 3) * ((fn "+") 5)
+            "#
+        ),
+        90,
+        i64
+    );
+}
