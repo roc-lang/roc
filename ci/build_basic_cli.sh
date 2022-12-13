a5ad0db868c6dbbbcb3dc0e703ac736dce51cf06
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2022-12-13 19:18:50 +0100

    add other os

--- ci/build_basic_cli.sh
+++ ci/build_basic_cli.sh
@@ -5,9 +5,6 @@ set -euxo pipefail
 
 git clone https://github.com/roc-lang/basic-cli.git
 
-# fetch releases data and save to file
-curl https://api.github.com/repos/roc-lang/roc/releases > roc_releases.json
-
 # Get the url of the latest release. We're not using the latest main source code for easier reproducibility.
 RELEASE_URL=$(./ci/get_latest_release_url.sh $1)
 
