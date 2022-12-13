1727a9a1239ad5ea2aabeecbf365244a198a5814
Ayaz Hafiz <ayaz@rwx.com>
2022-12-13 12:06:53 -0600

    Add gen test for #4733

--- crates/compiler/test_gen/src/gen_tags.rs
+++ crates/compiler/test_gen/src/gen_tags.rs
@@ -2083,3 +2083,26 @@ fn unify_types_with_fixed_fixpoints_outside_fixing_region() {
         RocStr
     );
 }
+
+#[test]
+#[cfg(any(feature = "gen-llvm", feature = "gen-wasm"))]
+fn lambda_set_with_imported_toplevels_issue_4733() {
+    assert_evals_to!(
+        indoc!(
+            r#"
+            app "test" provides [main] to "./platform"
+
+            fn = \s ->
+                instr = if s == "*" then (Op Num.mul) else (Op Num.add)
+
+                Op op = instr
+
+                \a -> op a a
+
+            main = ((fn "*") 3) * ((fn "+") 5)
+            "#
+        ),
+        90,
+        i64
+    );
+}
