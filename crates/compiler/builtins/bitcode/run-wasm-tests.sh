f55fbaad12292d582c426bb323b21f2bb3343851
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-12-14 12:09:31 +0000

    builtins: use roc_wasm_interp for Zig wasm tests

--- crates/compiler/builtins/bitcode/run-wasm-tests.sh
+++ crates/compiler/builtins/bitcode/run-wasm-tests.sh
@@ -8,9 +8,4 @@ set -euxo pipefail
 
 # Zig will try to run the test binary it produced, but since your OS doesn't know how to
 # run Wasm binaries natively, we need to provide a Wasm interpreter as a "test command".
-zig test -target wasm32-wasi-musl -O ReleaseFast src/main.zig --test-cmd ~/roc/target/release/roc_wasm_interp --test-cmd-bin
-
-hyperfine --warmup 1 \
-    'zig test -target wasm32-wasi-musl -O ReleaseFast src/main.zig --test-cmd wasmer --test-cmd-bin' \
-    'zig test -target wasm32-wasi-musl -O ReleaseFast src/main.zig --test-cmd wasm3 --test-cmd-bin' \
-    'zig test -target wasm32-wasi-musl -O ReleaseFast src/main.zig --test-cmd ~/roc/target/release/roc_wasm_interp --test-cmd-bin'
+zig test -target wasm32-wasi-musl -O ReleaseFast src/main.zig --test-cmd ../../../../target/release/roc_wasm_interp --test-cmd-bin
