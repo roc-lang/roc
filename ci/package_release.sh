520db5765f341c636044cf1538798589020edf53
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2023-01-17 11:56:26 +0100

    simplify removal of unnecessary files

--- ci/package_release.sh
+++ ci/package_release.sh
@@ -6,14 +6,7 @@ set -euxo pipefail
 cp target/release/roc ./roc # to be able to delete "target" later
 
 # delete unnecessary files and folders
-find . -name "target" -depth -type d -exec rm -rf {} \;
-find . -name "zig-cache" -depth -type d -exec rm -rf {} \;
-
-find . -name "*.o" -exec rm -rf {} \;
-find . -name "*.rh1" -exec rm -rf {} \;
-find . -name "*.rm1" -exec rm -rf {} \;
-
-fd --type executable . './examples' -X rm
+git clean -fdx --exclude roc
 
 mkdir $1
 tar -czvf "$1.tar.gz" roc LICENSE LEGAL_DETAILS examples/helloWorld.roc examples/platform-switching examples/cli crates/roc_std crates/compiler/builtins/bitcode/src
