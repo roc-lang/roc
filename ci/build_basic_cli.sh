67d813ea7ffc2099f509df8b451405371e79a91a
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2023-01-20 18:23:31 +0100

    simpler to use releases

--- ci/build_basic_cli.sh
+++ ci/build_basic_cli.sh
@@ -5,8 +5,25 @@ set -euxo pipefail
 
 git clone https://github.com/roc-lang/basic-cli.git
 
+# Get the url of the latest release. We're not using the latest main source code for easier reproducibility.
+RELEASE_URL=$(./ci/get_latest_release_url.sh $1)
+
+# get the archive from the url
+mkdir roc_nightly && cd roc_nightly && curl -OL $RELEASE_URL
+
+# decompress the tar
+ls | grep "roc_nightly.*tar\.gz" | xargs tar -xzvf
+
+# delete tar
+# ls | grep -v "roc_nightly.*tar\.gz" | xargs rm -rf
+
+# simplify dir name
+# mv roc_nightly* roc_nightly
+
+# cd roc_nightly
+
 # build the basic cli platform
-./target/release/roc build ../basic-cli/examples/file.roc
+./roc build ../basic-cli/examples/file.roc
 
 # We need this extra variable so we can safely check if $2 is empty later
 EXTRA_ARGS=${2:-}
