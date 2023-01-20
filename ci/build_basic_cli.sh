1d7520a08b1afd522698351250d36b756b9b5185
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2023-01-20 11:37:29 +0100

    prep basic-cli release built on macos 11

--- ci/build_basic_cli.sh
+++ ci/build_basic_cli.sh
@@ -5,6 +5,8 @@ set -euxo pipefail
 
 git clone https://github.com/roc-lang/basic-cli.git
 
+git checkout tags/0.1.3
+
 # Get the url of the latest release. We're not using the latest main source code for easier reproducibility.
 RELEASE_URL=$(./ci/get_latest_release_url.sh $1)
 
