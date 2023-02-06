dfdfedabdb8db01ce8535309a683b4d06428274f
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2023-02-06 14:30:31 +0100

    try without pipefail

--- ci/get_releases.sh
+++ ci/get_releases.sh
@@ -3,10 +3,12 @@
 # retrieves roc_releases.json, expects TOKEN to be set to ${{ secrets.GITHUB_TOKEN }}
 
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
-set -euxo pipefail
+# set -euxo pipefail
 
 curl --request GET \
           --url https://api.github.com/repos/roc-lang/roc/releases \
           --header 'authorization: Bearer $TOKEN' \
           --header 'content-type: application/json' \
           --output roc_releases.json
+
+cat roc_releases.json
