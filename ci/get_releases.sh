7ab760de6569231aff814fe6bf863e32dd6ae068
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2023-02-06 20:04:16 +0100

    cleaned up dbg

--- ci/get_releases.sh
+++ ci/get_releases.sh
@@ -3,14 +3,10 @@
 # retrieves roc_releases.json, expects TOKEN to be set to ${{ secrets.GITHUB_TOKEN }}
 
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
-# set -euxo pipefail
-
-echo $(echo "$AUTH_HEADER" | wc -c)
+set -euxo pipefail
 
 curl --request GET \
           --url https://api.github.com/repos/roc-lang/roc/releases \
           --header '$AUTH_HEADER' \
           --header 'content-type: application/json' \
           --output roc_releases.json
-
-cat roc_releases.json
