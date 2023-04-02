bc7e6c434052adde7be1bda9f2da55106512241d
Ayaz Hafiz <ayaz@rwx.com>
2023-04-02 12:50:19 -0500

    Unused test

--- crates/compiler/solve/tests/solve_expr.rs
+++ crates/compiler/solve/tests/solve_expr.rs
@@ -289,19 +289,6 @@ mod solve_expr {
         );
     }
 
-    // #[test]
-    // fn block_string_literal() {
-    //     infer_eq(
-    //         indoc!(
-    //             r#"
-    //             """type
-    //             inference!"""
-    //         "#
-    //         ),
-    //         "Str",
-    //     );
-    // }
-
     // LIST
 
     #[test]
