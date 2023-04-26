48cbdb731f62aecdd9fe3e6e988c52f55626c99b
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2023-04-26 16:29:02 +0200

    pinned criterion, trying different nix commit

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Roc flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?rev=c8018361fa1d1650ee8d4b96294783cf564e8a7f";
 
     # rust from nixpkgs has some libc problems, this is patched in the rust-overlay
     rust-overlay = {
