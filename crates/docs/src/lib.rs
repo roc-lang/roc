9fd2224afaa405e6798a72db23489a8cadbff3b9
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2023-05-15 19:58:49 +0200

    clippy fix
    Signed-off-by: Anton-4 <17049058+Anton-4@users.noreply.github.com>

--- crates/docs/src/lib.rs
+++ crates/docs/src/lib.rs
@@ -133,6 +133,7 @@ pub fn generate_docs_html(root_file: PathBuf) {
         set
     };
 
+    // TODO fix: as is, this overrides an existing index.html
     // Write index.html for package (/index.html)
     /*{
         let rendered_package = template_html
@@ -195,7 +196,8 @@ fn page_title(package_name: &str, module_name: &str) -> String {
     format!("<title>{module_name} - {package_name}</title>")
 }
 
-fn render_package_index(root_module: &LoadedModule) -> String {
+// TODO re-enable with let rendered_package = template_html...
+/*fn render_package_index(root_module: &LoadedModule) -> String {
     // The list items containing module links
     let mut module_list_buf = String::new();
 
@@ -226,7 +228,7 @@ fn render_package_index(root_module: &LoadedModule) -> String {
     );
 
     index_buf
-}
+}*/
 
 fn render_module_documentation(
     module: &ModuleDocumentation,
