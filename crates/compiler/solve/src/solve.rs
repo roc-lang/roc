ff4b7cdc0f93ee5dc202170d8efcb4cd3331b01d
Ajai Nelson <22969541+AjaiKN@users.noreply.github.com>
2023-06-16 22:42:49 -0400

    Start type expansion

--- crates/compiler/solve/src/solve.rs
+++ crates/compiler/solve/src/solve.rs
@@ -215,6 +215,7 @@ enum Work<'a> {
         /// mimic `type_to_var`, we must add these variables to `Pools`
         /// at the correct rank
         pool_variables: &'a [Variable],
+        expand: bool,
     },
     /// The ret_con part of a let constraint that introduces rigid and/or flex variables
     ///
@@ -230,6 +231,7 @@ enum Work<'a> {
         /// mimic `type_to_var`, we must add these variables to `Pools`
         /// at the correct rank
         pool_variables: &'a [Variable],
+        expand: bool,
     },
 }
 
@@ -277,6 +279,7 @@ fn solve(
                 rank,
                 let_con,
                 pool_variables,
+                expand,
             } => {
                 // NOTE be extremely careful with shadowing here
                 let offset = let_con.defs_and_ret_constraint.index();
@@ -309,7 +312,18 @@ fn solve(
                         *loc_var,
                     );
 
-                    new_scope.insert_symbol_var_if_vacant(*symbol, loc_var.value);
+                    if expand {
+                        // dbg!("yes expanding");
+                        let unexpanded_var = loc_var.value;
+                        let unexpanded_descriptor = subs.get(unexpanded_var);
+                        // let expanded_var = if let Content::Structure(FlatType::TagUnion(..)) = unexpanded_descriptor.content {
+                        let expanded_var = subs.fresh(unexpanded_descriptor.clone());
+                        open_tag_union(subs, pools, expanded_var);
+                        new_scope.insert_symbol_var_if_vacant(*symbol, expanded_var);
+                    } else {
+                        // dbg!("not expanding");
+                        new_env.insert_symbol_var_if_vacant(*symbol, loc_var.value);
+                    }
                 }
 
                 stack.push(Work::Constraint {
@@ -327,6 +341,7 @@ fn solve(
                 rank,
                 let_con,
                 pool_variables,
+                expand: _,
             } => {
                 // NOTE be extremely careful with shadowing here
                 let offset = let_con.defs_and_ret_constraint.index();
@@ -438,6 +453,7 @@ fn solve(
                         *loc_var,
                     );
 
+                    // TODO maybe
                     new_scope.insert_symbol_var_if_vacant(*symbol, loc_var.value);
                 }
 
@@ -768,12 +784,17 @@ fn solve(
                     }
                 }
             }
-            Let(index, pool_slice) => {
+            Let(index, pool_slice) |
+            LetAndExpandType(index, pool_slice) => {
+                // dbg!(constraint);
                 let let_con = &env.constraints.let_constraints[index.index()];
+                // dbg!(let_con);
 
                 let offset = let_con.defs_and_ret_constraint.index();
                 let defs_constraint = &env.constraints.constraints[offset];
                 let ret_constraint = &env.constraints.constraints[offset + 1];
+                // dbg!(defs_constraint);
+                // dbg!(ret_constraint);
 
                 let flex_vars = &env.constraints.variables[let_con.flex_vars.indices()];
                 let rigid_vars = &env.constraints.variables[let_con.rigid_vars.indices()];
@@ -781,6 +802,7 @@ fn solve(
                 let pool_variables = &env.constraints.variables[pool_slice.indices()];
 
                 if matches!(&ret_constraint, True) && let_con.rigid_vars.is_empty() {
+                    // dbg!("branch 1");
                     debug_assert!(pool_variables.is_empty());
 
                     env.introduce(rank, flex_vars);
@@ -795,16 +817,19 @@ fn solve(
 
                     state
                 } else if let_con.rigid_vars.is_empty() && let_con.flex_vars.is_empty() {
+                    // dbg!("branch 2");
                     // items are popped from the stack in reverse order. That means that we'll
                     // first solve then defs_constraint, and then (eventually) the ret_constraint.
                     //
                     // Note that the LetConSimple gets the current env and rank,
                     // and not the env/rank from after solving the defs_constraint
+                    // dbg!("pushing");
                     stack.push(Work::LetConNoVariables {
                         scope,
                         rank,
                         let_con,
                         pool_variables,
+                        expand: matches!(constraint, LetAndExpandType(..)),
                     });
                     stack.push(Work::Constraint {
                         scope,
@@ -814,6 +839,7 @@ fn solve(
 
                     state
                 } else {
+                    // dbg!("branch 3");
                     // If the let-binding is generalizable, work at the next rank (which will be
                     // the rank at which introduced variables will become generalized, if they end up
                     // staying there); otherwise, stay at the current level.
@@ -853,11 +879,13 @@ fn solve(
                     // NB: LetCon gets the current scope's env and rank, not the env/rank from after solving the defs_constraint.
                     // That's because the defs constraints will be solved in next_rank if it is eligible for generalization.
                     // The LetCon will then generalize variables that are at a higher rank than the rank of the current scope.
+                    // dbg!("pushing");
                     stack.push(Work::LetConIntroducesVariables {
                         scope,
                         rank,
                         let_con,
                         pool_variables,
+                        expand: matches!(constraint, LetAndExpandType(..)),
                     });
                     stack.push(Work::Constraint {
                         scope,
