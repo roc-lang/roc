23f4d02f814f24428c072ba184715c50347b969b
Ayaz Hafiz <ayaz@rwx.com>
2023-07-07 11:46:42 -0500

    Cleanup

--- crates/compiler/gen_llvm/src/llvm/fn_ptr.rs
+++ crates/compiler/gen_llvm/src/llvm/fn_ptr.rs
@@ -1,13 +1,10 @@
 use bumpalo::collections::CollectIn;
 use inkwell::{
     types::{FunctionType, PointerType},
-    values::{BasicValueEnum, FunctionValue, PointerValue},
-    AddressSpace,
+    values::{FunctionValue, PointerValue},
 };
-use roc_error_macros::internal_error;
-use roc_mono::layout::{
-    FunctionPointer, InLayout, LambdaName, LayoutInterner, LayoutRepr, STLayoutInterner,
-};
+
+use roc_mono::layout::{InLayout, LambdaName, LayoutInterner, STLayoutInterner};
 
 use super::{
     build::{function_value_by_func_spec, Env, FuncBorrowSpec, FunctionSpec, RocReturn},
@@ -32,35 +29,6 @@ pub fn function_type<'a, 'ctx>(
     let fn_spec = FunctionSpec::fastcc(env, roc_return, ret, args.collect_in(env.arena));
 
     fn_spec.typ
-}
-
-pub fn pointer_type<'a, 'ctx>(
-    env: &Env<'a, 'ctx, '_>,
-    layout_interner: &STLayoutInterner<'a>,
-    function_pointer: FunctionPointer<'a>,
-) -> PointerType<'ctx> {
-    let function_type = function_type(
-        env,
-        layout_interner,
-        function_pointer.args,
-        function_pointer.ret,
-    );
-
-    function_type.ptr_type(AddressSpace::default())
-}
-
-#[track_caller]
-pub fn pointer_type_expecting_layout<'a, 'ctx>(
-    env: &Env<'a, 'ctx, '_>,
-    layout_interner: &STLayoutInterner<'a>,
-    layout: InLayout<'a>,
-) -> PointerType<'ctx> {
-    let function_pointer = match layout_interner.get_repr(layout) {
-        LayoutRepr::FunctionPointer(function_pointer) => function_pointer,
-        _ => internal_error!("expected function pointer"),
-    };
-
-    pointer_type(env, layout_interner, function_pointer)
 }
 
 pub fn build<'a, 'ctx>(env: &Env<'a, 'ctx, '_>, lambda_name: LambdaName<'a>) -> PointerValue<'ctx> {
