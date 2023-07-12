cb5687256646b33e7d82cc341aade2823bd9c968
Ayaz Hafiz <ayaz@rwx.com>
2023-07-12 13:41:16 -0500

    Add initial tests

--- crates/compiler/test_gen/src/gen_erased.rs
+++ crates/compiler/test_gen/src/gen_erased.rs
@@ -5,6 +5,25 @@ use indoc::indoc;
 
 #[test]
 #[cfg(any(feature = "gen-llvm"))]
+fn capture_multiple() {
+    assert_evals_to_erased!(
+        indoc!(
+            r#"
+            app "test" provides [main] to "./platform"
+
+            f = \n, m ->
+              \{} -> n + m + 15u8
+
+            main = (f 10u8 20u8) {}
+            "#
+        ),
+        45,
+        u8
+    );
+}
+
+#[test]
+#[cfg(any(feature = "gen-llvm"))]
 fn multi_branch_capturing() {
     assert_evals_to_erased!(
         indoc!(
@@ -13,13 +32,13 @@ fn multi_branch_capturing() {
 
             f = \t, s ->
               if t
-              then \{} -> 0
+              then \{} -> 15nat
               else \{} -> Str.countGraphemes s
 
             main = ((f Bool.true "abc") {}, (f Bool.false "abc") {})
             "#
         ),
-        (0, 3),
+        (15, 3),
         (usize, usize)
     );
 }
