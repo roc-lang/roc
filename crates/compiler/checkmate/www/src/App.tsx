27dd9d03aa8e21fac99bc525115589e6a6d2b19f
Ayaz Hafiz <ayaz@rwx.com>
2023-07-16 13:54:50 -0500

    Stub out checkmate react app

---
+++ crates/compiler/checkmate/www/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
