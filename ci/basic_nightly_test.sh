4a7fe281b17498a3dcfedf725f2b313414ef0f07
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2023-07-17 16:09:35 +0200

    test old linux nightly

---
+++ ci/basic_nightly_test.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
+# Remove everything in this dir except the tar and ci folder.
+# We want to test like a user who would have downloaded the release, so we clean up all files from the repo checkout.
+ls | grep -v "roc_nightly.*tar\.gz"  | grep -v "^ci$" | xargs rm -rf
+
+# decompress the tar
+ls | grep "roc_nightly.*tar\.gz" | xargs tar -xzvf
+
+# delete tar
+ls | grep "roc_nightly.*tar\.gz" | xargs rm -rf
+
+# rename nightly folder
+mv roc_nightly* roc_nightly
+
+cd roc_nightly
+
+# test roc hello world
+./roc examples/helloWorld.roc 
+
+./roc examples/platform-switching/rocLovesRust.roc
+
+./roc examples/platform-switching/rocLovesZig.roc
+
+./roc examples/platform-switching/rocLovesC.roc
+
+cd ..
