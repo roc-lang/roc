171afcf79b1e3c294d60eb32affbc588fee8a3e6
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2023-07-26 11:39:48 +0200

    fix basic-cli docs css+js
    Signed-off-by: Anton-4 <17049058+Anton-4@users.noreply.github.com>

--- www/build.sh
+++ www/build.sh
@@ -115,8 +115,6 @@ rm -rf ./downloaded-basic-cli
 
 BASIC_CLI_PACKAGE_DIR="www/build/packages/basic-cli"
 mkdir -p $BASIC_CLI_PACKAGE_DIR
-mv generated-docs/index.html $BASIC_CLI_PACKAGE_DIR
-rm generated-docs/*.* # we already copied over the *.js and *.css files earlier for the builtins, so just drop these.
 mv generated-docs/* $BASIC_CLI_PACKAGE_DIR # move all the folders to build/packages/basic-cli
 
 # set up docs for basic-cli 0.3.2
