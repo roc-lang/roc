8b6bb9d75df72ea83e758eb9c38a2ec989f998c9
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2023-08-11 20:21:53 +0200

    prep basic-cli 0.5 release

--- ci/build_basic_cli.sh
+++ ci/build_basic_cli.sh
@@ -4,11 +4,18 @@
 set -euxo pipefail
 
 git clone https://github.com/roc-lang/basic-cli.git
+cd basic-cli
+git checkout new-abilities-syntax
+cd ..
 
-if [ "$(uname -m)" == "x86_64" ] && [ "$(uname -s)" == "Linux" ]; then
+if [ "$(uname -s)" == "Linux" ]; then
     sudo apt-get install musl-tools
     cd basic-cli/src # we cd to install the target for the right rust version
-    rustup target add x86_64-unknown-linux-musl 
+    if [ "$(uname -m)" == "x86_64" ]; then
+        rustup target add x86_64-unknown-linux-musl
+    elif [ "$(uname -m)" == "aarch64" ]; then
+        rustup target add aarch64-unknown-linux-gnu
+    fi
     cd ../..
 fi
 
