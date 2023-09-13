7c54cba17ff8a046add1f4a1ea0e465eeccee0d9
Brian Carroll <brian.carroll.ireland@gmail.com>
2023-09-13 08:07:40 +0100

    Remove unnecessary function wrapper

--- www/public/repl/repl.js
+++ www/public/repl/repl.js
@@ -1,16 +1,13 @@
 // The only way we can provide values to wasm_bindgen's generated code is to set globals
-function setGlobalsForWasmBindgen() {
-  window.js_create_app = js_create_app;
-  window.js_run_app = js_run_app;
-  window.js_get_result_and_memory = js_get_result_and_memory;
+window.js_create_app = js_create_app;
+window.js_run_app = js_run_app;
+window.js_get_result_and_memory = js_get_result_and_memory;
 
-  // The only place we use console.error is in wasm_bindgen, where it gets a single string argument.
-  console.error = function displayErrorInHistoryPanel(string) {
-    const html = `<div class="panic">${string}</div>`;
-    updateHistoryEntry(repl.inputHistoryIndex, false, html);
-  };
-}
-setGlobalsForWasmBindgen();
+// The only place we use console.error is in wasm_bindgen, where it gets a single string argument.
+console.error = function displayErrorInHistoryPanel(string) {
+  const html = `<div class="panic">${string}</div>`;
+  updateHistoryEntry(repl.inputHistoryIndex, false, html);
+};
 
 import * as roc_repl_wasm from "./roc_repl_wasm.js";
 import { getMockWasiImports } from "./wasi.js";
