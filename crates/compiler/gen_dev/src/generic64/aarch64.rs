5368c145cd1dffab488b7365df99beb094f78efe
Folkert <folkert@folkertdev.nl>
2023-10-09 14:52:19 +0200

    make stack cleanup symmetric
    we were doing things relative to the base pointer, but it got moved back to the caller's base pointer before use. this caused stack corruption

--- crates/compiler/gen_dev/src/generic64/aarch64.rs
+++ crates/compiler/gen_dev/src/generic64/aarch64.rs
@@ -477,19 +477,22 @@ impl CallConv<AArch64GeneralReg, AArch64FloatReg, AArch64Assembler> for AArch64C
 
         if aligned_stack_size > 0 {
             // All the following stores could be optimized by using `STP` to store pairs.
-            let w = aligned_stack_size;
-            AArch64Assembler::mov_reg64_stack32(buf, AArch64GeneralReg::FP, w - 0x10);
-            AArch64Assembler::mov_reg64_stack32(buf, AArch64GeneralReg::LR, w - 0x08);
-
             let mut offset = aligned_stack_size - fn_call_stack_size - frame_pointer_link_register;
+
             for reg in saved_general_regs {
                 AArch64Assembler::mov_reg64_base32(buf, *reg, offset);
                 offset -= 8;
             }
+
             for reg in saved_float_regs {
                 AArch64Assembler::mov_freg64_base32(buf, *reg, offset);
                 offset -= 8;
             }
+
+            let w = aligned_stack_size;
+            AArch64Assembler::mov_reg64_stack32(buf, AArch64GeneralReg::FP, w - 0x10);
+            AArch64Assembler::mov_reg64_stack32(buf, AArch64GeneralReg::LR, w - 0x08);
+
             AArch64Assembler::add_reg64_reg64_imm32(
                 buf,
                 AArch64GeneralReg::ZRSP,
