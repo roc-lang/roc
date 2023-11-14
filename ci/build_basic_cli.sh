3ada01dafdd9a9cdf449daeee1b79968e8094d7a
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2023-11-14 11:10:31 +0100

    sudo musl-tools fix

--- ci/build_basic_cli.sh
+++ ci/build_basic_cli.sh
@@ -6,7 +6,13 @@ set -euxo pipefail
 git clone https://github.com/roc-lang/basic-cli.git
 
 if [ "$(uname -s)" == "Linux" ]; then
-    sudo apt-get install -y musl-tools
+
+    # check if musl-tools is installed
+    if ! dpkg -l | grep -q musl-tools; then
+        # install musl-tools with timeout for sudo problems with CI
+        timeout 300s sudo apt-get install -y musl-tools
+    fi
+    
     cd basic-cli/src # we cd to install the target for the right rust version
     if [ "$(uname -m)" == "x86_64" ]; then
         rustup target add x86_64-unknown-linux-musl
