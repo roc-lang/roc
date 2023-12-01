02930b66a96a9f61b19f583bc561e1ddee5a530f
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2023-12-01 18:37:37 +0100

    partial revert of #6113

--- crates/wasm_interp/src/instance.rs
+++ crates/wasm_interp/src/instance.rs
@@ -70,7 +70,6 @@ pub struct Instance<'a, I: ImportDispatcher> {
 
 impl<'a, I: ImportDispatcher> Instance<'a, I> {
     #[cfg(test)]
-    #[allow(dead_code)]
     pub(crate) fn new<G>(
         arena: &'a Bump,
         memory_pages: u32,
