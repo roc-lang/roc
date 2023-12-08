4c76a224b5020f8d76337da08993e430c36d4b04
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2023-12-08 13:36:04 -0800

    bundle compiler-rt through zig

--- crates/compiler/builtins/bitcode/src/main.zig
+++ crates/compiler/builtins/bitcode/src/main.zig
@@ -6,10 +6,10 @@ const expect = @import("expect.zig");
 const panic_utils = @import("panic.zig");
 const dbg_utils = @import("dbg.zig");
 
-comptime {
-    _ = @import("compiler_rt.zig");
-    _ = @import("libc.zig");
-}
+// comptime {
+// _ = @import("compiler_rt.zig");
+// _ = @import("libc.zig");
+// }
 
 const ROC_BUILTINS = "roc_builtins";
 const NUM = "num";
