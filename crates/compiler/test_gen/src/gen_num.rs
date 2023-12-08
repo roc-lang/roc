c87dcd1205b54b7a4b949979437b8be888de0a6c
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2023-12-08 14:43:57 -0800

    correct wrong test case

--- crates/compiler/test_gen/src/gen_num.rs
+++ crates/compiler/test_gen/src/gen_num.rs
@@ -1813,7 +1813,7 @@ fn ceiling() {
 #[cfg(any(feature = "gen-llvm", feature = "gen-wasm"))]
 fn floor() {
     assert_evals_to!("Num.floor 1.9f64", 1, i64);
-    assert_evals_to!("Num.floor -1.9f64", -1, i64);
+    assert_evals_to!("Num.floor -1.9f64", -2, i64);
 }
 
 #[test]
