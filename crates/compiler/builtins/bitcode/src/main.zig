797ba640039361bac989bc8f033cf5d4abf052a2
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2023-12-08 13:59:44 -0800

    remove all compiler-rt and libc code ports from zig bitcode

--- crates/compiler/builtins/bitcode/src/main.zig
+++ crates/compiler/builtins/bitcode/src/main.zig
@@ -6,11 +6,6 @@ const expect = @import("expect.zig");
 const panic_utils = @import("panic.zig");
 const dbg_utils = @import("dbg.zig");
 
-// comptime {
-// _ = @import("compiler_rt.zig");
-// _ = @import("libc.zig");
-// }
-
 const ROC_BUILTINS = "roc_builtins";
 const NUM = "num";
 const STR = "str";
