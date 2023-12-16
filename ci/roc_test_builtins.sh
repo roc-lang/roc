d35987893bada3fc473e29b90316f4592f281e88
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2023-12-16 20:11:25 +0100

    bash flags

--- ci/roc_test_builtins.sh
+++ ci/roc_test_builtins.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
 for file in crates/compiler/builtins/roc/*.roc; do
     if grep -qE '^\s*expect' "$file"; then
         cargo run --locked --release -- test  "$file"
