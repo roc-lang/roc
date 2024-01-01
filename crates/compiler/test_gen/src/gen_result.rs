1126f85b5a7123b42e52958cd7fae55b4eea6c48
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2024-01-01 16:38:36 +0100

    fix clippy unnecessary hashes

--- crates/compiler/test_gen/src/gen_result.rs
+++ crates/compiler/test_gen/src/gen_result.rs
@@ -17,12 +17,12 @@ use roc_std::{RocResult, RocStr};
 fn with_default_ok() {
     assert_evals_to!(
         indoc!(
-            r#"
+            r"
             result : Result I64 {}
             result = Ok 12345
 
             Result.withDefault result 0
-            "#
+            "
         ),
         12345,
         i64
@@ -34,12 +34,12 @@ fn with_default_ok() {
 fn with_default_err() {
     assert_evals_to!(
         indoc!(
-            r#"
+            r"
             result : Result I64 {}
             result = Err {}
 
             Result.withDefault result 0
-            "#
+            "
         ),
         0,
         i64
@@ -51,14 +51,14 @@ fn with_default_err() {
 fn result_map() {
     assert_evals_to!(
         indoc!(
-            r#"
+            r"
             result : Result I64 {}
             result = Ok 2
 
             result
                 |> Result.map (\x -> x + 1)
                 |> Result.withDefault 0
-            "#
+            "
         ),
         3,
         i64
@@ -66,14 +66,14 @@ fn result_map() {
 
     assert_evals_to!(
         indoc!(
-            r#"
+            r"
             result : Result I64 {}
             result = Err {}
 
             result
                 |> Result.map (\x -> x + 1)
                 |> Result.withDefault 0
-            "#
+            "
         ),
         0,
         i64
@@ -85,14 +85,14 @@ fn result_map() {
 fn result_map_err() {
     assert_evals_to!(
         indoc!(
-            r#"
+            r"
             result : Result {} I64
             result = Err 2
 
             when Result.mapErr result (\x -> x + 1) is
                 Err n -> n
                 Ok _ -> 0
-            "#
+            "
         ),
         3,
         i64
@@ -100,14 +100,14 @@ fn result_map_err() {
 
     assert_evals_to!(
         indoc!(
-            r#"
+            r"
             result : Result {} I64
             result = Ok {}
 
             when Result.mapErr result (\x -> x + 1) is
                 Err n -> n
                 Ok _ -> 0
-            "#
+            "
         ),
         0,
         i64
@@ -119,10 +119,10 @@ fn result_map_err() {
 fn err_type_var() {
     assert_evals_to!(
         indoc!(
-            r#"
+            r"
             Result.map (Ok 3) (\x -> x + 1)
                 |> Result.withDefault -1
-            "#
+            "
         ),
         4,
         i64
@@ -134,13 +134,13 @@ fn err_type_var() {
 fn err_type_var_annotation() {
     assert_evals_to!(
         indoc!(
-            r#"
+            r"
             ok : Result I64 *
             ok = Ok 3
 
             Result.map ok (\x -> x + 1)
                 |> Result.withDefault -1
-            "#
+            "
         ),
         4,
         i64
@@ -152,13 +152,13 @@ fn err_type_var_annotation() {
 fn err_empty_tag_union() {
     assert_evals_to!(
         indoc!(
-            r#"
+            r"
             ok : Result I64 []
             ok = Ok 3
 
             Result.map ok (\x -> x + 1)
                 |> Result.withDefault -1
-            "#
+            "
         ),
         4,
         i64
@@ -170,12 +170,12 @@ fn err_empty_tag_union() {
 fn is_ok() {
     assert_evals_to!(
         indoc!(
-            r#"
+            r"
             result : Result I64 {}
             result = Ok 2
 
             Result.isOk result
-            "#
+            "
         ),
         true,
         bool
@@ -183,12 +183,12 @@ fn is_ok() {
 
     assert_evals_to!(
         indoc!(
-            r#"
+            r"
             result : Result I64 {}
             result = Err {}
 
             Result.isOk result
-            "#
+            "
         ),
         false,
         bool
@@ -200,12 +200,12 @@ fn is_ok() {
 fn is_err() {
     assert_evals_to!(
         indoc!(
-            r#"
+            r"
             result : Result I64 {}
             result = Ok 2
 
             Result.isErr result
-            "#
+            "
         ),
         false,
         bool
@@ -213,12 +213,12 @@ fn is_err() {
 
     assert_evals_to!(
         indoc!(
-            r#"
+            r"
             result : Result I64 {}
             result = Err {}
 
             Result.isErr result
-            "#
+            "
         ),
         true,
         bool
@@ -230,12 +230,12 @@ fn is_err() {
 fn roc_result_ok_i64() {
     assert_evals_to!(
         indoc!(
-            r#"
+            r"
             result : Result I64 {}
             result = Ok 42
 
             result
-            "#
+            "
         ),
         RocResult::ok(42),
         RocResult<i64, ()>
@@ -250,12 +250,12 @@ fn roc_result_ok_f64() {
 
     assert_evals_to!(
         indoc!(
-            r#"
+            r"
             result : Result F64 {}
             result = Ok 42.0
 
             result
-            "#
+            "
         ),
         RocResult::ok(42.0),
         RocResult<f64, ()>
