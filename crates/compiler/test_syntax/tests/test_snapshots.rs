163bde0c41eac9416c8bd56ac33ed58dc10eea81
Richard Feldman <oss@rtfeldman.com>
2024-01-06 08:36:59 -0500

    Update tests to use $(...) interpolation

--- crates/compiler/test_syntax/tests/test_snapshots.rs
+++ crates/compiler/test_syntax/tests/test_snapshots.rs
@@ -777,7 +777,7 @@ mod test_snapshots {
 
     #[test]
     fn string_with_multiple_interpolations() {
-        assert_segments(r#""Hi, \(name)! How is \(project) going?""#, |arena| {
+        assert_segments(r#""Hi, $(name)! How is $(project) going?""#, |arena| {
             let expr1 = arena.alloc(Var {
                 module_name: "",
                 ident: "name",
