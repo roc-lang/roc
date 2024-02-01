ed6b5639b7fad848a6e9d91eae5a4510fce27067
Richard Feldman <oss@rtfeldman.com>
2024-01-31 19:57:56 -0500

    Fix implementation

--- crates/sized_str/src/lib.rs
+++ crates/sized_str/src/lib.rs
@@ -3,7 +3,10 @@
 #![cfg_attr(not(any(debug_assertions, test)), no_std)]
 
 use core::num::{NonZeroU32, NonZeroU64, NonZeroU8, NonZeroUsize};
-use core::{fmt, slice};
+use core::slice;
+
+#[cfg(any(debug_assertions, test))]
+use core::fmt;
 
 macro_rules! make_str_n {
     ($num_lanes:expr, $name:ident, $chunk_name:ident, $chunks_name:ident, $int:ty, $nonzero_int:ty) => {
@@ -83,13 +86,16 @@ make_str_n!(4, Str4, Str4Chunk, Str4Chunks, u32, NonZeroU32);
 impl Str4 {
     /// This is only needed in aarch64; in x64, we call _mm_set_epi32 directly
     #[cfg(target_arch = "aarch64")]
-    const ANSWER_MASK: [u32; 4] = [0, 1, 2, 3];
+    #[cfg(target_endian = "little")]
+    const ANSWER_MASK: [u32; 4] = [3, 2, 1, 0];
 
     /// Returns Some(first index where self occurs in the given slice) or else None if it wasn't found.
+    /// (Essentially the same as `slice.iter().position(|e| e == self)`, except using SIMD.)
     ///
     /// # Safety
     /// There must bet at least least 16B of accessible memory after the end of the slice.
-    /// we'll never need to look that far. The reason for this is that we start by dereferencing the
+    /// This is because we keep iterating in chunks of 16B
+    /// The reason for this is that we start by dereferencing the
     /// slice's pointer into a 16B structure, and then discarding any memory garbage we find.
     /// So there must be 16B there that we can safely dereference, even if it contains garbage!
     pub unsafe fn first_index_in(&self, slice: &[Str4]) -> Option<usize> {
@@ -118,27 +124,35 @@ impl Str4 {
         #[cfg(target_arch = "aarch64")]
         {
             answer_mask = aarch64::vld1q_u32(Self::ANSWER_MASK.as_ptr()); // Load ANSWER_MASK into a SIMD register for later
-            needle = aarch64::vdupq_n_u32(*(self as *const Self).cast()); // Load self into each lane of a SIMD register
+            needle = aarch64::vdupq_n_u32(self.0.get()); // Load self into each lane of a SIMD register
         }
 
         #[cfg(target_arch = "x86_64")]
         {
-            answer_mask = x86_64::_mm_set_epi32(3, 2, 1, 0);
+            #[cfg(target_endian = "little")]
+            {
+                answer_mask = x86_64::_mm_set_epi32(3, 2, 1, 0);
+            }
+
             needle = x86_64::_mm_set1_epi32(*(self as *const Self).cast());
         }
 
         #[cfg(target_arch = "wasm32")]
         {
-            answer_mask = wasm32::i32x4(0, 1, 2, 3);
+            #[cfg(target_endian = "little")]
+            {
+                answer_mask = wasm32::i32x4(3, 2, 1, 0);
+            }
             needle = wasm32::i32x4_splat(*(self as *const Self).cast());
         }
 
-        let success = |ptr: *const Str4, offset| {
-            let index = dbg!(ptr.offset_from(first_elem) + dbg!(offset)) as usize;
+        let success = |ptr: *const Str4, offset: usize| {
+            let index = (ptr.offset_from(first_elem) + (offset as isize)) as usize;
 
             // It's possible that we got a false positive because we encountered memory garbage
             // after the last element, and it happened to be a match. If that's the case, it must
             // mean we examined all the elements and didn't find a match, so return None.
+            // (Note that this does not optimize to a conditional branch; it's very cheap.)
             if index < len {
                 Some(index)
             } else {
@@ -147,26 +161,26 @@ impl Str4 {
         };
 
         macro_rules! check_equality {
-            ($ptr:expr, $elem_ptr:expr, $apply_mask:expr) => {{
-                // This pointer should have the correct alignment for a 16B-aligned SIMD register
-                debug_assert_eq!(($ptr as *const core::ffi::c_void).align_offset(ALIGN), 0);
-
+            ($ptr:expr) => {{
                 #[cfg(target_arch = "aarch64")]
                 {
                     // Compare each lane of the chunk to the needle for equality.
                     // Apply a mask afterwards so we can discard lanes that contained garbage memory.
-                    let equality_reg = $apply_mask(aarch64::vceqq_u32(aarch64::vld1q_u32($ptr), needle));
+                    let equality_reg = aarch64::vceqq_u32(aarch64::vld1q_u32($ptr), needle);
 
                     // If lane-wise equality returned any nonzero values, we found a match.
                     if aarch64::vmaxvq_u32(equality_reg) != 0 {
-                        // Do a bitwise AND with answer_mask, whose lanes are initialized to [0, 1, 2, 3]
+                        // Do a bitwise AND with answer_mask, whose lanes are initialized to [3, 2, 1, 0]
                         // and then get the scalar min of that, which will tell us the first lane's index
-                        // where we found an occurrence of the needle. (If multiple lanes matched, min will
-                        // give us the first one that matched, which is what we want this function to do.)
+                        // (from the end; this is necessary because if we did it from the beginning, we'd
+                        // have to use min instead of max, but then min would always return 0 for the not-found ones)
+                        // where we found an occurrence of the needle. (If multiple lanes matched, then after
+                        // we have subtracted this from NUM_LANES, we'll end up with the first lane that matched,
+                        // which is what we want this function to do.)
                         let lane_index =
-                            aarch64::vminvq_u32(aarch64::vandq_u32(equality_reg, answer_mask)) as isize;
+                            NUM_LANES - (aarch64::vmaxvq_u32(aarch64::vandq_u32(equality_reg, answer_mask)) as usize) - 1;
 
-                        return success($elem_ptr, lane_index);
+                        return success($ptr, lane_index);
                     }
                 }
 
@@ -183,9 +197,11 @@ impl Str4 {
                     // because NonZeroU32 has a more efficient trailing_zeros() than u32 does.
                     if let Some(mask) = NonZeroU32::new(mask as u32) {
                         #[cfg(target_endian = "little")]
-                        let lane_index = mask.trailing_zeros();
+                        {
+                            let lane_index = mask.trailing_zeros() as usize;
 
-                        return success($elem_ptr, lane_index);
+                            return success($ptr, lane_index);
+                        }
                     }
                 }
 
@@ -202,9 +218,11 @@ impl Str4 {
                     // because NonZeroU32 has a more efficient trailing_zeros() than u32 does.
                     if let Some(mask) = NonZeroU32::new(mask as u32) {
                         #[cfg(target_endian = "little")]
-                        let lane_index = mask.trailing_zeros();
+                        {
+                            let lane_index = mask.trailing_zeros() as usize;
 
-                        return success($elem_ptr, lane_index);
+                            return success($ptr, lane_index);
+                        }
                     }
                 }
             }};
@@ -213,39 +231,9 @@ impl Str4 {
         // Special-case the first chunk, which may not be 16B-aligned.
         // (Subsequent chunks will definitely be 16B-aligned.)
         {
-            // We need to mask out this many lanes to discard memory garbage.
-            let garbage_lanes = dbg!(NUM_LANES.saturating_sub(dbg!(len)));
+            check_equality!(first_elem.cast());
 
-            dbg!(*first_elem);
-
-            // Give an offset that subtracts the zero padding, so the answer index is
-            // relative to the start of the slice rather than incorrectly offset by the zeros.
-            check_equality!(
-                first_elem.cast(),
-                first_elem.add(garbage_lanes).cast(),
-                // Do a bitwise AND on each lane, to remove the garbage memory.
-                // (This will happen after the equality chek, so all the garbage
-                // lanes will be reported as not equal.)
-                |reg| {
-                    #[repr(align(16))]
-                    struct AlignedU32x4([u32; 4]);
-
-                    // Mask 0 is all 1s, so it has no effect. The others mask out 1 more lane than the previous one.
-                    const MASKS: [AlignedU32x4; 4] = [
-                        AlignedU32x4([!0, !0, !0, !0]),
-                        AlignedU32x4([!0, !0, !0, 0]),
-                        AlignedU32x4([!0, !0, 0, 0]),
-                        AlignedU32x4([!0, 0, 0, 0]),
-                    ];
-
-                    aarch64::vandq_u32(
-                        reg,
-                        aarch64::vld1q_u32(dbg!(MASKS[garbage_lanes].0).as_ptr()),
-                    )
-                }
-            );
-
-            // Advance to the next 16B boundary so we can do SIMD loads on each subsequent chunk.
+            // Advance to the next 16B boundary so we can do aligned loads on each subsequent chunk.
             // If we were already on a 16B boundary, then branchlessly advance by NUM_LANES - otherwise
             // the first iteration of the loop will redo the exact same work we just did.
             let align_offset = first_elem.align_offset(ALIGN);
@@ -255,31 +243,14 @@ impl Str4 {
                 align_offset
             };
 
-            dbg!(&amount_to_advance);
-
             current_elem = first_elem.add(amount_to_advance).cast();
         }
 
-        while (dbg!(slice_end.offset_from(current_elem)) >= NUM_LANES as isize) {
-            dbg!(*(current_elem as *const Str4));
-
-            check_equality!(current_elem.cast(), current_elem.cast(), |a| a);
+        while slice_end.offset_from(current_elem) > 0 {
+            check_equality!(current_elem.cast());
 
             // Advance to the next chunk
             current_elem = current_elem.add(NUM_LANES);
-        }
-
-        // There may have been leftovers. If so, check for them by looking at the very last chunk.
-        // (We only want to do this if there were actually possibly leftovers; otherwise, we may read
-        // garbage memory. We don't want to have to mask that out in the common case where len >= NUM_LANES!)
-        if len > NUM_LANES {
-            dbg!("CHECKING FOR LEFTOVERS");
-            let last_chunk =
-                slice_end.offset(slice_end.align_offset(ALIGN) as isize - (NUM_LANES as isize));
-            debug_assert_eq!(last_chunk.align_offset(ALIGN), 0);
-            dbg!(*(last_chunk as *const Str4), last_chunk, slice_end);
-
-            check_equality!(last_chunk.cast(), last_chunk.cast(), |a| a);
         }
 
         None
@@ -500,7 +471,7 @@ mod str4_from_str {
 
         for index in 0..src.len() {
             let expected = Some(src.len() - index - 1);
-            let actual = unsafe { dbg!(last_input.unwrap()).first_index_in(dbg!(&src[index..])) };
+            let actual = unsafe { last_input.unwrap().first_index_in(&src[index..]) };
 
             assert_eq!(
                 expected, actual,
