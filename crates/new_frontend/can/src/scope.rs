0b27dbef3831ae189a887126bda035b596e3bb84
Richard Feldman <oss@rtfeldman.com>
2024-02-07 02:01:27 -0500

    Trying out one-pass failed lookups
    It looks like the amount of bookkeeping to make this work
    might end up being more costly than just materializing
    the nodes

--- crates/new_frontend/can/src/scope.rs
+++ crates/new_frontend/can/src/scope.rs
@@ -1,29 +1,92 @@
+use core::marker::PhantomData;
 use std::{env::current_exe, thread::current};
 
 use arena::{Vec16, Vec32};
 use intern::{InternKey, Interns};
 
+use crate::node::Node;
+
 pub struct IdentId(NonZeroU16);
 
-pub struct Scope<'a> {
-    // SoA version of Vec<{ name: InternKey, ident_id: IdentId, used: bool }>
-    names: Vec32<'a, InternKey>,
-    ident_ids: Vec32<'a, IdentId>,
-    used: Vec32<'a, bool>,
+/// This is all SoA, which means they should all share one length and capacity.
+struct ById<'c, 'f, 'b, Id> {
+    // The Id type is an index into each of these SoA arrays.
+    _phantom: PhantomData<Id>,
+
+    // Region information needs to persist for reporting (and maybe in the future debug info),
+    // and since only regions are used in reporting (not interns), interns don't need to live
+    // past canonicalization. (In type checking we only need the intern keys and the regions,
+    // not what the keys map to.)
+    regions: Vec16<'b, Region>,
+
+    // The interned name which goes with this ID.
+    names: Vec16<'f, InternKey>,
+
+    /// Bitvec indicating whether or not this is referenced by anything else. At the end of
+    /// canonicalization, these get turned into reports, which are based on Region and which
+    /// mean we no longer need this information.
+    used: Vec16<'c, bool>,
+}
+
+/// 'c - lives for canonicalization only, and is no longer needed afterwards
+/// 'f - lives for frontend, will be used after canonicalization but not by the backend
+/// 'b - lives for backend, will still need to be used after type checking has finished
+pub struct State<'c, 'f, 'b> {
+    // Arena for things only used in canonicalization, only used in the frontend, and used in the backend.
+    can_arena: Arena<'c>,
+    fe_arena: Arena<'f>,
+    be_arena: Arena<'f>,
+
+    // Named functions, e.g. `foo = \…`
+    // These are separate from constants because top-level functions need to be topologically sorted
+    // into strongly-connected components for type checking, and because the backend needs to iterate
+    // over all functions separately from all non-functions.
+    tl_fns: ById<'a, TopLevelId>,
+    ntl_fns: ById<'a, NonTopLevelId>,
+
+    // Named constants, e.g. `foo = <non-lambda>`
+    tl_constants: ById<'f, TopLevelId>,
+    ntl_constants: ById<'a, NonTopLevelId>,
+
+    // Named types (including opaques, aliases, abilities)
+    tl_types: ById<'a, TopLevelTypeId>,
+    ntl_types: ById<'a, NonTopLevelTypeId>,
+
+    // We intern lowercase and uppercase strings separately in order to make lookups faster.
+    // There's an argument for splitting them up even further, e.g.
+    // - Split uppercase into type variables, expression names, and record field names
+    // - Split lowercase into type variables, expression names, and record field names
+    // However, type variables are often either 1 letter (and therefore stored inline) or else
+    // they are the same name as variable name (e.g. `elem`). Record field names commonly
+    // overlap with variable names, especially with punning and destructuring (e.g. `{ foo }`).
+    // Uppercase names tend to be less numerous,
+    lowercase: Interns<'a>,
+    uppercase: Interns<'a>,
+
+    /// We only care about which top-level functions reference which other top-level functions,
+    /// which is necessary in order to topologically sort them into strongly-connected components.
+    /// (Type checking needs them to be sorted this way, and to know which ones are mutually recursive.)
+    /// Everything other than
+    fn_references: Vec16<'a, TopLevelId>,
+
+    // These go together, SoA style
+    names_in_scope: Vec32<'c, InternKey>,
+    ids_in_scope: Vec32<'c, ConstId>,
 
     /// Each nested scope can only have 65K more entries than the previous one. Seems like a safe limit.
     scope_lengths: SimdVec16<'a, u16>,
 
     prev_names_len: u32,
 
-    // Each failed lookup consists of both the key we tried to look up (it apparently wasn't in scope at the time)
-    // as well as the depth at which the lookup occurred, and also
-    failed_lookup_keys: SimdVec16<'a, InternKey>,
-    failed_lookup_depths: SimdVec16<'a, u16>,
-    failed_lookup_scope_ids: SimdVec16<'a, u16>,
+    // Whenever a lookup fails, we record it here in case it ends up being defined later at the top level.
+    missing_const_names: SimdVec16<'a, InternKey>,
+    missing_const_ids: SimdVec16<'a, ConstId>,
+    // Each name/id may have multiple regions which were looking for it.
+    missing_const_regions: SimdVec16<'a, SimdVec16<'a, Region>>,
+    missing_const_is_tl_non_fn: SimdVec16<'a, SimdVec16<'a, bool>>,
 }
 
-impl<'a> Scope<'a> {
+impl<'a> State<'a> {
     /// How many levels deep we are right now.
     /// Remember that depth can increase and then decrease and then increase again!
     pub fn depth() -> u16 {
@@ -86,23 +149,113 @@ impl<'a> Scope<'a> {
         was_in_scope
     }
 
-    /// If it's not in scope, still returns the InternKey so it can be stored for later.
-    pub fn lookup(
-        &mut self,
-        name: &str,
-        arena: &'a mut Arena<'a>,
-        interns: &'a mut Interns<'a>,
-    ) -> Option<InternKey> {
-        let intern_key = interns.key_from_str(name);
-        let answer = self.names.first_index_of(intern_key);
+    pub fn register_tl_non_fn(&mut self, name: &str, region: Region) -> ConstId {
+        // TODO same as register_tl_fn, *except* that for each missing_const_is_tl_non_fn
+        // which corresponds to a failed lookup on this name, we immediately report their
+        // Regions as errors (and remove them from those vecs), because those were necessarily
+        // instances of a top-level non-function trying to refer to another top-level non-function
+        // before it was defined. That is not allowed!
+    }
 
-        if answer.is_none() {
-            self.failed_lookup_keys.push(intern_key);
-            self.failed_lookup_depths.push(self.depth());
+    pub fn register_tl_fn(&mut self, name: &str, region: Region) -> ConstId {
+        let key = self.lowercase.key_from_str(name);
+
+        if let Some(index) = self.tl_fn_names.first_index_of(key) {
+            // We've redeclared this top-level function. That is not allowed!
+            self.problems.push(Problem::ShadowedTopLevelConst {
+                original_region: self.tl_fn_regions.get_unchecked(index),
+                shadowed_region: region,
+            });
+            return self.err_shadowed_tl_const(key);
         }
 
-        todo!(
-            "If pop the scope, check these out and be like 'ok it's fine iff this was a function'"
-        );
+        if let Some(index) = self.tl_non_fn_names.first_index_of(key) {
+            // We've redeclared this top-level value. That is not allowed!
+            self.problems.push(Problem::ShadowedTopLevelConst {
+                original_region: self.tl_non_fn_regions.get_unchecked(index),
+                shadowed_region: region,
+            });
+            return self.err_shadowed_tl_const(key);
+        }
+
+        // Next, check to see if we previously had any lookups which already tried unsuccessfully
+        // to reference this name.
+        let const_id = match self.missing_const_names.first_index_of(key) {
+            Some(index) => unsafe {
+                // It turns out we already had some lookups that tried to reference this top-level
+                // function, but it hadn't been defined yet. We'll use the ConstId they referred to,
+                // so that they're retroactively referring to the right thing!
+                //
+                // Also remove this entry from missing const lookups, so future lookups don't have to
+                // scan through as much. (Also so we don't end up incorrectly reporting it as missing!)
+                self.missing_const_names.remove_swap_unchecked(index);
+                self.missing_const_regions.remove_swap_unchecked(index);
+                self.missing_const_ids.remove_swap_unchecked(index)
+            },
+            None => {
+                // No previous lookups ever tried to reference this, so make a new ConstId for it.
+                self.new_const_id()
+            }
+        };
+
+        self.tl_fn_names.push(key);
+        self.tl_fn_ids.push(const_id);
+        self.tl_fn_regions.push(region);
+
+        const_id
+    }
+
+    /// If it's not in scope, still returns the InternKey so it can be stored for later.
+    pub fn lookup_const(
+        &mut self,
+        name: &str,
+        region: Region,
+        looking_up_from_tl_non_fn: bool,
+    ) -> Node {
+        let key = self.lowercase.key_from_str(name);
+        let const_id;
+
+        match self.names_in_scope.first_index_of(key) {
+            Some(index) => {
+                // It was in scope! Record a normal lookup for it.
+                const_id = unsafe { self.ids_in_scope.get_unchecked(self.arena, index) };
+            }
+            None => {
+                // It was not in scope. Get a ConstId (or create a new one) for it for later.
+                match self.missing_const_names.first_index_of(key) {
+                    Some(index) => {
+                        self.missing_const_is_tl_non_fn
+                            .get_unchecked(index)
+                            .push(looking_up_from_tl_non_fn);
+                        self.missing_const_regions.get_unchecked(index).push(region);
+                        const_id = self.missing_const_ids.get_unchecked(index);
+                    }
+                    None => {
+                        let index = self.missing_const_ids.len();
+
+                        const_id = invalid_lookup_const(key);
+
+                        self.missing_const_ids.push(const_id);
+                        self.missing_const_regions.push(vec![region]);
+                        self.missing_const_is_tl_non_fn
+                            .push(vec![looking_up_from_tl_non_fn]);
+                    }
+                };
+            }
+        };
+
+        // We always return a normal lookup, whether or not we successfully found anything.
+        // This is so that later on, if it turns out there *was* a top-level value which assigned
+        // this name to a value (after the fact), we can update what this ConstId refers to
+        // without needing to go back and fix up all the nodes that referred to it.
+        Node::LookupConst(const_id)
+    }
+
+    fn invalid_lookup_const(&mut self, key: InternKey) -> ConstId {
+        // TODO need to fabricate a new one!
+    }
+
+    fn err_shadowed_tl_const(&mut self, key: InternKey) -> ConstId {
+        // TODO need to fabricate a new one!
     }
 }
--- crates/new_frontend/parse/src/prep_for_can.rs
+++ crates/new_frontend/parse/src/prep_for_can.rs
@@ -4,22 +4,57 @@ use intern::InternKey;
 /// Prepare for canonicalization by:
 /// -
 
-struct State<'a> {
-    /// Each Pattern has a bit indicating whether the pattern continues.
+struct State<'t, 'd, 'r> {
+    /// Arena used for temporary allocations. This will be reset once we've moved to the next phase.
+    tmp: &'t mut Arena<'t>,
+
+    /// Arena used for recording things that last all the way until the reporting step of compilation
+    /// (e.g. interns). In batch builds, these will never be reset. In watch builds, they will only be
+    /// reset in between watch builds.
+    forever: &'r mut Arena<'r>,
+
+    /// Arena used for other durable allocations. Anything allocated in here will live through multiple
+    /// compiler phases.
+    ///
+    /// We can see how much of a difference this makes in the future by using the same arena for
+    /// both the temporary and durable allocators, and then removing the resets. If compile times
+    /// don't get noticeably worse, then it didn't help. But it should save page faults, which
+    /// should be relevant.
+    dur: &'d mut Arena<'d>,
+
+    /// Each of these that's a Pattern has a bit indicating whether the pattern continues.
     /// So we can look at each Pattern entry and tell whether the pattern is done.
-    top_level_patterns: Vec16<'a, Pattern<'a>>,
+    /// (When we encounter a token that tells us the pattern has ended, we can go back
+    /// and mutate the previous node to set the bit indicating it's the end of the Pattern.)
+    /// Similarly, each of these that's an Annotation has such a bit as well.
+    /// Using these combined, plus looking at line numbers, we can tell which annotations
+    /// were in fact immediately preceding patterns. Also I think there's a reasonable argument
+    /// for a design change here, where we just say "as long as the annotation is followed by a
+    /// matching pattern, it goes with it" and then the formatter collapses them. Yeah that
+    /// seems like a better design.
+    nodes: Vec32<'t, Node<'t>>,
 
-    /// The idea is that it will be possible to tell when each of these exprs ends.
-    /// At that point we proceed to the next entry in top_level_defs. There's no
-    /// need to record a mapping between which expr corresponds to which top level def,
-    /// because we only care about that in the course of traversing these, and in that
-    /// situation we can already tell which expr corresponds to which top-level def.
-    top_level_bodies: Vec16<'a, Expr<'a>>,
+    /// As we encounter semantic strings (variables, field names, tag names, type/ability/etc names)
+    /// we intern them in here. String literals just get saved as a byte range into the source file.
+    /// These will be preserved all the way to the end of compilation, because they get used in reporting.
+    interns: Scope<'r>,
 
-    /// TODO: In the future this will be more complex state, e.g. there can be a bunch
-    /// of different things that we're in the middle of.
-    top_level_pattern_in_progress: bool,
+    /// The initial scope we're going to prepare for canonicalization. Bas
+    scope: Scope<'d>,
+
+    /// Which top-level defs reference each other. We need this info in order to reorder them.
+    /// Note that this has to be a growable vec; we may need to resize it as we encounter more
+    /// top-level defs. And since it's n^2 bits, we have to be careful not to preallocate too
+    /// much capacity.
+    top_level_references: Vec32<'t, bool>,
+
+    /// The index in nodes where each top-level def begins. In conjunction
+    /// with top_level_references, we can change the order of this vec and then canonicalization
+    /// can traverse this vec in order to canonicalize the top-level defs in the correct order.
+    top_level_def_offsets: Vec16<'d, u32>,
 }
+
+struct ReferenceMatrix {}
 
 enum Token<'a> {
     OpenCurly,
