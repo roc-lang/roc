24a4367d2dc06809d561695a3bed7960c49b4e16
Richard Feldman <oss@rtfeldman.com>
2024-03-13 23:57:04 -0400

    Add allocation.rs

--- crates/virtual_alloc/src/allocation.rs
+++ crates/virtual_alloc/src/allocation.rs
@@ -11,22 +11,40 @@
 /// list behind the scenes, since wasm only supports growing the heap and that's it. Although
 /// wasm doesn't have a watch mode, it does have long-running processes in the form of the repl
 /// and also potentially in the future a playground.
-use core::{alloc::Layout, cell::Cell, ptr::NonNull};
+use core::{alloc::Layout, cell::Cell, fmt, ptr::NonNull};
 
 #[derive(Debug)]
 pub struct Allocation {
     pages: Cell<Page>,
-    unused_bytes: usize,
+    layout: Layout,
+    len: usize,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Clone, Copy)]
 #[repr(transparent)]
 struct Page {
-    bytes: [u8; PAGE_SIZE],
+    _bytes: [u8; PAGE_SIZE],
+}
+
+impl fmt::Debug for Page {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("Page")
+    }
 }
 
 // All allocations will be rounded up to this number of bytes.
+//
+// https://devblogs.microsoft.com/oldnewthing/20210510-00/?p=105200
+// 16KiB should be accepted by all Windows systems
+#[cfg(any(windows, unix))]
 const PAGE_SIZE: usize = 16384;
+
+// All allocations will be rounded up to this number of bytes.
+//
+// In wasm, "each page is sized 64KiB" according to
+// https://developer.mozilla.org/en-US/docs/webassembly/reference/memory/size
+#[cfg(wasm32)]
+const PAGE_SIZE: usize = 65536;
 
 /// We use wee_alloc for allocations on wasm because wasm natively supports only growing the heap,
 /// not releasing anything. Releasing has to be built in userspace, which wee_alloc provides.
@@ -35,21 +53,6 @@ static WEE_ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct AllocFailed;
-
-#[cfg(debug_assertions)]
-macro_rules! dealloc_failed {
-    ($ptr:expr) => {
-        let mut buf = [0; 256];
-
-        let _ = fs::IoError::most_recent().write(&mut buf);
-
-        panic!(
-            "Tried to deallocate address {:?} but it failed with error: {:?}",
-            $ptr,
-            core::ffi::CStr::from_bytes_until_nul(&buf)
-        );
-    };
-}
 
 impl Allocation {
     /// This may round the requested number of bytes up to the nearest page size,
@@ -83,8 +86,9 @@ impl Allocation {
             #[cfg(target_os = "linux")]
             const MAP_ANONYMOUS: i32 = 0x0020;
 
-            // Round up to nearest OS page size
-            let size = (size + (PAGE_SIZE - 1)) & !(PAGE_SIZE - 1);
+            // Round up to nearest OS page size or the requested alignment,
+            // whichevever is bigger.
+            let size = round_up_to(size, layout.align().max(PAGE_SIZE));
 
             // Safety: We rounded up `size` to the correct multiple already.
             let answer = unsafe {
@@ -108,7 +112,8 @@ impl Allocation {
 
                     Ok(Self {
                         pages: Cell::new(pages),
-                        unused_bytes: size,
+                        len: 0,
+                        layout,
                     })
                 }
                 _ => {
@@ -188,7 +193,44 @@ impl Allocation {
         }
     }
 
-    pub unsafe fn dealloc_virtual(ptr: *mut u8, layout: Layout) {
+    pub fn bytes_remaining(&self) -> usize {
+        self.layout.size().saturating_sub(self.len)
+    }
+
+    pub fn slice_mut(&mut self, layout: Layout) -> &mut [u8] {
+        // We won't return a slice that's bigger than the number of
+        // allocated bytes we have left!
+        let available_len = self.bytes_remaining();
+        let desired_len = available_len.min(layout.size());
+        let desired_align = self.layout.align().max(layout.align());
+
+        // Figure out how much padding we need to achieve the desired alignment
+        let ptr = self.pages.as_ptr() as *mut u8;
+        let padding_needed = round_up_to(ptr as usize, desired_align).saturating_sub(ptr as usize);
+
+        // Figure out what the actual length of the slice will be,
+        // taking into account necessary padding and how mny bytes are left.
+        let actual_len = desired_len
+            .saturating_add(padding_needed)
+            .min(available_len);
+
+        // Advance the pointer past the padding.
+        let ptr = unsafe { ptr.add(padding_needed) };
+
+        // After adding the padding, the pointer should now be aligned correctly!
+        debug_assert_eq!(ptr as usize % desired_align, 0);
+
+        // Record the new length
+        self.len += actual_len;
+
+        unsafe { core::slice::from_raw_parts_mut(ptr, actual_len) }
+    }
+}
+
+impl Drop for Allocation {
+    fn drop(&mut self) {
+        let ptr = self.pages.as_ptr();
+        let layout = self.layout;
         let size = layout.size();
 
         #[cfg(unix)]
@@ -202,12 +244,12 @@ impl Allocation {
             // If deallocation fails, panic in debug builds so we can try to diagnose it
             // (and so that it will fail tests), but silently continue in release builds
             // because a memory leak is generally a better user experience than a crash.
-            let _answer = munmap(ptr as *mut c_void, size);
+            let _answer = unsafe { munmap(ptr as *mut c_void, size) };
 
             #[cfg(debug_assertions)]
             {
                 if _answer < 0 {
-                    dealloc_failed!(ptr);
+                    panic!("Tried to deallocate address {:?} but it failed!", ptr);
                 }
             }
         }
@@ -224,12 +266,12 @@ impl Allocation {
 
             // When calling VirtualAlloc with MEM_RELEASE, the second argument must be 0.
             // https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualfree#parameters
-            let _answer = VirtualFree(ptr as *mut c_void, 0, MEM_RELEASE);
+            let _answer = unsafe { VirtualFree(ptr as *mut c_void, 0, MEM_RELEASE) };
 
             #[cfg(debug_assertions)]
             {
                 if _answer == 0 {
-                    dealloc_failed!(ptr);
+                    panic!("Tried to deallocate address {:?} but it failed!", ptr);
                 }
             }
         }
@@ -244,10 +286,7 @@ impl Allocation {
             #[cfg(debug_assertions)]
             {
                 if _ptr.is_null() {
-                    panic!(
-                        "Tried to deallocate address {:?} but it failed.",
-                        $ptr,
-                    );
+                    panic!("Tried to deallocate address {:?} but it failed!", ptr);
                 }
             }
         }
@@ -277,9 +316,13 @@ fn verify_page_size() {
         {
             // In wasm, "each page is sized 64KiB" according to
             // https://developer.mozilla.org/en-US/docs/webassembly/reference/memory/size
-            16384
+            65536
         }
     };
 
     assert_eq!(os_page_size, PAGE_SIZE);
+}
+
+fn round_up_to(num: usize, rounding_to: usize) -> usize {
+    (num + (rounding_to - 1)) & !(rounding_to - 1)
 }
