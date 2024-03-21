5e2fc1c3fdb0fe9dc31b59be678c792af3435105
Luke Boswell <lukewilliamboswell@gmail.com>
2024-03-21 18:04:52 +1100

    handle suffixed after pizza |>

--- crates/compiler/can/src/desugar.rs
+++ crates/compiler/can/src/desugar.rs
@@ -838,12 +838,28 @@ pub fn desugar_expr<'a>(
         }
         LowLevelDbg(_, _, _) => unreachable!("Only exists after desugaring"),
         Suffixed(expr) => {
-            // This is desugared in Defs, leave the node here
+            // Rewrite `Suffixed(BinOps([args...], Var(...)))` to `BinOps([args...], Suffixed(Var(...)))`
+            // This is to handle cases like e.g. `"Hello" |> line!`
+            if let BinOps(args, sub_expr) = expr {
+                return desugar_expr(
+                    arena,
+                    arena.alloc(Loc::at(
+                        loc_expr.region,
+                        BinOps(
+                            args,
+                            arena.alloc(Loc::at(sub_expr.region, Suffixed(&sub_expr.value))),
+                        ),
+                    )),
+                    src,
+                    line_info,
+                    module_path,
+                );
+            }
+
+            // Suffixed are also desugared in Defs
             // Any nodes that don't get desugared will be caught by canonicalize_expr
-            arena.alloc(Loc {
-                value: Suffixed(expr),
-                region: loc_expr.region,
-            })
+            // and we can handle those cases as required
+            loc_expr
         }
     }
 }
