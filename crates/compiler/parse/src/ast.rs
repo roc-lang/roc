a5df0aa1c2a241126d810a1451c90fbf37f6dd49
Luke Boswell <lukewilliamboswell@gmail.com>
2024-03-24 12:30:05 +1100

    implement equivalent for Pattern::Stmt

--- crates/compiler/parse/src/ast.rs
+++ crates/compiler/parse/src/ast.rs
@@ -1130,7 +1130,13 @@ impl<'a> Pattern<'a> {
                 }
             }
 
-            Stmt(_) => todo!(),
+            Stmt(str_x) => {
+                if let Stmt(str_y) = other {
+                    str_x == str_y
+                } else {
+                    false
+                }
+            }
         }
     }
 }
