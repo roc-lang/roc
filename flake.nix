686e1b91464fde632f47f8d1ff02bf32d0aebac6
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2024-04-06 14:16:40 +0200

    updated flake.nix as well

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Roc flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?rev=886c9aee6ca9324e127f9c2c4e6f68c2641c8256";
+    nixpkgs.url = "github:nixos/nixpkgs?rev=fd281bd6b7d3e32ddfa399853946f782553163b5";
 
     # rust from nixpkgs has some libc problems, this is patched in the rust-overlay
     rust-overlay = {
