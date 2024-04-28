7fcf13d14b77cc7ad2af4d17bc38e091b7cce53a
Richard Feldman <oss@rtfeldman.com>
2024-04-28 11:26:37 -0400

    Update some tests to use `!`

--- crates/cli/tests/cli_run.rs
+++ crates/cli/tests/cli_run.rs
@@ -952,12 +952,13 @@ mod cli_run {
                     packages { pf: "https://github.com/roc-lang/basic-cli/releases/download/0.10.0/vNe6s9hWzoTZtFmNkvEICPErI9ptji_ySjicO6CkucY.tar.br" }
                     imports [
                         pf.Stdout,
+                        pf.Task,
                         "ingested-file.roc" as ownCode : Str,
                     ]
                     provides [main] to pf
 
                 main =
-                    Stdout.line "\nThis roc file can print its own source code. The source is:\n\n$(ownCode)"
+                    Stdout.line! "\nThis roc file can print its own source code. The source is:\n\n$(ownCode)"
 
                 "#
             ),
