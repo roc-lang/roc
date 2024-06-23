96ac005632c858b8969ed31254bfd8997236ab1b
Ryan Barth <ryan@barth.tech>
2024-06-23 10:49:18 -0700

    refactor: release script cleanup

--- ci/package_release.sh
+++ ci/package_release.sh
@@ -7,21 +7,17 @@ set -euxo pipefail
 strip ./target/release-with-lto/roc
 strip ./target/release-with-lto/roc_language_server
 
-# to be able to delete "target" later
-cp target/release-with-lto/roc ./roc
-cp target/release-with-lto/roc_language_server ./roc_language_server
-cp target/release-with-lto/wasi-libc.a wasi-libc.a
-
-# delete unnecessary files and folders
-git clean -fdx --exclude roc --exclude roc_language_server --exclude wasi-libc.a
-
 mkdir -p $1 $1/examples $1/crates/compiler/builtins/bitcode
 
-mv roc roc_language_server lib LICENSE LEGAL_DETAILS $1
+mv target/release-with-lto/{roc,roc_language_server,lib} $1
+mv LICENSE LEGAL_DETAILS $1
 
-mv examples/helloWorld.roc examples/platform-switching examples/cli $1/examples
+mv examples/{helloWorld.roc,platform-switching,cli} $1/examples
 
 mv crates/roc_std $1/crates
 mv crates/compiler/builtins/bitcode/src $1/crates/compiler/builtins/bitcode
  
 tar -czvf "$1.tar.gz" $1
+
+# delete unnecessary files and folders
+git clean -fdx --exclude $1.tar.gz
