48fffa0e777d71d65cb030823fdad265af2e8582
Mattthew Heath <matthew.john.heath@gmail.com>
2024-07-01 22:28:48 +0100

    zig functions

--- crates/compiler/builtins/bitcode/src/crypt.zig
+++ crates/compiler/builtins/bitcode/src/crypt.zig
@@ -5,3 +5,17 @@ const sha2 = crypto.hash.sha2;
 pub fn sha256( )sha2.Sha256{
     return sha2.Sha256.init(.{});
 }
+
+pub fn addBytes(hasher: sha2.Sha256, bytes:[] const u8)sha2.Sha256{
+    var out = hasher;
+    out.update(bytes);
+    return out;
+}
+
+const sha256_digest_length = 32;
+
+pub const sha256_digest =  [sha256_digest_length2]u8;
+
+pub fn digest(hasher: sha2.Sha256 ): sha256_digest{
+    return hasher.peek();
+}
