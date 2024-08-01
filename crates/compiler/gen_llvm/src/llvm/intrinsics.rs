93b7056ccc5a17f6e1a1094cf13cd0db0a0b70a5
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2024-07-31 19:33:46 -0700

    update stacksave pointer name

--- crates/compiler/gen_llvm/src/llvm/intrinsics.rs
+++ crates/compiler/gen_llvm/src/llvm/intrinsics.rs
@@ -138,7 +138,7 @@ pub static LLVM_MEMSET_I64: &str = "llvm.memset.p0i8.i64";
 pub static LLVM_MEMSET_I32: &str = "llvm.memset.p0i8.i32";
 
 pub static LLVM_FRAME_ADDRESS: &str = "llvm.frameaddress.p0";
-pub static LLVM_STACK_SAVE: &str = "llvm.stacksave";
+pub static LLVM_STACK_SAVE: &str = "llvm.stacksave.p0";
 
 pub static LLVM_SETJMP: &str = "llvm.eh.sjlj.setjmp";
 pub static LLVM_LONGJMP: &str = "llvm.eh.sjlj.longjmp";
