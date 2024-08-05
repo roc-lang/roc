12e8710f4ffa84170b38c2b703fab809662a0a82
Luke Boswell <lukewilliamboswell@gmail.com>
2024-08-05 10:36:59 +1000

    improve comment on llvm_passes

--- crates/compiler/build/src/llvm_passes.rs
+++ crates/compiler/build/src/llvm_passes.rs
@@ -1,6 +1,9 @@
 use roc_mono::ir::OptLevel;
 
 // generated using "opt --passes="default<Oz>" --print-pipeline-passes example.ll"
+//
+// we also include a "globaldce" pass at the beggining of each pipeline here, which is required
+// to prevent bugs with the surgical linker, and also improves the build time.
 pub fn get_llvm_passes_str(opt_level: OptLevel) -> &'static str {
     match opt_level {
         OptLevel::Development | OptLevel::Normal => {
