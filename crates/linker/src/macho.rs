88962f9e981e81cd725394024e4ef739a20941a4
Jakub Konka <kubkon@jakubkonka.com>
2024-10-16 22:54:34 +0200

    ignore __eh_frame section for the time being in macho surgery

--- crates/linker/src/macho.rs
+++ crates/linker/src/macho.rs
@@ -1207,6 +1207,7 @@ fn surgery_macho_help(
     let rodata_sections: Vec<Section> = app_obj
         .sections()
         .filter(|sec| sec.kind() == SectionKind::ReadOnlyData)
+        .filter(|sec| sec.name().unwrap_or("") != "__eh_frame") // ignore __eh_frame for now
         .collect();
 
     // bss section is like rodata section, but it has zero file size and non-zero virtual size.
