8bc739b600581290644da319a10eaed53f7365d1
Richard Feldman <oss@rtfeldman.com>
2024-10-21 02:17:11 -0400

    Revise API again

--- crates/version_selection/src/version_deps.rs
+++ crates/version_selection/src/version_deps.rs
@@ -1,51 +1,28 @@
-use std::collections::{BTreeSet, HashMap, HashSet};
+use std::collections::HashMap;
 use std::hash::Hash;
 
 use roc_collections::{VecMap, VecSet};
 
 pub struct Deps<Pkg, Ver> {
     exact_versions: VecMap<Pkg, Ver>,
-    versions: HashMap<Pkg, BTreeSet<Ver>>,
     indirect_deps: HashMap<(Pkg, Ver), VecMap<Pkg, Ver>>,
-    exclusions: VecSet<(Pkg, Ver)>,
 }
 
 impl<Pkg: Ord + Clone + Copy + Eq + Hash, Ver: Ord + Clone + Copy + Eq + Hash> Deps<Pkg, Ver> {
     /// Root modules always require exact versions for all their dependencies.
-    pub fn from_root(
-        root_deps: impl IntoIterator<Item = (Pkg, Ver)>,
-        exclusions: impl IntoIterator<Item = (Pkg, Ver)>,
-    ) -> Result<Self, VecSet<Pkg>> {
-        let exclusions: VecSet<(Pkg, Ver)> = exclusions.into_iter().collect();
-        let root_deps = root_deps.into_iter();
-        let mut exact_versions = VecMap::with_capacity(root_deps.size_hint().0);
-
-        // Verify that no package-versions are both required and excluded.
-        // If any are, error out before attempting anything else.
-        let mut excluded_direct_deps = VecSet::default();
-
-        for (pkg, ver) in root_deps {
-            if exclusions.contains(&(pkg, ver)) {
-                // Package version is excluded; we're going to return early.
-                excluded_direct_deps.insert(pkg);
-            } else {
-                exact_versions.insert(pkg, ver);
-            }
-        }
-
-        if excluded_direct_deps.is_empty() {
-            Ok(Self {
-                exact_versions,
-                exclusions,
-                indirect_deps: HashMap::default(),
-            })
-        } else {
-            Err(excluded_direct_deps)
+    /// By design, there are no CLI arguments for specifying things like forcing particular indirect
+    /// dependencies to have particular versions, or excluding particular versions or ranges.
+    /// Instead, if you want to do that, do it by specifying an exact version in the root module.
+    /// (An upgrade tool can help you find an alternative exact version to use if you want to exclude one.)
+    pub fn new(exact_versions: VecMap<Pkg, Ver>) -> Self {
+        Self {
+            exact_versions,
+            indirect_deps: HashMap::default(),
         }
     }
 
-    fn is_excluded(&self, pkg: Pkg, ver: Ver) -> bool {
-        self.exclusions.contains(&(pkg, ver))
+    pub fn from_iter(exact_versions: impl IntoIterator<Item = (Pkg, Ver)>) -> Self {
+        Self::new(exact_versions.into_iter().collect())
     }
 
     /// Record that a package depends on (at least) these versions of other packages.
@@ -57,12 +34,9 @@ impl<Pkg: Ord + Clone + Copy + Eq + Hash, Ver: Ord + Clone + Copy + Eq + Hash> D
         ver: Ver,
         deps: impl IntoIterator<Item = (Pkg, Ver)>,
     ) {
-        // Excluded package/version combinations don't even get recorded. It's like they don't exist!
-        if !self.is_excluded(pkg, ver) {
-            self.indirect_deps
-                .entry((pkg, ver))
-                .or_insert_with(|| deps.into_iter().collect());
-        }
+        self.indirect_deps
+            .entry((pkg, ver))
+            .or_insert_with(|| deps.into_iter().collect());
     }
 
     /// Consume this and return an exact version to use for each package in the dep tree.
@@ -75,6 +49,9 @@ impl<Pkg: Ord + Clone + Copy + Eq + Hash, Ver: Ord + Clone + Copy + Eq + Hash> D
     /// If solving fails, this returns a Set of the packages that couldn't be solved.
     /// (Detecting dependency cycles should be done separately.)
     pub fn select_versions(self) -> Result<VecMap<Pkg, Ver>, VecSet<Pkg>> {
+        let mut indirect_deps = self.indirect_deps;
+        let mut failed_pkgs = VecSet::default();
+
         // Map of package to its required version. Use VecMap for deterministic ordering.
         // Initialize required_versions with exact_versions
         let mut required_versions: VecMap<Pkg, Ver> = self.exact_versions.clone();
@@ -86,57 +63,83 @@ impl<Pkg: Ord + Clone + Copy + Eq + Hash, Ver: Ord + Clone + Copy + Eq + Hash> D
         let mut updated_packages: VecSet<Pkg> = VecSet::default();
 
         // First, go through and delete all the unreachable dependencies.
-        // For example, if `foo@1` depends on `bar@2`, but `bar@2` is excluded,
-        // see if we can find a `bar@3` to use instead, etc. If we ultimately
-        // find a version of `bar` that `foo@1` can depend on, update `foo@1` to
-        // depend on that one. Otherwise, remove `foo@1` from the deps, because
-        // we can't use it, and then go recursively do this to everyone who depends
-        // on `foo@1` because we can't use them either unless they can find a `foo@2`
-        // or something to depend on.
-        let todo = ();
+        // For example, if `foo@1` depends on `bar@2`, but `bar` is required
+        // to be exactly 3 because of the root, then update bar to depend on 3.
+        // That way, later on it'll solve to the correct version. However,
+        // if instead `bar` is required to be exactly 1, then the fact that `foo@1`
+        // depends on `bar@2` means we can't use `foo@1`, so remove it from the deps,
+        // and then go recursively do this to everyone who depends on `foo@1` because
+        // we can't use them either unless they can find a `foo@2` or something to depend on.
+        // Initialize a set to keep track of package-versions to remove
+        {
+            let mut unusable: VecSet<(Pkg, Ver)> = VecSet::default();
+            let mut changed = true;
+
+            while changed {
+                changed = false;
+                for (&(pkg, ver), deps) in indirect_deps.iter() {
+                    if unusable.contains(&(pkg, ver)) {
+                        continue;
+                    }
+                    for (&dep_pkg, &dep_ver) in deps.iter() {
+                        if let Some(&req_ver) = self.exact_versions.get(&dep_pkg) {
+                            if dep_ver != req_ver {
+                                // Conflict between dep_ver and required version
+                                unusable.insert((pkg, ver));
+                                changed = true;
+                                break;
+                            }
+                        }
+                        if unusable.contains(&(dep_pkg, dep_ver)) {
+                            // Dependency on an unusable package-version
+                            unusable.insert((pkg, ver));
+                            changed = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // Remove the unusable packages from indirect_deps
+            for (pkg, ver) in unusable {
+                indirect_deps.remove(&(pkg, ver));
+            }
+        }
 
         while let Some(pkg) = packages_to_process.pop() {
             let ver = *required_versions.get(&pkg).unwrap();
 
             // Get dependencies of this package-version
-            if let Some(deps) = self.indirect_deps.get(&(pkg, ver)) {
-                for (&dep_pkg, &dep_ver) in deps.iter() {
-                    // This package-version is excluded. Try to find a higher one that isn't!
-                    if self.is_excluded(dep_pkg, dep_ver) {
-                        let todo = ();
-                        // TODO if it was excluded, go find in our versions list the next version
-                        // of this package that isn't excluded. (We can pre-prune that one to not
-                        // include excluded ones.)
-                        // If we can't find one, that means this
-                    }
+            match indirect_deps.get(&(pkg, ver)) {
+                Some(deps) => {
+                    for (&dep_pkg, &dep_ver) in deps.iter() {
+                        // See if dep_pkg is in required_versions
+                        match required_versions.get(&dep_pkg) {
+                            Some(&existing_ver) => {
+                                if dep_ver > existing_ver {
+                                    // Use the higher version
+                                    required_versions.insert(dep_pkg, dep_ver);
+                                    packages_to_process.insert(dep_pkg);
+                                    updated_packages.insert(dep_pkg);
+                                }
 
-                    // See if dep_pkg is in required_versions
-                    if let Some(&existing_ver) = required_versions.get(&dep_pkg) {
-                        if dep_ver > existing_ver {
-                            // Use the higher version
-                            required_versions.insert(dep_pkg, dep_ver);
-                            packages_to_process.insert(dep_pkg);
-                            updated_packages.insert(dep_pkg);
+                                // (If the existing version was already higher or equal, do nothing.)
+                            }
+                            None => {
+                                // First time we see dep_pkg
+                                required_versions.insert(dep_pkg, dep_ver);
+                                packages_to_process.insert(dep_pkg);
+                                updated_packages.insert(dep_pkg);
+                            }
                         }
-
-                        // (If the existing version was already higher or equal, do nothing.)
-                    } else {
-                        // First time we see dep_pkg
-                        required_versions.insert(dep_pkg, dep_ver);
-                        packages_to_process.insert(dep_pkg);
-                        updated_packages.insert(dep_pkg);
                     }
                 }
-            } else {
-                // We couldnd't find an entry in indirect_deps for this
-            }
-        }
-
-        // After processing, check if required_versions satisfy the exact_versions
-        let mut failed_pkgs = HashSet::new();
-        for (pkg, ver) in self.exact_versions.iter() {
-            if required_versions.get(pkg) != Some(ver) {
-                failed_pkgs.insert(pkg.clone());
+                None => {
+                    // We couldn't find an entry in indirect_deps for this. This means it must have been
+                    // removed from the tree as erroneous (because it depended on a conflicting version
+                    // of something due to the root), so we must error out!
+                    failed_pkgs.insert(pkg);
+                }
             }
         }
 
@@ -154,7 +157,7 @@ mod tests {
 
     #[test]
     fn test_from_root() {
-        let deps = Deps::from_root([("a", 1), ("b", 2)], []);
+        let deps = Deps::from_iter([("a", 1), ("b", 2)]);
         assert_eq!(deps.exact_versions.len(), 2);
         assert_eq!(deps.exact_versions.get(&"a"), Some(&1));
         assert_eq!(deps.exact_versions.get(&"b"), Some(&2));
@@ -162,7 +165,7 @@ mod tests {
 
     #[test]
     fn test_pkg_depends_on() {
-        let mut deps = Deps::from_root([("a", 1)], []);
+        let mut deps = Deps::from_iter([("a", 1)]);
         deps.pkg_depends_on("a", 1, [("b", 2), ("c", 3)]);
         deps.pkg_depends_on("b", 2, [("d", 4)]);
 
@@ -173,18 +176,18 @@ mod tests {
 
     #[test]
     fn test_select_versions_simple() {
-        let mut deps = Deps::from_root([("a", 1)], []);
+        let mut deps = Deps::from_iter([("a", 1)]);
         deps.pkg_depends_on("a", 1, [("b", 2)]);
         let result = deps.select_versions().unwrap();
 
         assert_eq!(result.len(), 2);
-        assert_eq!(result["a"], 1);
-        assert_eq!(result["b"], 2);
+        assert_eq!(result.get(&"a").copied(), Some(1));
+        assert_eq!(result.get(&"b").copied(), Some(2));
     }
 
     #[test]
     fn test_select_versions_incompatible() {
-        let mut deps = Deps::from_root([("a", 1), ("b", 2)], []);
+        let mut deps = Deps::from_iter([("a", 1), ("b", 2)]);
         deps.pkg_depends_on("a", 1, [("b", 3)]);
         let bad_pkgs = deps.select_versions().unwrap_err();
 
@@ -193,7 +196,7 @@ mod tests {
 
     #[test]
     fn test_select_versions_multiple_incompatibilities() {
-        let mut deps = Deps::from_root([("a", 1), ("b", 2), ("c", 3)], []);
+        let mut deps = Deps::from_iter([("a", 1), ("b", 2), ("c", 3)]);
         deps.pkg_depends_on("a", 1, [("b", 3), ("c", 4)]);
         deps.pkg_depends_on("b", 2, [("c", 1)]);
         deps.pkg_depends_on("c", 3, [("d", 4)]);
@@ -204,7 +207,7 @@ mod tests {
 
     #[test]
     fn test_select_versions_complex_scenario() {
-        let mut deps = Deps::from_root([("a", 1), ("b", 1)], []);
+        let mut deps = Deps::from_iter([("a", 1), ("b", 1)]);
         deps.pkg_depends_on("a", 1, [("c", 2), ("d", 1)]);
         deps.pkg_depends_on("b", 1, [("c", 3), ("e", 1)]); // means we'll select c@3
         deps.pkg_depends_on("c", 2, [("f", 4)]); // won't be selected bc we select c@3
@@ -218,47 +221,47 @@ mod tests {
         deps.pkg_depends_on("h", 2, []); // will be selected bc e@1 depends on it
         let result = deps.select_versions().unwrap();
 
-        assert_eq!(result["a"], 1);
-        assert_eq!(result["b"], 1);
-        assert_eq!(result["c"], 3);
-        assert_eq!(result["d"], 1);
-        assert_eq!(result["e"], 1);
-        assert_eq!(result["f"], 2);
-        assert_eq!(result["g"], 1);
-        assert_eq!(result["h"], 2);
+        assert_eq!(result.get(&"a").copied(), Some(1));
+        assert_eq!(result.get(&"b").copied(), Some(1));
+        assert_eq!(result.get(&"c").copied(), Some(3));
+        assert_eq!(result.get(&"d").copied(), Some(1));
+        assert_eq!(result.get(&"e").copied(), Some(1));
+        assert_eq!(result.get(&"f").copied(), Some(2));
+        assert_eq!(result.get(&"g").copied(), Some(1));
+        assert_eq!(result.get(&"h").copied(), Some(2));
         assert_eq!(result.len(), 8);
     }
 
     #[test]
     fn test_select_versions_with_multiple_paths() {
-        let mut deps = Deps::from_root([("a", 1)], []);
+        let mut deps = Deps::from_iter([("a", 1)]);
         deps.pkg_depends_on("a", 1, [("b", 1), ("c", 1)]);
         deps.pkg_depends_on("b", 1, [("d", 2)]);
         deps.pkg_depends_on("c", 1, [("d", 3)]);
         let result = deps.select_versions().unwrap();
 
-        assert_eq!(result["a"], 1);
-        assert_eq!(result["b"], 1);
-        assert_eq!(result["c"], 1);
-        assert_eq!(result["d"], 3);
+        assert_eq!(result.get(&"a").copied(), Some(1));
+        assert_eq!(result.get(&"b").copied(), Some(1));
+        assert_eq!(result.get(&"c").copied(), Some(1));
+        assert_eq!(result.get(&"d").copied(), Some(3));
         assert_eq!(result.len(), 4);
     }
 
     #[test]
     fn test_select_versions_circular_dependency() {
-        let mut deps = Deps::from_root([("a", 1)], []);
+        let mut deps = Deps::from_iter([("a", 1)]);
         deps.pkg_depends_on("a", 1, [("b", 1)]);
         deps.pkg_depends_on("b", 1, [("a", 1)]);
         let result = deps.select_versions().unwrap();
 
-        assert_eq!(result["a"], 1);
-        assert_eq!(result["b"], 1);
+        assert_eq!(result.get(&"a").copied(), Some(1));
+        assert_eq!(result.get(&"b").copied(), Some(1));
         assert_eq!(result.len(), 2);
     }
 
     #[test]
     fn test_select_versions_empty() {
-        let deps: Deps<&'static str, i32> = Deps::from_root([], []);
+        let deps: Deps<&'static str, i32> = Deps::from_iter([]);
         let result = deps.select_versions().unwrap();
 
         assert_eq!(result.len(), 0);
@@ -266,7 +269,7 @@ mod tests {
 
     #[test]
     fn test_backtracking() {
-        let mut deps = Deps::from_root([("foo", 1), ("bar", 1)], []);
+        let mut deps = Deps::from_iter([("foo", 1), ("bar", 1)]);
         // Here, our `foo` dependency leads us to select v2 of baz.
         deps.pkg_depends_on("foo", 1, [("baz", 2)]);
         // If we only consider `foo`, we would select v2 of baz, and therefore v3 of `other`.
@@ -277,36 +280,36 @@ mod tests {
         deps.pkg_depends_on("bar", 1, [("baz", 3)]);
         let result = deps.select_versions().unwrap();
 
-        assert_eq!(result["foo"], 1);
-        assert_eq!(result["bar"], 1);
-        assert_eq!(result["baz"], 3);
-        assert_eq!(result["other"], 1);
+        assert_eq!(result.get(&"foo").copied(), Some(1));
+        assert_eq!(result.get(&"bar").copied(), Some(1));
+        assert_eq!(result.get(&"baz").copied(), Some(3));
+        assert_eq!(result.get(&"other").copied(), Some(1));
         assert_eq!(result.len(), 4);
     }
 
     #[test]
     fn test_consistent_higher_version_selection() {
-        let mut deps = Deps::from_root([("a", 1), ("b", 1)], []);
+        let mut deps = Deps::from_iter([("a", 1), ("b", 1)]);
         deps.pkg_depends_on("a", 1, [("c", 2)]);
         deps.pkg_depends_on("b", 1, [("c", 3)]);
         let result = deps.select_versions().unwrap();
 
-        assert_eq!(result["a"], 1);
-        assert_eq!(result["b"], 1);
-        assert_eq!(result["c"], 3);
+        assert_eq!(result.get(&"a").copied(), Some(1));
+        assert_eq!(result.get(&"b").copied(), Some(1));
+        assert_eq!(result.get(&"c").copied(), Some(3));
     }
 
     #[test]
     fn test_multiple_higher_versions_consistency() {
-        let mut deps = Deps::from_root([("a", 1), ("b", 1), ("c", 1)], []);
+        let mut deps = Deps::from_iter([("a", 1), ("b", 1), ("c", 1)]);
         deps.pkg_depends_on("a", 1, [("d", 2)]);
         deps.pkg_depends_on("b", 1, [("d", 3)]);
         deps.pkg_depends_on("c", 1, [("d", 4)]);
         let result = deps.select_versions().unwrap();
 
-        assert_eq!(result["a"], 1);
-        assert_eq!(result["b"], 1);
-        assert_eq!(result["c"], 1);
-        assert_eq!(result["d"], 4);
+        assert_eq!(result.get(&"a").copied(), Some(1));
+        assert_eq!(result.get(&"b").copied(), Some(1));
+        assert_eq!(result.get(&"c").copied(), Some(1));
+        assert_eq!(result.get(&"d").copied(), Some(4));
     }
 }
