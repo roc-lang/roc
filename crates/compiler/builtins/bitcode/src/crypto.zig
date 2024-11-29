e54ff61ed6488d4628448b3512b6688b9747b525
Mattthew Heath <matthew.john.heath@gmail.com>
2024-11-29 16:13:54 +0000

    add test

--- crates/compiler/builtins/bitcode/src/crypto.zig
+++ crates/compiler/builtins/bitcode/src/crypto.zig
@@ -3,6 +3,7 @@ const crypto = std.crypto;
 const sha2 = crypto.hash.sha2;
 const list = @import("list.zig");
 const utils = @import("utils.zig");
+const testing = std.testing;
 
 const Sha256 = extern struct {
     location: [*]u8,
@@ -20,8 +21,14 @@ pub fn emptySha256() callconv(.C) Sha256 {
     };
 }
 
+test "emptySha256" {
+    const emptyHash = emptySha256().pointer().*.peek();
+    try std.testing.expectEqualSlices(u8, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", emptyHash[0..emptyHash.len]);
+    }
+
 pub fn sha256AddBytes(sha: Sha256, data: list.RocList) callconv(.C) Sha256 {
     var out = emptySha256();
+    std.debug.print("Here!", .{});
     out.pointer().* = sha.pointer().*;
     if (data.bytes) |bytes| {
         const byteSlice: []u8 = bytes[0..data.length];
