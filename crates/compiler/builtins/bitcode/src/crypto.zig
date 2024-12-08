056b918701ed9b0a3cb6aaa1b90aa76c6edc60f2
Mattthew Heath <matthew.john.heath@gmail.com>
2024-12-08 18:44:34 +0000

    Removed alloc calls from zig code

--- crates/compiler/builtins/bitcode/src/crypto.zig
+++ crates/compiler/builtins/bitcode/src/crypto.zig
@@ -7,50 +7,45 @@ const utils = @import("utils.zig");
 const testing = std.testing;
 
 const Sha256 = extern struct {
-    location: *sha2.Sha256,
+    data: [@sizeOf(sha2.Sha256)]u8 align(@alignOf(sha2.Sha256)),
 };
 
-fn create(comptime T: type) *T {
-    //test_roc_alloc ignores alignment
-    if (builtin.is_test) {
-        return std.testing.allocator.create(T) catch unreachable;
-    }
-    return @alignCast(@ptrCast(utils.allocateWithRefcount(@sizeOf(sha2.Sha256), @alignOf(sha2.Sha256), false)));
+test "Sha256 size and alignment" {
+    try std.testing.expectEqual(@sizeOf(Sha256), 128);
+    try std.testing.expectEqual(@alignOf(Sha256), 16);
 }
 
 pub fn emptySha256() callconv(.C) Sha256 {
-    const location: *sha2.Sha256 = create(sha2.Sha256);
-    location.* = sha2.Sha256.init(.{});
-    return Sha256{
-        .location = location,
-    };
+    var sha: Sha256 = undefined;
+    const ptr: *sha2.Sha256 = @constCast(@ptrCast(&sha));
+    ptr.* = sha2.Sha256.init(.{});
+    return sha;
 }
 
 test "emptySha256" {
     const empty_sha = emptySha256();
-    defer std.testing.allocator.destroy(empty_sha.location);
-    const empty_hash = empty_sha.location.*.peek();
+    const ptr: *sha2.Sha256 = @constCast(@ptrCast(&empty_sha));
+    const empty_hash = ptr.*.peek();
     try std.testing.expect(sameBytesAsHex("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", empty_hash[0..empty_hash.len]));
 }
 
 pub fn sha256AddBytes(sha: Sha256, data: list.RocList) callconv(.C) Sha256 {
-    const out = emptySha256();
-    out.location.* = sha.location.*;
+    var out = sha;
+    const out_ptr: *sha2.Sha256 = @constCast(@ptrCast(&out));
     if (data.bytes) |bytes| {
         const byteSlice: []u8 = bytes[0..data.length];
-        out.location.*.update(byteSlice);
+        out_ptr.*.update(byteSlice);
     }
     return out;
 }
 
 test "sha256AddBytes" {
     const empty_sha = emptySha256();
-    defer std.testing.allocator.destroy(empty_sha.location);
     const abc = list.RocList.fromSlice(u8, "abc", false);
     defer abc.decref(@alignOf(u8), @sizeOf(u8), false, rcNone);
     const abc_sha = sha256AddBytes(empty_sha, abc);
-    defer std.testing.allocator.destroy(abc_sha.location);
-    const abc_hash = abc_sha.location.*.peek();
+    const abc_ptr: *sha2.Sha256 = @constCast(@ptrCast(&abc_sha));
+    const abc_hash = abc_ptr.*.peek();
     try std.testing.expect(sameBytesAsHex("ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad", abc_hash[0..abc_hash.len]));
 }
 
@@ -60,19 +55,19 @@ pub const Digest256 = extern struct {
 };
 
 pub fn sha256Digest(sha: Sha256) callconv(.C) Digest256 {
-    return @bitCast(sha.location.*.peek());
+    const ptr: *sha2.Sha256 = @constCast(@ptrCast(&sha));
+    return @bitCast(ptr.*.peek());
 }
 
 test "sha256Digest" {
     const empty_sha = emptySha256();
-    defer std.testing.allocator.destroy(empty_sha.location);
     const digest = sha256Digest(empty_sha);
     const first_half_bytes: [16]u8 = @bitCast(digest.first_half);
     const second_half_bytes: [16]u8 = @bitCast(digest.second_half);
     try std.testing.expect(sameBytesAsHex("e3b0c44298fc1c149afbf4c8996fb924", first_half_bytes[0..first_half_bytes.len]));
     try std.testing.expect(sameBytesAsHex("27ae41e4649b934ca495991b7852b855", second_half_bytes[0..second_half_bytes.len]));
 }
-//----------------test utilities ------------------------
+// ----------------test utilities ------------------------
 fn rcNone(_: ?[*]u8) callconv(.C) void {}
 
 fn sameBytesAsHex(comptime expected_hex: [:0]const u8, input: []const u8) bool {
