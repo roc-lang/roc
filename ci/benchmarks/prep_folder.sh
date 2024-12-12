8ec999f8010d54a184a42cc8904e6c5f25c05ce0
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2024-12-11 22:47:24 -0800

    try quotes for wildcard

--- ci/benchmarks/prep_folder.sh
+++ ci/benchmarks/prep_folder.sh
@@ -16,7 +16,7 @@ mkdir -p bench-folder/target/release/deps
 mkdir -p bench-folder/target/release/lib
 cp "crates/cli/tests/benchmarks/"*".roc" bench-folder/crates/cli/tests/benchmarks/
 cp -r crates/cli/tests/benchmarks/platform bench-folder/crates/cli/tests/benchmarks/
-cp crates/compiler/builtins/bitcode/src/*.zig bench-folder/target/release/lib/
+cp "crates/compiler/builtins/bitcode/src/*.zig" bench-folder/target/release/lib/
 cp target/release/roc bench-folder/target/release
 
 # copy the most recent time bench to bench-folder
