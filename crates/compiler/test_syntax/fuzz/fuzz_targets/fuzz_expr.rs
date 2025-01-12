5ebd6e0884db65f2d099dfb0ebf255d0bdf80c2b
Joshua Warner <joshuawarner32@gmail.com>
2025-01-11 20:23:42 -0800

    Only conditionally fuzz canonicalize

--- crates/compiler/test_syntax/fuzz/fuzz_targets/fuzz_expr.rs
+++ crates/compiler/test_syntax/fuzz/fuzz_targets/fuzz_expr.rs
@@ -5,13 +5,19 @@ use roc_parse::ast::Malformed;
 use test_syntax::test_helpers::Input;
 
 fuzz_target!(|data: &[u8]| {
+    let canonicalize_fuzz_config = if cfg!(feature = "fuzz_canonicalize") {
+        Some(false)
+    } else {
+        None
+    };
+
     if let Ok(input) = std::str::from_utf8(data) {
         let input = Input::Expr(input);
         let arena = Bump::new();
         let ast = input.parse_in(&arena);
         if let Ok(ast) = ast {
             if !ast.is_malformed() {
-                input.check_invariants(|_| (), true, Some(false));
+                input.check_invariants(|_| (), true, canonicalize_fuzz_config);
             }
         }
     }
