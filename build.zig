b81cfdc4b03cf953f10889e1da275957ac20a1f7
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2025-02-14 09:50:03 -0800

    move fuzz function above all llvm functions

--- build.zig
+++ build.zig
@@ -139,6 +139,55 @@ pub fn build(b: *std.Build) void {
     }
 }
 
+fn add_fuzz_target(
+    b: *std.Build,
+    build_afl: bool,
+    parent_step: *Step,
+    target: ResolvedTarget,
+    name: []const u8,
+    root_source_file: LazyPath,
+    imports: []const Import,
+) void {
+    const fuzz_obj = b.addObject(.{
+        .name = b.fmt("{s}_obj", .{name}),
+        .root_source_file = root_source_file,
+        .target = target,
+        .optimize = .ReleaseSafe,
+    });
+
+    for (imports) |import| {
+        fuzz_obj.root_module.addImport(import.name, import.module);
+    }
+
+    // TODO: Once 0.14.0 is released, uncomment this. Will make fuzzing work better.
+    // Until then, to get the best fuzzing result modify the std library as specified here:
+    // https://github.com/kristoff-it/zig-afl-kit?tab=readme-ov-file#-------important-------
+    // fuzz_obj.root_module.fuzz = true;
+    fuzz_obj.root_module.stack_check = false; // not linking with compiler-rt
+    fuzz_obj.root_module.link_libc = true; // afl runtime depends on libc
+
+    const name_exe = b.fmt("fuzz-{s}", .{name});
+    const fuzz_step = b.step(name_exe, b.fmt("Generate fuzz executable for {s}", .{name}));
+    parent_step.dependOn(fuzz_step);
+
+    const name_repro = b.fmt("repro-{s}", .{name});
+    const repro = b.addExecutable(.{
+        .name = name_repro,
+        .root_source_file = b.path("src/fuzz/repro.zig"),
+        .target = target,
+        .optimize = .Debug,
+        .link_libc = true,
+    });
+    repro.root_module.addImport("fuzz_test", &fuzz_obj.root_module);
+    fuzz_step.dependOn(&b.addInstallBinFile(repro.getEmittedBin(), name_repro).step);
+
+    if (build_afl) {
+        const afl = b.lazyImport(@This(), "zig-afl-kit") orelse return;
+        const fuzz_exe = afl.addInstrumentedExe(b, target, .ReleaseSafe, &.{}, fuzz_obj);
+        fuzz_step.dependOn(&b.addInstallBinFile(fuzz_exe, name_exe).step);
+    }
+}
+
 const LlvmPaths = struct {
     include: []const u8,
     lib: []const u8,
@@ -216,55 +265,6 @@ const supported_deps_triples = std.StaticStringMap([]const u8).initComptime(.{
     .{ "x86-linux-gnu", "x86-linux-musl" },
     .{ "x86_64-linux-gnu", "x86_64-linux-musl" },
 });
-
-fn add_fuzz_target(
-    b: *std.Build,
-    build_afl: bool,
-    parent_step: *Step,
-    target: ResolvedTarget,
-    name: []const u8,
-    root_source_file: LazyPath,
-    imports: []const Import,
-) void {
-    const fuzz_obj = b.addObject(.{
-        .name = b.fmt("{s}_obj", .{name}),
-        .root_source_file = root_source_file,
-        .target = target,
-        .optimize = .ReleaseSafe,
-    });
-
-    for (imports) |import| {
-        fuzz_obj.root_module.addImport(import.name, import.module);
-    }
-
-    // TODO: Once 0.14.0 is released, uncomment this. Will make fuzzing work better.
-    // Until then, to get the best fuzzing result modify the std library as specified here:
-    // https://github.com/kristoff-it/zig-afl-kit?tab=readme-ov-file#-------important-------
-    // fuzz_obj.root_module.fuzz = true;
-    fuzz_obj.root_module.stack_check = false; // not linking with compiler-rt
-    fuzz_obj.root_module.link_libc = true; // afl runtime depends on libc
-
-    const name_exe = b.fmt("fuzz-{s}", .{name});
-    const fuzz_step = b.step(name_exe, b.fmt("Generate fuzz executable for {s}", .{name}));
-    parent_step.dependOn(fuzz_step);
-
-    const name_repro = b.fmt("repro-{s}", .{name});
-    const repro = b.addExecutable(.{
-        .name = name_repro,
-        .root_source_file = b.path("src/fuzz/repro.zig"),
-        .target = target,
-        .optimize = .Debug,
-        .link_libc = true,
-    });
-    repro.root_module.addImport("fuzz_test", &fuzz_obj.root_module);
-    fuzz_step.dependOn(&b.addInstallBinFile(repro.getEmittedBin(), name_repro).step);
-
-    if (build_afl) {
-        const afl = b.lazyImport(@This(), "zig-afl-kit") orelse return;
-        const fuzz_exe = afl.addInstrumentedExe(b, target, .ReleaseSafe, &.{}, fuzz_obj);
-        fuzz_step.dependOn(&b.addInstallBinFile(fuzz_exe, name_exe).step);
-    }
-}
 
 // The following is lifted from the zig compiler.
 fn addStaticLlvmOptionsToModule(mod: *std.Build.Module) !void {
