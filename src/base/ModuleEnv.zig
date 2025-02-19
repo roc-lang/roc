15df5fa2007c20b0724aae48b0457abd587bd113
Luke Boswell <lukewilliamboswell@gmail.com>
2025-02-19 15:15:28 +1100

    add next_type_id into ModuleEnv

--- src/base/ModuleEnv.zig
+++ src/base/ModuleEnv.zig
@@ -27,6 +27,7 @@ tag_name_ids_for_slicing: collections.SafeList(TagName.Idx),
 field_names: FieldName.Store,
 strings: StringLiteral.Store,
 problems: std.ArrayList(Problem),
+next_type_id: TypeId,
 
 pub fn init(allocator: std.mem.Allocator) Self {
     var ident_store = Ident.Store.init(allocator);
@@ -40,6 +41,7 @@ pub fn init(allocator: std.mem.Allocator) Self {
         .field_names = FieldName.Store.init(allocator),
         .strings = StringLiteral.Store.init(allocator),
         .problems = std.ArrayList(Problem).init(allocator),
+        .next_type_id = @enumFromInt(0),
     };
 }
 
@@ -58,3 +60,33 @@ pub fn addExposedIdentForModule(self: *Self, ident: Ident.Idx, module: Module.Id
     self.modules.addExposedIdent(module, ident, &self.problems);
     self.idents.setExposingModule(ident, module);
 }
+
+/// Generate a new type ID.
+pub fn newTypeId(self: *Self) TypeId {
+    const id = self.next_type_id;
+    self.next_type_id = self.next_type_id.next();
+    return id;
+}
+
+pub const TypeId = enum(u32) {
+    string = 0,
+    // Add builtins here
+    first_user_space = 1,
+
+    fn index(self: TypeId) u32 {
+        return @intFromEnum(self);
+    }
+
+    fn next(self: TypeId) TypeId {
+        return @enumFromInt(self.index() + 1);
+    }
+
+    pub fn format(self: TypeId, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+        _ = fmt;
+        _ = options;
+
+        try writer.writeAll("TypeId(");
+        try std.fmt.format(writer, "{any}", .{self.index()});
+        try writer.writeAll(")");
+    }
+};
