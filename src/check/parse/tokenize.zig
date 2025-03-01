aba308aba435a60c873aa16527ec10bd41ad73c5
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2025-03-01 14:25:35 -0800

    fix infinite loop in tokenizer

--- src/check/parse/tokenize.zig
+++ src/check/parse/tokenize.zig
@@ -706,6 +706,7 @@ pub const Cursor = struct {
                     self.pos += 1;
                 } else if (c == '\n') {
                     self.pushMessage(.UnclosedSingleQuote, @intCast(start), @intCast(self.pos));
+                    return;
                 } else if (c == '\'') {
                     self.pos += 1;
                 } else {
