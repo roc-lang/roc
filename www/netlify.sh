23f23434c6d0660e3bd9e19dd398b584fa0fca55
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2025-03-05 11:57:33 +0100

    debug netlify.sh
    Signed-off-by: Anton-4 <17049058+Anton-4@users.noreply.github.com>

--- www/netlify.sh
+++ www/netlify.sh
@@ -5,6 +5,9 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euxo pipefail
 
+# for debugging
+cat ./netlify.sh
+
 rustup update
 # get Rust version from rust-toolchain.toml
 rustup default $(grep "channel =" ../rust-toolchain.toml | head -1 | cut -d '"' -f 2)
