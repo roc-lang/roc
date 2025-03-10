18c6417f3b018e4735d5b272200475368352556e
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2025-03-10 15:16:28 +0100

    fix pub comment

--- src/temptest.zig
+++ src/temptest.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 //! This is a test
 
+/// pub comment
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Hello, World!\n", .{});
