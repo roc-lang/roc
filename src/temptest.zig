3a59bdadcb9f2c0d2a5531c40bef40f810b48ce8
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2025-03-10 15:34:59 +0100

    remove test file

--- src/temptest.zig
+++
@@ -1,9 +0,0 @@
-//! This is a test
-
-const std = @import("std");
-
-/// pub comment
-pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("Hello, World!\n", .{});
-}
