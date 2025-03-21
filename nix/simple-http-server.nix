a4ae083b6056db73b11314614bcc6995a59d7b37
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2025-03-21 17:49:41 +0100

    security fix, simple-http-server fix

--- nix/simple-http-server.nix
+++ nix/simple-http-server.nix
@@ -1,6 +1,6 @@
-{ rustPlatform, fetchFromGitHub }:
+{ rustPlatform, fetchFromGitHub, pkg-config, openssl }:
 
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   pname = "simple-http-server";
   version = "0.1.0";  # adjust version as needed
   
@@ -8,9 +8,15 @@ rustPlatform.buildRustPackage {
     owner = "Anton-4";
     repo = "simple-http-server";
     rev = "f3089e5736a1e8abdb69ba9e7618fe5e518a2df0";
+    sha256 = "sha256-Vcckv75hmJV7F9mqPtM3piSIZg9MvKI/oU7/tv4viy4=";
   };
   
   cargoLock = {
     lockFile = "${src}/Cargo.lock";
   };
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs =
+    [ openssl ];
 }
