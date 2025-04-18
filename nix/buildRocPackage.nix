e47f14c4b747fe01367a5e3a4f095893e82fc7cc
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2025-04-18 16:54:08 +0200

    fix unnecessary deps
    Signed-off-by: Anton-4 <17049058+Anton-4@users.noreply.github.com>

--- nix/buildRocPackage.nix
+++ nix/buildRocPackage.nix
@@ -17,20 +17,59 @@ let
     nativeBuildInputs = with pkgs; [ gnutar brotli ripgrep wget cacert ];
 
     buildPhase = ''
+      set -x
+
       declare -A visitedUrls
 
       # function that recursively prefetches roc dependencies
       # so they're available during roc build stage
       function prefetch () {
         local searchPath=$1
+        local skipApp=$2 # to skip any code gen app files in dependencies
+
+        echo "Searching for deps in $searchPath"
+        
+        # Set default value for skipApp if not provided
+        if [ -z "$skipApp" ]; then
+          skipApp=false
+        fi
 
         local dependenciesRegexp='https://[^"]*tar.br|https://[^"]*tar.gz'
-        local getDependenciesCommand="rg -o '$dependenciesRegexp' -IN $searchPath"
-        local depsUrlsList=$(eval "$getDependenciesCommand")
+        
+        # If skipApp is true, exclude files containing app declarations
+        if [ "$skipApp" = true ]; then
+          # Find files containing app declarations
+          local appFiles=$(rg -l '^\s*app\s*\[' -IN $searchPath)
+          echo "appFiles $appFiles"
+          
+          # If app files were found, exclude them from search
+          if [ -n "$appFiles" ]; then
+            local filesWithUrls=$(rg -l "$dependenciesRegexp" -IN  $searchPath)
+
+            for appFile in $appFiles; do
+              local appFullPath=$(realpath "$appFile")
+              # Remove appFullPath from depsUrlsList
+              filesWithUrls=$(echo "$filesWithUrls" | grep -vxF "$appFullPath" || true)
+            done
+
+            if [ -n "$filesWithUrls" ]; then
+              local depsUrlsList=$(rg -o "$dependenciesRegexp" -IN  $filesWithUrls)
+            else
+              return 0
+            fi
+          else
+            local depsUrlsList=$(rg -o "$dependenciesRegexp" -IN  $searchPath)
+          fi
+        else
+          local depsUrlsList=$(rg -o "$dependenciesRegexp" -IN  $searchPath)
+        fi
+
+        depsUrlsList=$(echo "$depsUrlsList" | tr ' ' '\n' | sort | uniq | tr '\n' ' ' | sed 's/ $//')
+        echo "depsUrlsList: $depsUrlsList"
 
         if [ -z "$depsUrlsList" ]; then
-          echo "Executed: $getDependenciesCommand"
-          echo "No URLs found in $searchPath"
+          echo "No dependency URLs need to be downloaded."
+          return 0
         fi
 
         for url in $depsUrlsList; do
@@ -49,12 +88,10 @@ let
             mkdir -p "$outputPackagePath"
 
             # Download dependency
-            set -e
             if ! (wget -P "$outputPackagePath" "$url" 2>/tmp/wget_error); then
               echo "WARNING: Failed to download $url: $(cat /tmp/wget_error)"
-              continue 
+              exit 1
             fi
-            set -e
 
             # Unpack dependency
             if [[ $url == *.br ]]; then
@@ -68,12 +105,13 @@ let
             rm "$outputPackagePath"/*tar*
 
             # Recursively fetch dependencies of dependencies
-            prefetch "$outputPackagePath"
+            # Pass the skipApp parameter to recursive calls
+            prefetch "$outputPackagePath" true
           fi
         done
       }
 
-      prefetch ${src}
+      prefetch ${src} false
 
       if [ -d "$out/roc/packages" ]; then
         echo "Successfully prefetched packages:"
@@ -101,4 +139,3 @@ pkgs.stdenv.mkDerivation {
     mv ${name} $out/bin/${name}
   '';
 }
-
