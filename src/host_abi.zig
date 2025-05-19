1448adb5abeee2c8a9c0b225303bf150045a2d40
Richard Feldman <oss@rtfeldman.com>
2025-05-18 21:34:33 -0400

    fix typo

--- src/host_abi.zig
+++ src/host_abi.zig
@@ -1,5 +1,5 @@
 // Since all of these can be passed across the host boundary, and since compiled Roc
-// programs must not depend on libc or on Zig's standard libray, it's important that
+// programs must not depend on libc or on Zig's standard library, it's important that
 // none of these operations depend on any Zig types like slices or Allocator.
 
 /// All Roc functions that are exposed to the host return `void` and take a `*RocOps`
