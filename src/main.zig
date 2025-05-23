75384d09671cda31eb3a6e6d1ba563649f3f9408
Richard Feldman <oss@rtfeldman.com>
2025-05-22 21:57:48 -0400

    Add a stack memory system for the interpreter

--- src/main.zig
+++ src/main.zig
@@ -12,6 +12,8 @@ const problem_mod = @import("problem.zig");
 const tracy = @import("tracy.zig");
 const Filesystem = @import("coordinate/Filesystem.zig");
 
+const _ = @import("eval/stack.zig"); // TODO this is just so stack.zig gets tested; remove it once it's actually used!
+
 const RocCmd = cli.RocCmd;
 const RocOpt = cli.RocOpt;
 const Problem = problem_mod.Problem;
