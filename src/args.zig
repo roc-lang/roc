93cd4a49bcb5c12d00667c2583fcbc993d3bd6ce
Isaac Van Doren <isaacjvandoren@gmail.com>
2025-06-02 08:16:08 -0500

    use expect equal strings

--- src/args.zig
+++ src/args.zig
@@ -3,7 +3,9 @@ const std = @import("std");
 const testing = std.testing;
 const mem = std.mem;
 
-// The core type representing a parsed command
+/// The core type representing a parsed command
+/// We could use anonymous structs for the argument types instead of defining one for each command to be more concise,
+/// but defining a struct per command means that we can easily take that type and pass it into the function that implements each command.
 pub const CliArgs = union(enum) {
     run: RunArgs,
     check: CheckArgs,
@@ -72,8 +74,14 @@ fn parseCheck(args: []const []const u8) CliArgs {
 
 fn parseBuild(args: []const []const u8) CliArgs {
     if (args.len == 0) return CliArgs{ .build = BuildArgs{ .file = "main.roc" } };
+    // var file = "main.roc";
+    // for (args) |arg| {
+    //     if
+    // }
     return CliArgs{ .build = BuildArgs{ .file = args[0] } };
 }
+
+// fn get_flag_name([]const u8) ?[]const u8 {}
 
 fn parseRun(args: []const []const u8) CliArgs {
     _ = args;
@@ -81,28 +89,43 @@ fn parseRun(args: []const []const u8) CliArgs {
 }
 
 test "roc run" {
-    const result = parse(&[_][]const u8{""});
-    try testing.expectEqual(result.run.file, "main.roc");
+    {
+        const result = parse(&[_][]const u8{});
+        try testing.expectEqualStrings("main.roc", result.run.file);
+    }
+    {
+        const result = parse(&[_][]const u8{""});
+        try testing.expectEqualStrings("main.roc", result.run.file);
+    }
+    {
+        const result = parse(&[_][]const u8{ "", "", "" });
+        try testing.expectEqualStrings("main.roc", result.run.file);
+    }
 }
 
 test "roc check" {
     {
         const result = parse(&[_][]const u8{"check"});
-        try testing.expectEqual(result.check.file, "main.roc");
+        try testing.expectEqualStrings("main.roc", result.check.file);
     }
     {
         const result = parse(&[_][]const u8{ "check", "some/file.roc" });
-        try testing.expectEqual(result.check.file, "some/file.roc");
+        try testing.expectEqualStrings("some/file.roc", result.check.file);
     }
 }
 
 test "roc build" {
     {
         const result = parse(&[_][]const u8{"build"});
-        try testing.expectEqual(result.build.file, "main.roc");
+        try testing.expectEqualStrings("main.roc", result.build.file);
     }
     {
         const result = parse(&[_][]const u8{ "build", "foo.roc" });
-        try testing.expectEqual(result.build.file, "foo.roc");
+        try testing.expectEqualStrings("foo.roc", result.build.file);
+    }
+    {
+        const result = parse(&[_][]const u8{ "build", "--opt=size" });
+        try testing.expectEqualStrings("main.roc", result.build.file);
+        try testing.expectEqual(OptLevel.size, result.build.opt);
     }
 }
