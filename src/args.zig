41884414b544c2a69d64ae2550d7a570fe14fa90
Isaac Van Doren <isaacjvandoren@gmail.com>
2025-06-07 20:59:44 -0500

    Add format

--- src/args.zig
+++ src/args.zig
@@ -27,27 +27,27 @@ pub const OptLevel = enum {
 };
 
 pub const RunArgs = struct {
-    file: []const u8,
+    path: []const u8,
     opt: OptLevel = .none,
 };
 
 pub const CheckArgs = struct {
-    file: []const u8,
+    path: []const u8,
 };
 
 pub const BuildArgs = struct {
-    file: []const u8,
+    path: []const u8,
     opt: OptLevel = .none,
     output: ?[]const u8 = null,
 };
 
 pub const TestArgs = struct {
-    file: []const u8,
+    path: []const u8,
     opt: OptLevel = .none,
 };
 
 pub const FormatArgs = struct {
-    path: ?[]const u8 = null,
+    path: []const u8,
     stdin: bool = false,
     check: bool = false,
 };
@@ -60,20 +60,21 @@ pub const DocsArgs = struct {
 /// Parse a list of arguments.
 // TODO: should we ignore extra arguments or return errors when they are included?
 pub fn parse(args: []const []const u8) CliArgs {
-    if (args.len == 0) return CliArgs{ .run = RunArgs{ .file = "main.roc" } };
+    if (args.len == 0) return CliArgs{ .run = RunArgs{ .path = "main.roc" } };
     if (mem.eql(u8, args[0], "check")) return parseCheck(args[1..]);
     if (mem.eql(u8, args[0], "build")) return parseBuild(args[1..]);
+    if (mem.eql(u8, args[0], "format")) return parseFormat(args[1..]);
 
     return parseRun(args[1..]);
 }
 
 fn parseCheck(args: []const []const u8) CliArgs {
-    if (args.len == 0) return CliArgs{ .check = CheckArgs{ .file = "main.roc" } };
-    return CliArgs{ .check = CheckArgs{ .file = args[0] } };
+    if (args.len == 0) return CliArgs{ .check = CheckArgs{ .path = "main.roc" } };
+    return CliArgs{ .check = CheckArgs{ .path = args[0] } };
 }
 
 fn parseBuild(args: []const []const u8) CliArgs {
-    var file: ?[]const u8 = null;
+    var path: ?[]const u8 = null;
     var opt: OptLevel = .none;
     var output: ?[]const u8 = null;
     for (args) |arg| {
@@ -98,63 +99,85 @@ fn parseBuild(args: []const []const u8) CliArgs {
                 return CliArgs{ .invalid = "--opt can be either speed or size" };
             }
         } else {
-            if (file != null) {
+            if (path != null) {
                 return CliArgs{ .invalid = "unexpected argument" };
             }
-            file = arg;
+            path = arg;
         }
     }
-    return CliArgs{ .build = BuildArgs{ .file = file orelse "main.roc", .opt = opt, .output = output } };
+    return CliArgs{ .build = BuildArgs{ .path = path orelse "main.roc", .opt = opt, .output = output } };
 }
 
+fn parseFormat(args: []const []const u8) CliArgs {
+    var path: ?[]const u8 = null;
+    var stdin = false;
+    var check = false;
+    for (args) |arg| {
+        if (mem.eql(u8, arg, "-h") or mem.eql(u8, arg, "--help")) {
+            return CliArgs{ .help = 
+            \\TODO build help message here
+        };
+        } else if (mem.eql(u8, arg, "--stdin")) {
+            stdin = true;
+        } else if (mem.eql(u8, arg, "--check")) {
+            check = true;
+        } else {
+            if (path != null) {
+                return CliArgs{ .invalid = "unexpected argument" };
+            }
+            path = arg;
+        }
+    }
+    return CliArgs{ .format = FormatArgs{ .path = path orelse "main.roc", .stdin = stdin, .check = check } };
+}
 fn parseRun(args: []const []const u8) CliArgs {
     _ = args;
-    return CliArgs{ .run = RunArgs{ .file = "main.roc" } };
+    return CliArgs{ .run = RunArgs{ .path = "main.roc" } };
 }
 
 test "roc run" {
     {
         const result = parse(&[_][]const u8{});
-        try testing.expectEqualStrings("main.roc", result.run.file);
+        try testing.expectEqualStrings("main.roc", result.run.path);
     }
     {
         const result = parse(&[_][]const u8{""});
-        try testing.expectEqualStrings("main.roc", result.run.file);
+        try testing.expectEqualStrings("main.roc", result.run.path);
     }
     {
         const result = parse(&[_][]const u8{ "", "", "" });
-        try testing.expectEqualStrings("main.roc", result.run.file);
+        try testing.expectEqualStrings("main.roc", result.run.path);
     }
 }
 
 test "roc check" {
     {
         const result = parse(&[_][]const u8{"check"});
-        try testing.expectEqualStrings("main.roc", result.check.file);
+        try testing.expectEqualStrings("main.roc", result.check.path);
     }
     {
         const result = parse(&[_][]const u8{ "check", "some/file.roc" });
-        try testing.expectEqualStrings("some/file.roc", result.check.file);
+        try testing.expectEqualStrings("some/file.roc", result.check.path);
     }
 }
 
 test "roc build" {
     {
         const result = parse(&[_][]const u8{"build"});
-        try testing.expectEqualStrings("main.roc", result.build.file);
+        try testing.expectEqualStrings("main.roc", result.build.path);
     }
     {
         const result = parse(&[_][]const u8{ "build", "foo.roc" });
-        try testing.expectEqualStrings("foo.roc", result.build.file);
+        try testing.expectEqualStrings("foo.roc", result.build.path);
     }
     {
         const result = parse(&[_][]const u8{ "build", "--opt=size" });
-        try testing.expectEqualStrings("main.roc", result.build.file);
+        try testing.expectEqualStrings("main.roc", result.build.path);
         try testing.expectEqual(OptLevel.size, result.build.opt);
     }
     {
         const result = parse(&[_][]const u8{ "build", "--opt=speed", "foo/bar.roc", "--output=mypath" });
-        try testing.expectEqualStrings("foo/bar.roc", result.build.file);
+        try testing.expectEqualStrings("foo/bar.roc", result.build.path);
         try testing.expectEqual(OptLevel.speed, result.build.opt);
         try testing.expectEqualStrings("mypath", result.build.output.?);
     }
@@ -177,5 +200,60 @@ test "roc build" {
     {
         const result = parse(&[_][]const u8{ "build", "foo.roc", "--opt=size", "--help" });
         try testing.expectEqualStrings("TODO build help message here", result.help);
+    }
+    {
+        const result = parse(&[_][]const u8{ "build", "--thisisactuallyafile" });
+        try testing.expectEqualStrings("--thisisactuallyafile", result.build.path);
+    }
+}
+
+test "roc format" {
+    {
+        const result = parse(&[_][]const u8{"format"});
+        try testing.expectEqualStrings("main.roc", result.format.path);
+        try testing.expect(!result.format.stdin);
+        try testing.expect(!result.format.check);
+    }
+    {
+        const result = parse(&[_][]const u8{ "format", "--check" });
+        try testing.expectEqualStrings("main.roc", result.format.path);
+        try testing.expect(!result.format.stdin);
+        try testing.expect(result.format.check);
+    }
+    {
+        const result = parse(&[_][]const u8{ "format", "--stdin" });
+        try testing.expectEqualStrings("main.roc", result.format.path);
+        try testing.expect(result.format.stdin);
+        try testing.expect(!result.format.check);
+    }
+    {
+        const result = parse(&[_][]const u8{ "format", "--stdin", "--check", "foo.roc" });
+        try testing.expectEqualStrings("foo.roc", result.format.path);
+        try testing.expect(result.format.stdin);
+        try testing.expect(result.format.check);
+    }
+    {
+        const result = parse(&[_][]const u8{ "format", "foo.roc" });
+        try testing.expectEqualStrings("foo.roc", result.format.path);
+    }
+    {
+        const result = parse(&[_][]const u8{ "format", "foo.roc", "bar.roc" });
+        try testing.expectEqualStrings("unexpected argument", result.invalid);
+    }
+    {
+        const result = parse(&[_][]const u8{ "format", "-h" });
+        try testing.expectEqualStrings("TODO build help message here", result.help);
+    }
+    {
+        const result = parse(&[_][]const u8{ "format", "--help" });
+        try testing.expectEqualStrings("TODO build help message here", result.help);
+    }
+    {
+        const result = parse(&[_][]const u8{ "format", "foo.roc", "--help" });
+        try testing.expectEqualStrings("TODO build help message here", result.help);
+    }
+    {
+        const result = parse(&[_][]const u8{ "format", "--thisisactuallyafile" });
+        try testing.expectEqualStrings("--thisisactuallyafile", result.format.path);
     }
 }
