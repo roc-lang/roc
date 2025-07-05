71c47eb1996ce02258cd97c361d251f3dad3dbcf
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2025-07-05 14:19:06 +0200

    rm nightly_benches

--- nightly_benches/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    println!("""
-        To run benchmarks:
-            - Install cargo criterion: cargo install cargo-criterion
-            - Necessary to get cache misses...: sudo sh -c 'echo 1 >/proc/sys/kernel/perf_event_paranoid'
-            - run: cargo criterion
-    """);
-}
