50a5657edf3c0820923bf7c230f71ae388916698
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2025-07-09 16:07:31 +0200

    Update new string interpolation syntax
    Signed-off-by: Anton-4 <17049058+Anton-4@users.noreply.github.com>

--- www/public/site.js
+++ www/public/site.js
@@ -40,7 +40,7 @@ const repl = {
     },
     {
       match: (input) => input.replace(/ /g, "").match(/^name="/i),
-      show: '<p>This created a new <a href="https://www.roc-lang.org/tutorial#defs">definition</a>&mdash;<code>name</code> is now defined to be equal to the <a href="/tutorial#strings-and-numbers">string</a> you entered.</p><p>Try using this definition by entering <code>"Hi, \$(name)!"</code></p>',
+      show: '<p>This created a new <a href="https://www.roc-lang.org/tutorial#defs">definition</a>&mdash;<code>name</code> is now defined to be equal to the <a href="/tutorial#strings-and-numbers">string</a> you entered.</p><p>Try using this definition by entering <code>"Hi, \${name}!"</code></p>',
     },
     {
       match: (input) => input.match(/^"[^\$]+\$\(name\)/i),
