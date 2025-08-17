2e270fe607a5685c3c7ff73d2cced4c70a0be734
Richard Feldman <oss@rtfeldman.com>
2025-08-17 12:53:55 -0400

    More Parser2 improvements

--- src/parse/mod.zig
+++ src/parse/mod.zig
@@ -31,7 +31,7 @@ pub const AST = @import("AST.zig");
 pub const AST2 = @import("AST2.zig");
 
 /// New version of the Parser module for development.
-// pub const Parser2 = @import("Parser2.zig"); // TODO: Uncomment once AST2 has header support
+pub const Parser2 = @import("Parser2.zig");
 
 fn runParse(env: *CommonEnv, gpa: std.mem.Allocator, parserCall: *const fn (*Parser) Parser.Error!u32) Parser.Error!AST {
     const trace = tracy.trace(@src());
@@ -124,7 +124,7 @@ test "parser tests" {
     std.testing.refAllDecls(@import("Node.zig"));
     std.testing.refAllDecls(@import("NodeStore.zig"));
     std.testing.refAllDecls(@import("Parser.zig"));
-    // std.testing.refAllDecls(@import("Parser2.zig")); // TODO: Uncomment once AST2 has header support
+    std.testing.refAllDecls(@import("Parser2.zig"));
     std.testing.refAllDecls(@import("tokenize.zig"));
     std.testing.refAllDecls(@import("test/ast_node_store_test.zig"));
 }
