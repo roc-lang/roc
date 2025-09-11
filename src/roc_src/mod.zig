7237eec644abcd3854b8c458798563f19c54f2bf
Richard Feldman <oss@rtfeldman.com>
2025-09-11 18:36:53 -0400

    Delete Lines.zig

--- src/roc_src/mod.zig
+++ src/roc_src/mod.zig
@@ -2,9 +2,4 @@
 const std = @import("std");
 
 pub const Span = @import("Span.zig");
-pub const Lines = @import("Lines.zig");
 pub const Bytes = [:'\n']align(16) u8;
-
-test "roc_src tests" {
-    std.testing.refAllDecls(@import("Lines.zig"));
-}
