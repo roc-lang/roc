504d556bb9acb1f2140930276f104fdc659aded0
Joshua Warner <joshuawarner32@gmail.com>
2025-10-12 15:51:52 -0700

    Move from triple-double for multiline strings to zig-like double-backslash

--- src/fmt/fmt.zig
+++ src/fmt/fmt.zig
@@ -837,7 +837,7 @@ const Formatter = struct {
                     fmt.curr_indent += 1;
                 }
                 var add_newline = false;
-                try fmt.pushAll("\"\"\"");
+                try fmt.pushAll("\\\\");
                 for (fmt.ast.store.exprSlice(s.parts)) |idx| {
                     const e = fmt.ast.store.getExpr(idx);
                     switch (e) {
@@ -847,7 +847,7 @@ const Formatter = struct {
                                 _ = try fmt.flushCommentsBefore(str.region.start - 1);
                                 try fmt.ensureNewline();
                                 try fmt.pushIndent();
-                                try fmt.pushAll("\"\"\"");
+                                try fmt.pushAll("\\\\");
                             }
 
                             add_newline = true;
