363bf42e94292f4c51777569ef9d66b313048c92
Richard Feldman <oss@rtfeldman.com>
2025-12-09 22:51:45 -0500

    Restore some tests

--- src/check/mod.zig
+++ src/check/mod.zig
@@ -34,7 +34,6 @@ test "check tests" {
 
     std.testing.refAllDecls(@import("test/cross_module_test.zig"));
     std.testing.refAllDecls(@import("test/type_checking_integration.zig"));
-    std.testing.refAllDecls(@import("test/static_dispatch_test.zig"));
     std.testing.refAllDecls(@import("test/let_polymorphism_integration_test.zig"));
     std.testing.refAllDecls(@import("test/num_type_requirements_test.zig"));
     std.testing.refAllDecls(@import("test/custom_num_type_test.zig"));
