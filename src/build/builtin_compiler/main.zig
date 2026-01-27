b8aeb39ba14355f7d6e06792b0e5bc4550d44a4a
Jared Ramirez <jaredpramirez@proton.me>
2026-01-27 15:16:48 -0500

    Fix bad err type

--- src/build/builtin_compiler/main.zig
+++ src/build/builtin_compiler/main.zig
@@ -1751,7 +1751,7 @@ fn compileModule(
         const config = reporting.ReportingConfig.initColorTerminal();
 
         const problem = check.problem;
-        var report_builder = check.report.ReportBuilder.init(
+        var report_builder = try check.report.ReportBuilder.init(
             gpa,
             module_env,
             module_env,
@@ -1760,7 +1760,7 @@ fn compileModule(
             source_path,
             imported_envs.items,
             &checker.import_mapping,
-        ) catch return 1;
+        );
         defer report_builder.deinit();
 
         for (0..checker.problems.len()) |i| {
