09bc452afe1672b7c9f31def62b40dc7a875bb8e
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2026-01-28 16:57:39 +0100

    should be +=

--- ci/benchmarks_zig/run_fx_benchmarks.sh
+++ ci/benchmarks_zig/run_fx_benchmarks.sh
@@ -99,7 +99,7 @@ for fx_file in $FX_FILES; do
         issue8826_minimal.roc|\
         unused_state_var.roc|\
         issue8943.roc)
-            EXTRA_ARGS="--ignore-failure=1,2"
+            EXTRA_ARGS+=" --ignore-failure=1,2"
             ;;
         num_method_call.roc)
             ROC_EXTRA_ARGS="--allow-errors"
