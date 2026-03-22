1ae4b9e6751d44a0da5bb2ca5d1ca204fa64f940
Norbert Hajagos <hajagosnorbi@gmail.com>
2026-03-22 09:00:01 +0100

    apply Anton's suggestion

--- src/eval/interpreter.zig
+++ src/eval/interpreter.zig
@@ -2787,8 +2787,7 @@ pub const Interpreter = struct {
                 const result_rt_var = return_rt_var orelse try self.runtime_types.fresh();
                 var dest = try self.pushRaw(record_layout, 0, result_rt_var);
                 var acc = try dest.asRecord(&self.runtime_layout_store);
-
-                const list_field_stack_val = try acc.getFieldByIndex(list_field.idx, list_arg.rt_var);
+                const list_field_stack_val = try acc.getFieldByName("list", list_arg.rt_var);
                 const list_ptr: *builtins.list.RocList = @ptrCast(@alignCast(list_field_stack_val.ptr.?));
 
                 if (self.runtime_layout_store.isZeroSized(list_info.elem_layout)) {
@@ -2802,7 +2801,7 @@ pub const Interpreter = struct {
 
                 // Get pointer space for the replaced item
                 const elem_rt_var = elt_arg.rt_var;
-                const prev_field_stack_val = try acc.getFieldByIndex(prev_field.idx, elem_rt_var);
+                const prev_field_stack_val = try acc.getFieldByName("prev", elem_rt_var);
                 const prev_field_ptr = prev_field_stack_val.ptr orelse unreachable;
 
                 // Handle numeric type mismatch for index argument.
