9c1d538c86925816a4713bc3add66f88c80a8175
Richard Feldman <oss@rtfeldman.com>
2026-06-08 02:29:29 -0400

    Inline collection item parent stack operations

--- src/parse/Parser.zig
+++ src/parse/Parser.zig
@@ -2011,14 +2011,14 @@ const ExprCollectionItemParents = struct {
         self.stack.deinit(allocator);
     }
 
-    fn set(self: *ExprCollectionItemParents, allocator: std.mem.Allocator, open_depth: usize) Error!void {
+    inline fn set(self: *ExprCollectionItemParents, allocator: std.mem.Allocator, open_depth: usize) Error!void {
         if (self.current) |current| {
             try self.stack.append(allocator, current);
         }
         self.current = open_depth;
     }
 
-    fn take(self: *ExprCollectionItemParents) void {
+    inline fn take(self: *ExprCollectionItemParents) void {
         _ = self.current orelse unreachable;
         self.current = self.stack.pop();
     }
